44c80c82c6737b1cf790bbab09767228bc14ffef
Ira Hopkinson <ira_hopkinson@sil.org>
2023-01-24 11:19:27 +1300

    Update for Paranext

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # paranext-core
+
 Backend webserver/extension host and frontend Electron client for Paranext
+
+<div align="center" style="background-color: #b8d432;">
+  <br />
+  <img src="assets/pt-react.png" />
+  <br />
+  <br />
+</div>
+
+<div align="center">
+
+[![Build Status][github-actions-status]][github-actions-url]
+[![Github Tag][github-tag-image]][github-tag-url]
+
+</div>
+
+## Users
+
+This software is not yet ready for users. We'll update here with where you can install it when its ready.
+
+## Developer Install
+
+Clone the repo and install dependencies:
+
+```bash
+git clone https://github.com/sillsdev/Paranext.git
+cd Paranext
+npm install
+```
+
+## Starting Development
+
+Start the app in the `dev` environment:
+
+```bash
+npm start
+```
+
+## Packaging for Production
+
+To package apps for the local platform:
+
+```bash
+npm run package
+```
+
+## License
+
+MIT © [SIL International](https://www.sil.org/)
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,5 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import icon from '../../assets/pt-react.png';
 import './App.css';
 
 const Hello = () => {
@@ -8,32 +8,8 @@ const Hello = () => {
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+        <h1>Paranext</h1>
       </div>
     </div>
   );
