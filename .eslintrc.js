52897856f9f4be9803ef686aeb1840199316348e
tjcouch-sil <tj_couch@sil.org>
2023-01-23 12:41:44 -0600

    Added .NET 7 project, set up some config from POC

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,23 @@ module.exports = {
     'import/no-unresolved': 'error',
     // Since React 17 and typescript 4.1 you can safely disable the rule
     'react/react-in-jsx-scope': 'off',
+
+    // TJ's rules
+    indent: 'off',
+    'react/jsx-indent-props': ['warn', 4],
+    'comma-dangle': ['error', 'always-multiline'],
+    'prettier/prettier': ['warn', { tabWidth: 2, trailingComma: 'all' }],
+    'no-console': 'off',
+    'react/require-default-props': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+      },
+    ],
+    // This is already a Typescript rule, so we don't need it to be reported twice
+    '@typescript-eslint/no-unused-vars': 'off',
   },
   parserOptions: {
     ecmaVersion: 2020,
