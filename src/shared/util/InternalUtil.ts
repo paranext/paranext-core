fd7258101c004614a292a64aa2a650bbe6b27463
tjcouch-sil <tj_couch@sil.org>
2023-02-01 16:33:37 -0600

    Added attribution for process check

--- src/shared/util/InternalUtil.ts
+++ src/shared/util/InternalUtil.ts
@@ -3,7 +3,8 @@
  */
 
 /**
- * Determine if running on the clientor on the server
+ * Determine if running on the client or on the server.
+ * Thanks to Marshal at https://stackoverflow.com/a/71388839/8535752
  * @returns Returns true if running on the client, false otherwise
  */
 // eslint-disable-next-line import/prefer-default-export
