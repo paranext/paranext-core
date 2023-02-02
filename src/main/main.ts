54a7240cb6dbb2dcd3702d26876fc2632374d54b
tjcouch-sil <tj_couch@sil.org>
2023-02-02 09:02:15 -0600

    Added proper icon files

--- src/main/main.ts
+++ src/main/main.ts
@@ -73,7 +73,7 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
-    icon: getAssetPath('pt-react.png'),
+    icon: getAssetPath('icon.png'),
     webPreferences: {
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
