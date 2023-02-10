c62a2941aa857f0fb0d98052eaa5d6cced24c14c
tjcouch-sil <tj_couch@sil.org>
2023-02-09 22:01:43 -0600

    Fixed path aliasing for jest, sorta fixed jest test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,21 @@
+/* eslint-disable import/first */
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { act, render, RenderResult } from '@testing-library/react';
+
+jest.mock('@shared/services/NetworkService', () => ({
+  createRequestFunction:
+    (requestType: string) =>
+    async (...args: unknown[]) =>
+      `Mocked ${requestType} request with args ${args.join(', ')}`,
+}));
 import App from '../renderer/App';
 
 describe('App', () => {
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
+  it('should render', async () => {
+    let renderResult: RenderResult | undefined;
+    act(() => {
+      renderResult = render(<App />);
+    });
+    expect(renderResult).toBeTruthy();
   });
 });
