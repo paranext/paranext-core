2864d267cf0d36b89e51b58737c00e92f5f82269
tjcouch-sil <tj_couch@sil.org>
2023-03-06 17:33:52 -0600

    Improved documentation for useEvent

--- src/renderer/hooks/papi-hooks/useEvent.ts
+++ src/renderer/hooks/papi-hooks/useEvent.ts
@@ -3,6 +3,15 @@ import { Event, EventHandler } from '@shared/util/Event';
 import { isString } from '@shared/util/Util';
 import { useEffect } from 'react';
 
+/**
+ * Adds an event handler to an event so the event handler runs when the event is emitted
+ * @param event the event to subscribe to. Can be either a string or an Event
+ *  - If event is a `string`, the network event associated with this type will automatically be used
+ *  - If event is an `Event`, that event will be used
+ * @param eventHandler the callback to run when the event is emitted
+ *
+ *    WARNING: MUST BE WRAPPED IN A useCallback. The reference must not be updated every render
+ */
 export default <T>(event: Event<T> | string, eventHandler: EventHandler<T>) => {
   useEffect(() => {
     const onEvent = isString(event) ? getNetworkEvent<T>(event) : event;
--- src/renderer/hooks/papi-hooks/usePromise.ts
+++ src/renderer/hooks/papi-hooks/usePromise.ts
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 
-/** Awaits a promise and returns a loading value while the promise is unresolved
+/**
+ * Awaits a promise and returns a loading value while the promise is unresolved
  * @param promiseFactoryCallback a function that returns the promise to await. If the promise resolves to null, the value will not change.
  *
- *      WARNING: MUST BE WRAPPED IN A useCallback. The reference must not be updated every render
+ *    WARNING: MUST BE WRAPPED IN A useCallback. The reference must not be updated every render
  * @param defaultValue the initial value to return while first awaiting the promise. If preserveValue is false, this value is also shown while awaiting the promise on subsequent calls.
  *
- *      WARNING: MUST BE WRAPPED IN A useState, useMemo, etc. The reference must not be updated every render
+ *    WARNING: MUST BE WRAPPED IN A useState, useMemo, etc. The reference must not be updated every render
  * @param preserveValue whether to leave the value as the most recent resolved promise value or set it back to defaultValue while running the promise again. Default to true
  * @returns [value, isLoading] the current value for the promise, either the defaultValue or the resolved promise value; whether the promise is waiting to be resolved
  */
