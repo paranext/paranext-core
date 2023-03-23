4a517fe57463af4da6a16bc496a4ab59dbf2dedf
rolfheij-sil <rolf_heij@sil.org>
2023-03-23 20:35:53 +0100

    Remove reference to PButton

--- src/renderer/components/papi-components/papi-components.ts
+++ src/renderer/components/papi-components/papi-components.ts
@@ -1,4 +1,3 @@
-import { PButton } from '@renderer/components/papi-components/PButton';
 import Button from '@renderer/components/papi-components/Button';
 import ComboBox from '@renderer/components/papi-components/ComboBox';
 import Slider from '@renderer/components/papi-components/Slider';
@@ -7,7 +6,6 @@ import TextField from '@renderer/components/papi-components/TextField';
 
 /** All React components to be exposed on the papi */
 export default {
-  PButton,
   Button,
   ComboBox,
   Slider,
