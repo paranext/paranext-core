5f3ddf4fbafad869ee61d5bd28b005e464e8e331
rolfheij-sil <rolf_heij@sil.org>
2023-03-31 17:56:49 +0200

    Added more props to Table

--- src/renderer/components/papi-components/Table.tsx
+++ src/renderer/components/papi-components/Table.tsx
@@ -1,64 +1,118 @@
 import 'react-data-grid/lib/styles.css';
 import DataGrid, {
   CellClickArgs,
+  CellKeyboardEvent,
+  CellKeyDownArgs,
   CellMouseEvent,
   Column,
   CopyEvent,
   FillEvent,
   PasteEvent,
   RowsChangeData,
+  SortColumn,
 } from 'react-data-grid';
-import { Key } from 'react';
+import { Key, UIEvent } from 'react';
 
+// Make default TRow type
+type TRow = {};
+
+export type TableCellClickArgs<T = TRow> = CellClickArgs<T>;
+export type TableCellKeyboardEvent = CellKeyboardEvent;
+export type TableCellKeyDownArgs<T = TRow> = CellKeyDownArgs<T>;
+export type TableCellMouseEvent = CellMouseEvent;
+export type TableColumn<T = TRow> = Column<T>;
+export type TableCopyEvent<T = TRow> = CopyEvent<T>;
+export type TableFillEvent<T = TRow> = FillEvent<T>;
+export type TablePasteEvent<T = TRow> = PasteEvent<T>;
+export type TableRowsChangeData<T = TRow> = RowsChangeData<T>;
+export type TableSortColumn = SortColumn;
+
+// Subset of https://github.com/adazzle/react-data-grid#api
 export type TableProps = {
-  columns: readonly Column<unknown, unknown>[];
+  // Add comments & defaults
+  columns: readonly TableColumn<unknown>[];
+  sortColumns?: readonly TableSortColumn[];
+  onSortColumnsChange?: (sortColumns: TableSortColumn[]) => void;
+  onColumnResize?: (idx: number, width: number) => void;
+  sortable?: boolean;
+  resizable?: boolean;
   rows: readonly unknown[];
   rowKeyGetter?: (row: unknown) => Key;
-  onRowsChange?: (rows: unknown[], data: RowsChangeData<unknown, unknown>) => void;
-  onFill?: (event: FillEvent<unknown>) => unknown;
-  onCopy?: (event: CopyEvent<unknown>) => void;
-  onPaste?: (event: PasteEvent<unknown>) => unknown;
+  rowClass?: (row: unknown) => string;
   rowHeight?: number;
+  headerRowHeight?: number;
   selectedRows?: ReadonlySet<Key>;
   onSelectedRowsChange?: (selectedRows: Set<Key>) => void;
-  className?: string;
-  rowClass?: (row: unknown) => string;
+  onRowsChange?: (rows: unknown[], data: TableRowsChangeData<unknown>) => void;
+  onCellClick?: (args: TableCellClickArgs<unknown>, event: TableCellMouseEvent) => void;
+  onCellDoubleClick?: (args: TableCellClickArgs<unknown>, event: TableCellMouseEvent) => void;
+  onCellContextMenu?: (args: TableCellClickArgs<unknown>, event: TableCellMouseEvent) => void;
+  onCellKeyDown?: (args: TableCellKeyDownArgs<unknown>, event: TableCellKeyboardEvent) => void;
   direction?: 'ltr' | 'rtl';
-  onCellClick?: (args: CellClickArgs<unknown, unknown>, event: CellMouseEvent) => void;
+  enableVirtualization?: boolean;
+  onScroll?: (event: UIEvent<HTMLDivElement>) => void;
+  onFill?: (event: TableFillEvent<unknown>) => unknown;
+  onCopy?: (event: TableCopyEvent<unknown>) => void;
+  onPaste?: (event: TablePasteEvent<unknown>) => unknown;
+  className?: string;
 };
 
 function Table({
   columns,
+  sortColumns,
+  onSortColumnsChange,
+  onColumnResize,
+  sortable,
+  resizable,
   rows,
   rowKeyGetter,
+  rowClass,
+  rowHeight,
+  headerRowHeight,
+  selectedRows,
+  onSelectedRowsChange,
   onRowsChange,
+  onCellClick,
+  onCellDoubleClick,
+  onCellContextMenu,
+  onCellKeyDown,
+  direction,
+  enableVirtualization,
   onFill,
   onCopy,
   onPaste,
-  rowHeight,
-  selectedRows,
-  onSelectedRowsChange,
+  onScroll,
   className,
-  rowClass,
-  direction,
-  onCellClick,
 }: TableProps) {
   return (
     <DataGrid
       columns={columns}
+      defaultColumnOptions={{
+        sortable,
+        resizable,
+      }}
+      sortColumns={sortColumns}
+      onSortColumnsChange={onSortColumnsChange}
+      onColumnResize={onColumnResize}
       rows={rows}
       rowKeyGetter={rowKeyGetter}
+      rowClass={rowClass}
+      rowHeight={rowHeight}
+      headerRowHeight={headerRowHeight}
+      selectedRows={selectedRows}
+      onSelectedRowsChange={onSelectedRowsChange}
       onRowsChange={onRowsChange}
+      onCellClick={onCellClick}
+      onCellDoubleClick={onCellDoubleClick}
+      onCellContextMenu={onCellContextMenu}
+      onCellKeyDown={onCellKeyDown}
+      direction={direction}
+      enableVirtualization={enableVirtualization}
       onFill={onFill}
       onCopy={onCopy}
       onPaste={onPaste}
-      rowHeight={rowHeight}
-      selectedRows={selectedRows}
-      onSelectedRowsChange={onSelectedRowsChange}
+      onScroll={onScroll}
       className={className}
-      rowClass={rowClass}
-      direction={direction}
-      onCellClick={onCellClick}
     />
   );
 }
