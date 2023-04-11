242040094c25ed5219fce240fbd85f6bbb826fad
tjcouch-sil <tj_couch@sil.org>
2023-04-11 17:03:20 -0500

    Added useDataProvider to papi.react.hooks

--- src/renderer/hooks/papi-hooks/papi-hooks.ts
+++ src/renderer/hooks/papi-hooks/papi-hooks.ts
@@ -1,6 +1,7 @@
 import usePromise from '@renderer/hooks/papi-hooks/usePromise';
 import useEvent from '@renderer/hooks/papi-hooks/useEvent';
 import useEventAsync from '@renderer/hooks/papi-hooks/useEventAsync';
+import useDataProvider from '@renderer/hooks/papi-hooks/useDataProvider';
 import useData from '@renderer/hooks/papi-hooks/useData';
 
 /** All React hooks to be exposed on the papi */
@@ -8,5 +9,6 @@ export default {
   usePromise,
   useEvent,
   useEventAsync,
+  useDataProvider,
   useData,
 };
