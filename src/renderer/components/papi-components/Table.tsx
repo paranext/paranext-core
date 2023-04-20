0c5024fa1213598ec65de949a044037618d29aa7
rolfheij-sil <rolf_heij@sil.org>
2023-04-20 14:13:02 +0200

    Manage state in stories with useArgs

--- src/renderer/components/papi-components/Table.tsx
+++ src/renderer/components/papi-components/Table.tsx
@@ -6,21 +6,36 @@ import DataGrid, {
   CellMouseEvent,
   CopyEvent,
   FillEvent,
+  FormatterProps,
   PasteEvent,
   RowsChangeData,
+  SelectColumn,
   SortColumn,
+  textEditor,
 } from 'react-data-grid';
-import { Key, UIEvent } from 'react';
+import { Key, ReactElement, ReactNode, UIEvent } from 'react';
 
+export interface TableCalculatedColumn<T> extends TableColumn<T> {
+  readonly idx: number;
+  readonly width: number | string;
+  readonly minWidth: number;
+  readonly maxWidth: number | undefined;
+  readonly resizable: boolean;
+  readonly sortable: boolean;
+  readonly frozen: boolean;
+  readonly isLastFrozenColumn: boolean;
+  readonly rowGroup: boolean;
+  readonly formatter: (props: TableFormatterProps<T>) => ReactNode;
+}
 export type TableCellClickArgs<T> = CellClickArgs<T>;
 export type TableCellKeyboardEvent = CellKeyboardEvent;
 export type TableCellKeyDownArgs<T> = CellKeyDownArgs<T>;
 export type TableCellMouseEvent = CellMouseEvent;
-export type TableColumn = {
+export type TableColumn<T> = {
   /**
    * The name of the column. By default it will be displayed in the header cell
    */
-  readonly name: string;
+  readonly name: string | ReactElement;
   /**
    * A unique key to distinguish each column
    */
@@ -29,49 +44,67 @@ export type TableColumn = {
    * Column width. If not specified, it will be determined automatically
    * based on grid width and specified widths of other columns
    */
-  readonly width?: number;
+  readonly width?: number | string | null;
   /**
    * Minimum column width in px.
    */
-  readonly minWidth?: number;
+  readonly minWidth?: number | null;
   /**
    * Maximum column width in px.
    */
-  readonly maxWidth?: number;
+  readonly maxWidth?: number | null;
   /**
    * Enables cell editing
    */
-  readonly editable?: boolean;
+  readonly editable?: boolean | ((row: T) => boolean) | null;
+  /**
+   * Formatter to be used to render the cell content
+   */
+  readonly formatter?: ((props: TableFormatterProps<T>) => ReactNode) | null;
   /**
    * Determines whether column is frozen or not
    */
-  readonly frozen?: boolean;
+  readonly frozen?: boolean | null;
   /**
    * Enable resizing of a column
    */
-  readonly resizable?: boolean;
+  readonly resizable?: boolean | null;
   /**
    * Enable sorting of a column
    */
-  readonly sortable?: boolean;
+  readonly sortable?: boolean | null;
   /**
    * Sets the column sort order to be descending instead of ascending the first time
    * the column is sorted
    */
-  readonly sortDescendingFirst?: boolean;
+  readonly sortDescendingFirst?: boolean | null;
+  /**
+   * Editor to be rendered when cell of column is being edited.
+   * If set, then the column is automatically set to be editable
+   */
+  readonly editor?: ((props: TableEditorProps<T>) => ReactNode) | null;
 };
 export type TableCopyEvent<T> = CopyEvent<T>;
+export type TableEditorProps<T> = {
+  column: TableCalculatedColumn<T>;
+  row: T;
+  onRowChange: (row: T, commitChanges?: boolean) => void;
+  onClose: (commitChanges?: boolean) => void;
+};
 export type TableFillEvent<T> = FillEvent<T>;
+export type TableFormatterProps<T> = FormatterProps<T>;
 export type TablePasteEvent<T> = PasteEvent<T>;
 export type TableRowsChangeData<T> = RowsChangeData<T>;
+export const TableSelectColumn = SelectColumn;
 export type TableSortColumn = SortColumn;
+export const TableTextEditor = textEditor;
 
 // Subset of https://github.com/adazzle/react-data-grid#api
-export type TableProps = {
+export type TableProps<T> = {
   /**
    * An array of objects representing each column on the grid
    */
-  columns: readonly TableColumn[];
+  columns: readonly TableColumn<T>[];
   /**
    * An array of objects representing the currently sorted columns
    */
@@ -113,15 +146,15 @@ export type TableProps = {
   /**
    * An array of objects representing the rows in the grid
    */
-  rows: readonly unknown[];
+  rows: readonly T[];
   /**
    * A function that returns the key for a given row
    */
-  rowKeyGetter?: (row: unknown) => Key;
+  rowKeyGetter?: (row: T) => Key;
   /**
    * A function that returns the class name for a given row
    */
-  rowClass?: (row: unknown) => string;
+  rowClassGetter?: (row: T) => string;
   /**
    * The height of each row in pixels
    * @default 35
@@ -143,27 +176,27 @@ export type TableProps = {
   /**
    * A callback function that is called when the rows in the grid change
    */
-  onRowsChange?: (rows: unknown[], data: TableRowsChangeData<unknown>) => void;
+  onRowsChange?: (rows: T[], data: TableRowsChangeData<T>) => void;
   /**
    * A callback function that is called when a cell is clicked
    * @param event The event source of the callback
    */
-  onCellClick?: (args: TableCellClickArgs<unknown>, event: TableCellMouseEvent) => void;
+  onCellClick?: (args: TableCellClickArgs<T>, event: TableCellMouseEvent) => void;
   /**
    * A callback function that is called when a cell is double-clicked
    * @param event The event source of the callback
    */
-  onCellDoubleClick?: (args: TableCellClickArgs<unknown>, event: TableCellMouseEvent) => void;
+  onCellDoubleClick?: (args: TableCellClickArgs<T>, event: TableCellMouseEvent) => void;
   /**
    * A callback function that is called when a cell is right-clicked
    * @param event The event source of the callback
    */
-  onCellContextMenu?: (args: TableCellClickArgs<unknown>, event: TableCellMouseEvent) => void;
+  onCellContextMenu?: (args: TableCellClickArgs<T>, event: TableCellMouseEvent) => void;
   /**
    * A callback function that is called when a key is pressed while a cell is focused
    * @param event The event source of the callback
    */
-  onCellKeyDown?: (args: TableCellKeyDownArgs<unknown>, event: TableCellKeyboardEvent) => void;
+  onCellKeyDown?: (args: TableCellKeyDownArgs<T>, event: TableCellKeyboardEvent) => void;
   /**
    * The text direction of the table
    * @default "ltr"
@@ -183,24 +216,24 @@ export type TableProps = {
    * A callback function that is called when the table is filled with data
    * @param event The event source of the callback
    */
-  onFill?: (event: TableFillEvent<unknown>) => unknown;
+  onFill?: (event: TableFillEvent<T>) => T;
   /**
    * A callback function that is called when the user copies data from the table.
    * @param event The event source of the callback
    */
-  onCopy?: (event: TableCopyEvent<unknown>) => void;
+  onCopy?: (event: TableCopyEvent<T>) => void;
   /**
    * A callback function that is called when the user pastes data into the table.
    * @param event The event source of the callback
    */
-  onPaste?: (event: TablePasteEvent<unknown>) => unknown;
+  onPaste?: (event: TablePasteEvent<T>) => T;
   /**
    * Additional css classes to help with unique styling of the table
    */
   className?: string;
 };
 
-function Table({
+function Table<T>({
   columns,
   sortColumns,
   onSortColumnsChange,
@@ -212,7 +245,7 @@ function Table({
   defaultColumnResizable = true,
   rows,
   rowKeyGetter,
-  rowClass,
+  rowClassGetter,
   rowHeight = 35,
   headerRowHeight = 35,
   selectedRows,
@@ -229,9 +262,9 @@ function Table({
   onPaste,
   onScroll,
   className,
-}: TableProps) {
+}: TableProps<T>) {
   return (
-    <DataGrid
+    <DataGrid<T>
       columns={columns}
       defaultColumnOptions={{
         width: defaultColumnWidth,
@@ -245,7 +278,7 @@ function Table({
       onColumnResize={onColumnResize}
       rows={rows}
       rowKeyGetter={rowKeyGetter}
-      rowClass={rowClass}
+      rowClass={rowClassGetter}
       rowHeight={rowHeight}
       headerRowHeight={headerRowHeight}
       selectedRows={selectedRows}
--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -1,31 +1,291 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryContext, StoryObj } from '@storybook/react';
+import { useArgs } from '@storybook/client-api';
 
-import Table from '@renderer/components/papi-components/Table';
+import Table, {
+  TableProps,
+  TableTextEditor,
+  TableSelectColumn,
+  TableSortColumn,
+} from '@renderer/components/papi-components/Table';
+import { ReactElement } from 'react';
 
-const meta: Meta<typeof Table> = {
+type Row = {
+  id: number;
+  title: string;
+};
+
+type Story = StoryObj<typeof Table<Row>>;
+
+function TableDecorator(
+  Story: (props: Partial<TableProps<Row>>) => ReactElement,
+  context: StoryContext,
+) {
+  const [args, updateArgs] = useArgs();
+
+  const setSortColumns = (cols: TableSortColumn[]) => {
+    updateArgs({ sortColumns: cols });
+  };
+
+  const setSelectedRows = (rows: Row[]) => {
+    updateArgs({ selectedRows: rows });
+  };
+
+  // const setRows = (rows: Row[]) => {
+  //   updateArgs({ rows });
+  // };
+
+  // const rows: Row[] = [
+  //   { id: 0, title: 'Lorem ipsum dolor sit amet' },
+  //   { id: 1, title: 'Consectetur adipiscing elit' },
+  //   { id: 2, title: 'Pellentesque suscipit tortor est' },
+  //   { id: 3, title: 'Ut egestas massa aliquam a' },
+  //   { id: 4, title: 'Nulla egestas vestibulum felis a venenatis' },
+  //   { id: 5, title: 'Sed aliquet pulvinar neque' },
+  // ];
+
+  // type Comparator = (a: Row, b: Row) => number;
+  // function getComparator(sortColumn: string): Comparator {
+  //   switch (sortColumn) {
+  //     case 'title':
+  //       return (a, b) => {
+  //         return a[sortColumn].localeCompare(b[sortColumn]);
+  //       };
+  //     case 'id':
+  //       return (a, b) => {
+  //         return a[sortColumn] - b[sortColumn];
+  //       };
+  //     default:
+  //       throw new Error(`unsupported sortColumn: "${sortColumn}"`);
+  //   }
+  // }
+
+  // const sortedRows = (): readonly Row[] => {
+  //   console.log('hi');
+  //   if (args.sortColumns.length === 0) return rows;
+
+  //   return [...rows].sort((a, b) => {
+  //     // eslint-disable-next-line no-restricted-syntax
+  //     for (const sort of args.sortColumns) {
+  //       const comparator = getComparator(sort.columnKey);
+  //       const compResult = comparator(a, b);
+  //       if (compResult !== 0) {
+  //         return sort.direction === 'ASC' ? compResult : -compResult;
+  //       }
+  //     }
+  //     return 0;
+  //   });
+  // };
+
+  return (
+    <Story
+      {...context}
+      args={{
+        ...args,
+        onSelectedRowsChange: setSelectedRows,
+        onSortColumnsChange: setSortColumns,
+        // onRowsChange: setRows,
+        // rows: sortedRows,
+      }}
+    />
+  );
+}
+
+const meta: Meta<typeof Table<Row>> = {
   title: 'Basics/Table',
-  component: Table,
+  component: Table<Row>,
   tags: ['autodocs'],
-  argTypes: {},
+  args: {
+    rowKeyGetter: (row: Row) => {
+      return row.id;
+    },
+  },
+  argTypes: {
+    defaultColumnWidth: { control: 'number' },
+    defaultColumnMinWidth: { control: 'number' },
+    defaultColumnMaxWidth: { control: 'number' },
+    defaultColumnSortable: { control: 'boolean' },
+    defaultColumnResizable: { control: 'boolean' },
+    rowHeight: { control: 'number' },
+    headerRowHeight: { control: 'number' },
+    direction: {
+      options: [0, 1],
+      mapping: ['ltr', 'rtl'],
+      control: {
+        type: 'select',
+        labels: ['LTR', 'RTL'],
+      },
+    },
+    enableVirtualization: { control: 'boolean' },
+    className: { control: 'text' },
+  },
+  decorators: [TableDecorator],
 };
 export default meta;
 
-type Story = StoryObj<typeof Table>;
-
-export const Primary: Story = {
+export const Default: Story = {
   args: {
     columns: [
-      { key: 'id', name: 'ID' },
-      { key: 'title', name: 'Title' },
+      {
+        key: 'id',
+        name: 'ID',
+      },
+      {
+        key: 'title',
+        name: 'Title',
+        editor: TableTextEditor,
+      },
+    ],
+    rows: [
+      { id: 0, title: 'Lorem ipsum dolor sit amet' },
+      { id: 1, title: 'Consectetur adipiscing elit' },
+      { id: 2, title: 'Pellentesque suscipit tortor est' },
+      { id: 3, title: 'Ut egestas massa aliquam a' },
+      { id: 4, title: 'Nulla egestas vestibulum felis a venenatis' },
+      { id: 5, title: 'Sed aliquet pulvinar neque' },
+    ],
+  },
+};
+
+export const CustomizedColumns: Story = {
+  args: {
+    columns: [
+      {
+        key: 'id',
+        name: 'ID',
+        width: 300,
+        minWidth: 200,
+        maxWidth: 400,
+        editable: true,
+        frozen: false,
+        resizable: true,
+        sortable: true,
+      },
+      {
+        key: 'title',
+        name: 'Title',
+        width: 300,
+        minWidth: 200,
+        maxWidth: 400,
+        editable: false,
+        frozen: false,
+        resizable: true,
+        sortable: true,
+      },
     ],
 
     rows: [
-      { id: 0, title: 'Example' },
-      { id: 1, title: 'Demo' },
+      { id: 0, title: 'Lorem ipsum dolor sit amet' },
+      { id: 1, title: 'Consectetur adipiscing elit' },
+      { id: 2, title: 'Pellentesque suscipit tortor est' },
+      { id: 3, title: 'Ut egestas massa aliquam a' },
+      { id: 4, title: 'Nulla egestas vestibulum felis a venenatis' },
+      { id: 5, title: 'Sed aliquet pulvinar neque' },
+    ],
+  },
+};
+
+export const ConfigureDefaultColumnSettings: Story = {
+  args: {
+    columns: [
+      {
+        key: 'id',
+        name: 'ID',
+      },
+      {
+        key: 'title',
+        name: 'Title',
+        editor: TableTextEditor,
+      },
     ],
 
-    // rowKeyGetter: (row: { id: number }) => {
-    //   return row.id;
-    // },
+    rows: [
+      { id: 0, title: 'Lorem ipsum dolor sit amet' },
+      { id: 1, title: 'Consectetur adipiscing elit' },
+      { id: 2, title: 'Pellentesque suscipit tortor est' },
+      { id: 3, title: 'Ut egestas massa aliquam a' },
+      { id: 4, title: 'Nulla egestas vestibulum felis a venenatis' },
+      { id: 5, title: 'Sed aliquet pulvinar neque' },
+    ],
+
+    defaultColumnWidth: 300,
+    defaultColumnMinWidth: 100,
+    defaultColumnMaxWidth: 500,
+    defaultColumnSortable: true, // Sorting columns doesn't seem to work yet
+    defaultColumnResizable: true,
+  },
+};
+
+export const ColumnCallBackFunctions: Story = {
+  args: {
+    columns: [
+      {
+        key: 'id',
+        name: 'ID (sortable)',
+        sortable: true,
+      },
+      {
+        key: 'title',
+        name: 'Title (not sortable)',
+        sortable: false,
+        editor: TableTextEditor,
+      },
+    ],
+
+    rows: [
+      { id: 0, title: 'Lorem ipsum dolor sit amet' },
+      { id: 1, title: 'Consectetur adipiscing elit' },
+      { id: 2, title: 'Pellentesque suscipit tortor est' },
+      { id: 3, title: 'Ut egestas massa aliquam a' },
+      { id: 4, title: 'Nulla egestas vestibulum felis a venenatis' },
+      { id: 5, title: 'Sed aliquet pulvinar neque' },
+    ],
+
+    onColumnResize: () => {
+      // eslint-disable-next-line no-console
+      console.log('Column resized');
+    },
+
+    onSortColumnsChange: (col) => {
+      console.log(col);
+      // eslint-disable-next-line no-console
+      console.log('SortColumns Changed');
+    },
+  },
+};
+
+export const RowCallBackFunctions: Story = {
+  args: {
+    columns: [
+      TableSelectColumn,
+      {
+        key: 'id',
+        name: 'ID',
+      },
+      {
+        key: 'title',
+        name: 'Title',
+        editor: TableTextEditor,
+      },
+    ],
+
+    rows: [
+      { id: 0, title: 'Lorem ipsum dolor sit amet' },
+      { id: 1, title: 'Consectetur adipiscing elit' },
+      { id: 2, title: 'Pellentesque suscipit tortor est' },
+      { id: 3, title: 'Ut egestas massa aliquam a' },
+      { id: 4, title: 'Nulla egestas vestibulum felis a venenatis' },
+      { id: 5, title: 'Sed aliquet pulvinar neque' },
+    ],
+
+    onSelectedRowsChange: () => {
+      console.log('test');
+    },
+
+    // onRowsChange,
+
+    // rowClassGetter,
+
+    rowHeight: 50,
+    headerRowHeight: 100,
   },
 };
