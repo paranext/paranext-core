8ea90a01a072f012fc3ff0079595fa91f8809b33
Katherine Jensen <katherinejensen00@gmail.com>
2023-04-25 07:59:44 -0700

    Finish first attempt at defining the get of the data handler, add a subscription
    (cherry picked from commit 36afb2d2c25f3d67bf86791eb3dc5e45e4149541)

--- extensions/lib/sneeze-board/sneeze-board.ts
+++ extensions/lib/sneeze-board/sneeze-board.ts
@@ -14,82 +14,84 @@ logger.info('Sneeze Board is importing!');
 const unsubscribers: UnsubscriberAsync[] = [];
 
 // TODO: Change date to have a Date type once JSON gets parsed to a Date type
-export type SneezeBoardData = { userId: string; date: string; comment?: string };
-// TODO: change date to a Date type once we deserialize the json into a Date type
-export type SneezeBoardDictionary = { [id: string]: { date: string; Comment?: string } };
+export type SerializedSneeze = { userId: string; date: string; comment?: string };
+export type Sneeze = SerializedSneeze & { sneezeId: number };
 
 class AchYouDataProviderEngine
-  implements IDataProviderEngine<string | SneezeBoardData, SneezeBoardDictionary, SneezeBoardData>
+  implements IDataProviderEngine<string | number | Date, Sneeze[], Sneeze>
 {
+  sneezes: Sneeze[];
+  startOfCountdown: number;
+  constructor() {
+    this.startOfCountdown = +blessYouData.CountdownStart;
+    this.sneezes = blessYouData.Sneezes.map((s) => {
+      // TODO: Should we actually disable the line below?
+      // eslint-disable-next-line no-plusplus
+      return { sneezeId: this.startOfCountdown--, ...s };
+    });
+    console.log(this.sneezes);
+  }
+
   // TODO: Define the set method
-  /**
-   * Sneezes stored by the Data Provider.
-   * Keys are User Ids.
-   * Values are { date: Date, Comment?: string }
-   */
-  displaySneezes: SneezeBoardDictionary = {};
 
   /**
    * @param selector
    */
-  get = async (selector: string | SneezeBoardData) => {
-    this.displaySneezes = {};
-
-    if (selector === '*')
-      this.displaySneezes = Object.fromEntries(
-        blessYouData.Sneezes.map(({ userId, ...rest }) => [userId, rest]),
-      );
+  get = async (selector: string | number | Date) => {
+    if (!selector) return [];
+    // TODO: Update blessyoudata to a data set with sneezes in it (bless you data has serializedSneezes only which don't have sneezeIds)
+    if (selector === '*') return this.sneezes;
     // Handle all string selectors so it can be assumed the following selectors are of type SneezeBoardData
-    if (typeof selector === 'string') return this.displaySneezes;
-
-    if (selector.userId) {
-      const filteredSneezes: SneezeBoardData[] = blessYouData.Sneezes.filter((sneeze) => {
-        return selector.userId === sneeze.userId;
+    if (typeof selector === 'string')
+      return this.sneezes.filter((sneeze) => {
+        return selector === sneeze.userId;
       });
-      this.displaySneezes = Object.fromEntries(
-        filteredSneezes.map(({ userId, ...rest }) => [userId, rest]),
-      );
-    }
-    // TODO: Come back and parse Json date into the date type instead of type string
-    // if (selector.date)
-    //   return blessYouData.Sneezes.filter((sneeze) => {
+
+    // TODO: Come back and parse Json date into the date type instead of type string and make this sortable by day, month, year etc.
+    // if (selector.date && typeOf date === 'Date')
+    //   return this.sneezes.filter((sneeze) => {
     //     return selector.date === sneeze.date;
     //   });
-    if (selector.comment) {
-      const filteredSneezes: SneezeBoardData[] = blessYouData.Sneezes.filter((sneeze) => {
-        return selector.comment === sneeze.comment;
+    if (typeof selector === 'number') {
+      return this.sneezes.filter((sneeze) => {
+        return selector === sneeze.sneezeId;
       });
-      this.displaySneezes = Object.fromEntries(
-        filteredSneezes.map(({ userId, ...rest }) => [userId, rest]),
-      );
     }
-    return this.displaySneezes;
+    return [];
   };
 }
 
 export async function activate() {
   logger.info('Sneeze Board is activating!');
 
-  const AchYouDataProviderEnginePromise = papi.dataProvider.registerEngine(
+  const AchYouDataProviderEngineInfo = await papi.dataProvider.registerEngine(
     'sneeze-board.sneezes',
     new AchYouDataProviderEngine(),
   );
-
-  // TODO: Add data provider interface that implements IDataProvider like the other extensions and
-  // then call the dispose method on the promise variable below instead of logging it
-  // eslint-disable-next-line no-console
-  console.log({ AchYouDataProviderEnginePromise });
 
   await papi.webViews.addWebView({
     componentName: 'SneezeBoard',
     contents: sneezeBoardReactWebView,
   });
 
+  const sneezeDataProvider = AchYouDataProviderEngineInfo?.dataProvider;
+
   const unsubPromises = [
     papi.commands.registerCommand('sneeze-board.get-sneezes', () => {
-      return `27002 Achoos`;
+      return sneezeDataProvider.get('*');
     }),
   ];
+
+  if (sneezeDataProvider) {
+    // Test subscribing to a data provider
+    const unsubGreetings = await sneezeDataProvider.subscribe(
+      'c897cd73-9100-4e6a-8a32-fe237f1e9928',
+      (timSneeze: Sneeze[]) =>
+        logger.info(`Tim sneezed the ${timSneeze[timSneeze.length - 1].sneezeId} sneeze`),
+    );
+
+    unsubscribers.push(unsubGreetings);
+  }
 
   return Promise.all(unsubPromises.map((unsubPromise) => unsubPromise.promise)).then(() => {
     logger.info('Sneeze Board is finished activating!');
