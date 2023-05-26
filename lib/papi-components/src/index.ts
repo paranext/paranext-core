84e2ff74198f94429411e4504b725e0c6d12f6ef
Ira Hopkinson <ira_hopkinson@sil.org>
2023-05-26 14:01:08 +1200

    Remove `papi.react.components`

--- lib/papi-components/src/index.ts
+++ lib/papi-components/src/index.ts
@@ -1,24 +1,6 @@
-import Button from './button.component';
-import Checkbox from './checkbox.component';
-import ComboBox from './combo-box.component';
-import Slider from './slider.component';
-import Switch from './switch.component';
-import TextField from './text-field.component';
-
-/** All React components to be exposed on the papi */
-export const papiComponents = {
-  Button,
-  Checkbox,
-  ComboBox,
-  Slider,
-  Switch,
-  TextField,
-};
-
-export type PapiComponents = typeof papiComponents;
-
 export { default as Button } from './button.component';
 export { default as Checkbox } from './checkbox.component';
+export { default as LabelPosition } from './label-position.model';
 export { default as ComboBox } from './combo-box.component';
 export { default as RefSelector } from './ref-selector.component';
 export type { ScrRefSelectorProps } from './ref-selector.component';
