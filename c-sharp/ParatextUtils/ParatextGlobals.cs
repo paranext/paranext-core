42bc80f9dd69a12c2b4f824df3aece71257350f3
Matt Lyons <matt_lyons@sil.org>
2023-06-21 17:54:34 -0500

    Add a PAPI data provider that reads scripture using ParatextData

---
+++ c-sharp/ParatextUtils/ParatextGlobals.cs
@@ -0,0 +1,39 @@
+using System.Text;
+using Paratext.Data;
+using PtxUtils;
+
+namespace Paranext.DataProvider.ParatextUtils
+{
+    internal static class ParatextGlobals
+    {
+        private static readonly object s_locker = new();
+        private static bool s_initialized = false;
+
+        public static void Initialize(string dataFolder)
+        {
+            // Paratext not supported on MacOS for now
+            if (OperatingSystem.IsMacOS())
+                return;
+
+            lock (s_locker)
+            {
+                if (s_initialized)
+                    return;
+
+                // Required for the Paratext.Data.Encodings.StringEncoders static constructor
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                // Required for non-Windows platforms
+                Alert.Implementation = new AlertStub();
+                RegistryU.Implementation = new RegistryStub();
+
+                // Required for ICU.NET
+                ICUDllLocator.Initialize(false, false);
+
+                // Now tell Paratext.Data to use the specified folder
+                ParatextData.Initialize(dataFolder, false);
+                s_initialized = true;
+            }
+        }
+    }
+}
