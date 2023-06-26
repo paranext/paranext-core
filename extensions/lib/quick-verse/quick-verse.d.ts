3712e73cc100794bc2b9462dab280b79a83d74d4
tjcouch-sil <tj_couch@sil.org>
2023-06-26 12:37:45 -0500

    Converted quick-verse types to module

--- extensions/lib/quick-verse/quick-verse.d.ts
+++ extensions/lib/quick-verse/quick-verse.d.ts
@@ -3,12 +3,14 @@ import type IDataProvider from 'shared/models/data-provider.interface';
 
 // TODO: Move these types to quick-verse.ts and generate this file from quick-verse.ts in the future?
 
-export type QuickVerseSetData = string | { text: string; isHeresy: boolean };
+declare module 'quick-verse' {
+  export type QuickVerseSetData = string | { text: string; isHeresy: boolean };
 
-export type QuickVerseDataTypes = {
-  Verse: DataProviderDataType<string, string | undefined, QuickVerseSetData>;
-  Heresy: DataProviderDataType<string, string | undefined, string>;
-  Chapter: DataProviderDataType<[book: string, chapter: number], string | undefined, never>;
-};
+  export type QuickVerseDataTypes = {
+    Verse: DataProviderDataType<string, string | undefined, QuickVerseSetData>;
+    Heresy: DataProviderDataType<string, string | undefined, string>;
+    Chapter: DataProviderDataType<[book: string, chapter: number], string | undefined, never>;
+  };
 
-export type QuickVerseDataProvider = IDataProvider<QuickVerseDataTypes>;
+  export type QuickVerseDataProvider = IDataProvider<QuickVerseDataTypes>;
+}
