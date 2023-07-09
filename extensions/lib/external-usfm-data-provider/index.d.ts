062629d300730c2446bc6b645b41515e61338410
Ira Hopkinson <ira_hopkinson@sil.org>
2023-07-10 08:46:22 +1200

    Use `@sillsdev/scripture`
    - also lower-case cSpell dictionary since it ignores case

--- extensions/lib/external-usfm-data-provider/index.d.ts
+++ extensions/lib/external-usfm-data-provider/index.d.ts
@@ -1,45 +1,6 @@
+import { VerseRef } from '@sillsdev/scripture';
 import type { DataProviderDataType } from 'shared/models/data-provider.model';
 import type IDataProvider from 'shared/models/data-provider.interface';
-
-// An enum might be nice here, but they don't serialize into messages properly
-export type Versification =
-  | 'Unknown'
-  | 'Original'
-  | 'Septuagint'
-  | 'Vulgate'
-  | 'English'
-  | 'RussianProtestant'
-  | 'RussianOrthodox';
-
-export type TextualVerseRef = {
-  verseString: string;
-  versification?: Versification;
-};
-
-export type NumericVerseRef = {
-  bookChapterVerse: number;
-  versification?: Versification;
-};
-
-export type DetailedNumericalVerseRef = {
-  book: number;
-  chapter: number;
-  verse: number;
-  versification?: Versification;
-};
-
-export type DetailedTextualVerseRef = {
-  book: string;
-  chapter: string;
-  verse: string;
-  versification: Versification;
-};
-
-export type VerseRef =
-  | NumericVerseRef
-  | TextualVerseRef
-  | DetailedNumericalVerseRef
-  | DetailedTextualVerseRef;
 
 export type UsfmProviderDataTypes = {
   BookNames: DataProviderDataType<boolean, string[], never>;
