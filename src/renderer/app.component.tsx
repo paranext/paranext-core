587939d7f321b3295a20a91b86b674f3bcbada39
Ira Hopkinson <irahopkinson@users.noreply.github.com>
2023-07-18 01:19:02 +1200

    Fix dock height (#296)
    - also move toolbar to app component

--- src/renderer/app.component.tsx
+++ src/renderer/app.component.tsx
@@ -2,10 +2,12 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './app.component.css';
 import ParanextDockLayout from '@renderer/components/docking/paranext-dock-layout.component';
 import TestContext from '@renderer/context/papi-context/test.context';
+import PlatformBibleToolbar from './components/platform-bible-toolbar';
 
-function Hello() {
+function Main() {
   return (
     <TestContext.Provider value="test">
+      <PlatformBibleToolbar />
       <ParanextDockLayout />
     </TestContext.Provider>
   );
@@ -15,7 +17,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
