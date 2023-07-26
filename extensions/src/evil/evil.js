cbe8dff65c8412580add0123400a0d93a78e2cc7
tjcouch-sil <tj_couch@sil.org>
2023-07-26 10:37:41 -0500

    Clarified when evil console logs are actual problems

--- extensions/src/evil/evil.js
+++ extensions/src/evil/evil.js
@@ -12,7 +12,7 @@ async function tryImports() {
   try {
     // This will be blocked and will suggest the papi.storage api.
     const fs = require('fs');
-    logger.error(`Evil: Successfully imported fs! fs.readFileSync = ${fs.readFileSync}`);
+    logger.error(`Evil: <<BAD>> Successfully imported fs! fs.readFileSync = ${fs.readFileSync}`);
   } catch (e) {
     logger.info(`Evil: Good error on require fs: ${e.message}`);
   }
@@ -20,7 +20,7 @@ async function tryImports() {
   try {
     // This will be blocked and will suggest the papi.fetch api.
     const https = require('https');
-    logger.error(`Evil: Successfully imported https! ${JSON.stringify(https)}`);
+    logger.error(`Evil: <<BAD>> Successfully imported https! ${JSON.stringify(https)}`);
   } catch (e) {
     logger.info(`Evil: Good error on require https: ${e.message}`);
   }
@@ -28,16 +28,16 @@ async function tryImports() {
   try {
     // This should always work because `fetch` is replaced with `papi.fetch`.
     await fetch('https://www.example.com');
-    logger.info('Evil: fetch is working.');
+    logger.info('Evil: Good - fetch is working.');
   } catch (e) {
-    logger.error(`Evil: Bad error on fetch! ${e}`);
+    logger.error(`Evil: <<BAD>> error on fetch! ${e}`);
   }
 
   try {
     // This is just for testing and will throw an exception.
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const xhr = new XMLHttpRequest();
-    logger.error(`Evil: Successfully created an XMLHttpRequest!`);
+    logger.error(`Evil: <<BAD>> Successfully created an XMLHttpRequest!`);
   } catch (e) {
     logger.info(`Evil: Good error on XMLHttpRequest! ${e}`);
   }
@@ -46,7 +46,7 @@ async function tryImports() {
     // This is just for testing and will throw an exception.
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const webSocket = new WebSocket();
-    logger.error(`Evil: Successfully created a WebSocket!`);
+    logger.error(`Evil: <<BAD>> Successfully created a WebSocket!`);
   } catch (e) {
     logger.info(`Evil: Good error on WebSocket! ${e}`);
   }
@@ -55,7 +55,7 @@ async function tryImports() {
     // This will be blocked and will suggest the papi.storage api.
     const fs = await import('fs');
     logger.error(
-      `Evil: Successfully dynamically imported fs! fs.readFileSync = ${fs.readFileSync}`,
+      `Evil: <<BAD>> Successfully dynamically imported fs! fs.readFileSync = ${fs.readFileSync}`,
     );
   } catch (e) {
     logger.info(`Evil: Good error on dynamic import! ${e.message}`);
@@ -64,9 +64,11 @@ async function tryImports() {
   try {
     // This should always work.
     const genericFetch = await (await papi.fetch('https://www.example.com')).text();
-    logger.info(`Evil: Success - could papi.fetch example.com "${genericFetch.substring(0, 100)}"`);
+    logger.info(
+      `Evil: Good success - could papi.fetch example.com "${genericFetch.substring(0, 100)}"`,
+    );
   } catch (e) {
-    logger.error(`Evil: Bad error on papi.fetch! ${e}`);
+    logger.error(`Evil: <<BAD>> error on papi.fetch! ${e}`);
   }
 }
 
