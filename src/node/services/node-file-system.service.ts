f2ce2bbeb893272f3d5eb12684aace4467d05516
Matt Lyons <matt_lyons@sil.org>
2023-08-11 13:05:09 -0500

    Load extensions from ZIP files (#349)

--- src/node/services/node-file-system.service.ts
+++ src/node/services/node-file-system.service.ts
@@ -2,15 +2,15 @@
  * File system calls from Node
  */
 
-import fs from 'fs';
+import fs, { BigIntStats } from 'fs';
 import path from 'path';
 import { Uri } from '@shared/data/file-system.model';
 import { getPathFromUri, joinUriPaths } from '@node/utils/util';
 import { groupBy } from '@shared/utils/util';
 
 /**
- * Reads a text file asynchronously
- * @param uri Uri of file
+ * Read a text file
+ * @param uri URI of file
  * @returns promise that resolves to the contents of the file
  */
 export async function readFileText(uri: Uri): Promise<string> {
@@ -18,8 +18,8 @@ export async function readFileText(uri: Uri): Promise<string> {
 }
 
 /**
- * Reads a binary file asynchronously
- * @param uri Uri of file
+ * Read a binary file
+ * @param uri URI of file
  * @returns promise that resolves to the contents of the file
  */
 export async function readFileBinary(uri: Uri): Promise<Buffer> {
@@ -27,20 +27,49 @@ export async function readFileBinary(uri: Uri): Promise<Buffer> {
 }
 
 /**
- * Writes the string to a file asynchronously
- * @param uri Uri of file
- * @param fileContents string to write into the file
+ * Write data to a file
+ * @param uri URI of file
+ * @param fileContents string or Buffer to write into the file
  * @returns promise that resolves after writing the file
  */
-export async function writeFileText(uri: Uri, fileContents: string): Promise<void> {
+export async function writeFile(uri: Uri, fileContents: string | Buffer): Promise<void> {
   const filePath: string = getPathFromUri(uri);
   const directoryName: string = path.dirname(filePath);
   await fs.promises.mkdir(directoryName, { recursive: true });
   return fs.promises.writeFile(filePath, fileContents);
 }
 
+/**
+ * Delete a file if it exists
+ * @param uri URI of file
+ * @returns promise that resolves when the file is deleted or determined to not exist
+ */
 export async function deleteFile(uri: Uri): Promise<void> {
-  return fs.promises.rm(getPathFromUri(uri));
+  const stats = await getStats(uri);
+  if (stats && stats.isFile()) await fs.promises.rm(getPathFromUri(uri));
+}
+
+/**
+ * Get stats about the file or directory. Note that BigInts are used instead of ints to avoid.
+ * https://en.wikipedia.org/wiki/Year_2038_problem
+ * @param uri URI of file or directory
+ * @returns Promise that resolves to object of type https://nodejs.org/api/fs.html#class-fsstats if file or directory exists, undefined if it doesn't
+ */
+export async function getStats(uri: Uri): Promise<BigIntStats | undefined> {
+  try {
+    return await fs.promises.stat(getPathFromUri(uri), { bigint: true });
+  } catch (error) {
+    return undefined;
+  }
+}
+
+/**
+ * Set the last modified and accessed times for the file or directory
+ * @param uri URI of file or directory
+ * @returns Promise that resolves once the touch operation finishes
+ */
+export async function touch(uri: Uri, date: Date): Promise<void> {
+  await fs.promises.utimes(getPathFromUri(uri), date, date);
 }
 
 /** Type of file system item in a directory */
@@ -57,7 +86,7 @@ export type DirectoryEntries = Readonly<{
 
 /**
  * Reads a directory and returns lists of entries in the directory by entry type
- * @param uri uri of directory
+ * @param uri URI of directory
  * @param entryFilter function to filter out entries in the directory based on their names
  * @returns map of entry type to list of uris for each entry in the directory with that type
  */
@@ -65,6 +94,8 @@ export async function readDir(
   uri: Uri,
   entryFilter?: (entryName: string) => boolean,
 ): Promise<DirectoryEntries> {
+  const stats = await getStats(uri);
+  if (!stats || !stats.isDirectory()) return <DirectoryEntries>{};
   const unfilteredDirEntries = await fs.promises.readdir(getPathFromUri(uri), {
     withFileTypes: true,
   });
@@ -82,4 +113,25 @@ export async function readDir(
       (dirent) => joinUriPaths(uri, dirent.name),
     ),
   ) as DirectoryEntries;
+}
+
+/**
+ * Create a directory in the file system
+ * @param uri URI of directory
+ * @returns Promise that resolves once the directory has been created
+ */
+export async function createDir(uri: Uri): Promise<void> {
+  const dirPath: string = getPathFromUri(uri);
+  await fs.promises.mkdir(dirPath, { recursive: true });
+}
+
+/**
+ * Remove a directory and all its contents recursively from the file system
+ * @param uri URI of directory
+ * @returns Promise that resolves when the delete operation finishes
+ */
+export async function deleteDir(uri: Uri): Promise<void> {
+  const stats = await getStats(uri);
+  if (!stats || !stats.isDirectory()) return;
+  await fs.promises.rm(getPathFromUri(uri), { recursive: true, maxRetries: 1 });
 }
