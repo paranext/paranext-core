23a48d18eff73c99f09595109dd94622f3fb1ee8
rolfheij-sil <rolf_heij@sil.org>
2023-08-21 15:29:32 -0400

    Update comments

--- src/renderer/hooks/papi-hooks/use-setting.hook.ts
+++ src/renderer/hooks/papi-hooks/use-setting.hook.ts
@@ -3,17 +3,23 @@ import settingsService from '@shared/services/settings.service';
 import { SettingNames, SettingTypes } from 'papi-shared-types';
 
 /**
- * Gets and sets a setting on the papi. Also notifies subscribers when the setting changes.
+ * Gets and sets a setting on the papi. Also notifies subscribers when the setting changes and gets
+ * updated when the setting is changed by others.
+ *
  * Setting the value to `null` is the equivalent of deleting the setting.
  * @param key The string id that is used to store the setting in local storage
  *
- *    WARNING: MUST BE STABLE - const or wrapped in useState, useMemo, etc. The reference must not be updated every render
- * @param defaultState The default state of the setting. If the setting already has a value set to it in local storage, this parameter will be ignored.
+ *    WARNING: MUST BE STABLE - const or wrapped in useState, useMemo, etc. The reference must not
+ *    be updated every render
+ * @param defaultState The default state of the setting. If the setting already has a value set to
+ * it in local storage, this parameter will be ignored.
  *
- *    WARNING: MUST BE STABLE - const or wrapped in useState, useMemo, etc. The reference must not be updated every render
+ *    WARNING: MUST BE STABLE - const or wrapped in useState, useMemo, etc. The reference must not
+ *    be updated every render
  *
  * @returns [setting, setSetting]
- *  - `setting`: The current state of the setting, either the defaultState or the stored state on the papi, if any
+ *  - `setting`: The current state of the setting, either the defaultState or the stored state on
+ *    the papi, if any
  *  - `setSetting`: Function that updates the setting to a new value
  */
 const useSetting = <SettingName extends SettingNames>(
