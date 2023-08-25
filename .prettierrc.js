fc6a408c35b1d94f6687f823c475a8513f0b63cf
rolfheij-sil <rolf_heij@sil.org>
2023-08-25 16:43:02 -0400

    Cleaning up some more

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+// Please keep this file in sync with the .prettierrc.js file in the paranext-extension-template repo
+module.exports = {
+  tabWidth: 2,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  singleQuote: true,
+};
