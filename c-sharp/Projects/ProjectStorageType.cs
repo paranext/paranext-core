d1816ad8db267e0cfa99852cb7322bdabcfd0101
Matt Lyons <matt_lyons@sil.org>
2023-09-21 10:33:11 -0500

    Convert ProjectStorageType from an enum to a string for extensibility

--- c-sharp/Projects/ProjectStorageType.cs
+++ c-sharp/Projects/ProjectStorageType.cs
@@ -4,50 +4,9 @@ namespace Paranext.DataProvider.Projects;
 /// Structure of the data in persisted storage.
 /// This is not related to the format of the data itself but the way it is stored
 /// </summary>
-public enum ProjectStorageType
+public static class ProjectStorageType
 {
-    Unknown,
-    ParatextFolders,
-    ParatextZipped
-}
-
-// Add an extension methods related to the enum
-public static class ProjectStorageTypeExtensions
-{
-    /// <summary>
-    /// Convert a ProjectStorageType into a value recognized by PAPI
-    /// </summary>
-    /// <param name="projectStorageType">Enum value</param>
-    /// <returns>String that represents the enum value that can be passed over the network</returns>
-    public static string ToSerializedString(this ProjectStorageType projectStorageType)
-    {
-        return projectStorageType switch
-        {
-            ProjectStorageType.Unknown => "unknown",
-            ProjectStorageType.ParatextFolders => "paratextFolders",
-            ProjectStorageType.ParatextZipped => "paratextZipped",
-            _ => throw new Exception("Unexpected project storage type: " + projectStorageType)
-        };
-    }
-
-    /// <summary>
-    /// Convert a string from PAPI into a ProjectStorageType enum value
-    /// </summary>
-    /// <param name="input">string representing a ProjectStorageType value</param>
-    /// <param name="output">ProjectStorageType enum value</param>
-    public static void FromSerializedString(this string input, out ProjectStorageType output)
-    {
-        switch (input)
-        {
-            case "paratextFolders":
-                output = ProjectStorageType.ParatextFolders;
-                return;
-            case "paratextZipped":
-                output = ProjectStorageType.ParatextZipped;
-                return;
-            default:
-                output = ProjectStorageType.Unknown;
-                return;
-        }
-    }
+    public const string Unknown = "unknown";
+    public const string ParatextFolders = "paratextFolders";
+    public const string ParatextZipped = "paratextZipped";
 }
--- c-sharp/Projects/ProjectType.cs
+++ c-sharp/Projects/ProjectType.cs
@@ -3,7 +3,7 @@ namespace Paranext.DataProvider.Projects;
 /// <summary>
 /// Format of main (Project Data Provider, not extension) data in a project
 /// </summary>
-internal static class ProjectType
+public static class ProjectType
 {
     public const string Paratext = "ParatextStandard";
 }
