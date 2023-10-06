2f783c6db3efcba53a2bab6086892c7ebab574a4
tombogle <tom_bogle@sil.org>
2023-10-06 10:33:57 -0400

    #363: created useProjectDataProvider hook

--- src/renderer/hooks/papi-hooks/use-project-data-provider.hook.ts
+++ src/renderer/hooks/papi-hooks/use-project-data-provider.hook.ts
@@ -1,48 +1,49 @@
-import dataProviderService from '@shared/services/data-provider.service';
-import IDataProvider from '@shared/models/data-provider.interface';
+import projectDataProviderService from '@shared/services/data-provider.service';
+import IProjectDataProvider from '@shared/models/data-provider.interface';
 import { useCallback, useMemo, useState } from 'react';
 import useEvent from '@renderer/hooks/papi-hooks/use-event.hook';
 import usePromise from '@renderer/hooks/papi-hooks/use-promise.hook';
 import { isString } from '@shared/utils/util';
 
 /**
- * Gets a data provider with specified provider name
- * @param dataProviderSource string name of the data provider to get OR dataProvider (result of
- *  useDataProvider, if you want this hook to just return the data provider again)
- * @returns undefined if the data provider has not been retrieved,
- *  data provider if it has been retrieved and is not disposed,
- *  and undefined again if the data provider is disposed
+ * Gets a project data provider with specified provider name
+ * @param dataProviderSource string name of the data provider to get OR projectDataProvider (result
+ * of useProjectDataProvider, if you want this hook to just return the data provider again)
+ * @returns undefined if the project data provider has not been retrieved, the requested project
+ *  data provider if it has been retrieved and is not disposed, and undefined again if the project
+ *  data provider is disposed
  *
- * @type `T` - the type of data provider to return. Use `IDataProvider<TDataProviderDataTypes>`,
- *  specifying your own types, or provide a custom data provider type
+ * @type `T` - the type of project data provider to return. Use
+ *  `IProjectDataProvider<TProjectDataProviderDataTypes>`, specifying your own types, or provide a
+ *  custom project data provider type
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-function useDataProvider<T extends IDataProvider<any>>(
+function useProjectDataProvider<T extends IProjectDataProvider<any>>(
   dataProviderSource: string | T | undefined,
 ): T | undefined {
   // Check to see if they passed in the results of a useDataProvider hook or undefined
   const didReceiveDataProvider = !isString(dataProviderSource);
 
-  // Get the data provider for this data provider name
-  // Note: do nothing if we already received a data provider, but still run this hook.
+  // Get the requested project data provider
+  // Note: do nothing if we have already received a project data provider, but still run this hook.
   // (We must make sure to run the same number of hooks in all code paths.)
   const [dataProvider] = usePromise(
     useMemo(() => {
       return didReceiveDataProvider
-        ? // We already have a data provider or undefined, so we don't need to run this promise
+        ? // We already have a project data provider or undefined, so we don't need to run this promise
           undefined
         : async () =>
-            // We have the data provider's type, so we need to get the provider
+            // We have the project data provider's type, so we need to get the provider
             dataProviderSource
               ? // Type assert here - the user of this hook must make sure to provide the correct type
-                (dataProviderService.get(dataProviderSource) as Promise<T | undefined>)
+                (projectDataProviderService.get(dataProviderSource) as Promise<T | undefined>)
               : undefined;
     }, [didReceiveDataProvider, dataProviderSource]),
     undefined,
   );
 
-  // Disable this hook when the data provider is disposed
-  // Note: do nothing if we already received a data provider, but still run this hook.
+  // Disable this hook when the project data provider is disposed
+  // Note: do nothing if we already received a project data provider, but still run this hook.
   // (We must make sure to run the same number of hooks in all code paths.)
   const [isDisposed, setIsDisposed] = useState<boolean>(false);
   useEvent(
@@ -57,4 +58,4 @@ function useDataProvider<T extends IDataProvider<any>>(
   return dataProvider && !isDisposed ? dataProvider : undefined;
 }
 
-export default useDataProvider;
+export default useProjectDataProvider;
