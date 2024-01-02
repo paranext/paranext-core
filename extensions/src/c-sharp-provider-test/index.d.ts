83b3243776ed5f73e7b90b9a84733850b2f2c29f
tjcouch-sil <tj_couch@sil.org>
2024-01-02 15:00:06 -0600

    Added extensions that are just copied directly to dist, fixed eslint errors in extensions dir

---
+++ extensions/src/c-sharp-provider-test/index.d.ts
@@ -0,0 +1,21 @@
+declare module 'c-sharp-provider-test' {
+  import { IDataProvider, DataProviderDataType } from '@papi/core';
+
+  type TimeDataTypes = {
+    Time: DataProviderDataType<undefined, string | undefined, never>;
+  };
+
+  export type TimeDataProvider = IDataProvider<TimeDataTypes>;
+}
+
+declare module 'papi-shared-types' {
+  import type { TimeDataProvider } from 'c-sharp-provider-test';
+
+  export interface CommandHandlers {
+    'test.addOne': (num: number) => number;
+  }
+
+  export interface DataProviders {
+    'current-time': TimeDataProvider;
+  }
+}
