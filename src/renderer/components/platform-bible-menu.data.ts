d492ccf9a48950513137ffd2ef52703fc7809af9
tjcouch-sil <tj_couch@sil.org>
2024-01-03 16:14:36 -0600

    Accommodate rename paratextDotBible to paratextBible

--- src/renderer/components/platform-bible-menu.data.ts
+++ src/renderer/components/platform-bible-menu.data.ts
@@ -27,12 +27,12 @@ const standardMenuLayout: GridMenuInfo = {
         },
         {
           name: 'Open Text Collection...',
-          command: 'paratextDotBibleTextCollection.open',
+          command: 'paratextBibleTextCollection.open',
           hasDivider: true,
         },
         {
           name: 'Open Word List...',
-          command: 'paratextDotBibleWordList.open',
+          command: 'paratextBibleWordList.open',
           hasDivider: true,
         },
         {
@@ -100,12 +100,12 @@ export const supportAndDevelopmentMenuLayout: GridMenuInfo = {
         },
         {
           name: 'Open Text Collection...',
-          command: 'paratextDotBibleTextCollection.open',
+          command: 'paratextBibleTextCollection.open',
           hasDivider: true,
         },
         {
           name: 'Open Word List...',
-          command: 'paratextDotBibleWordList.open',
+          command: 'paratextBibleWordList.open',
           hasDivider: true,
         },
         {
