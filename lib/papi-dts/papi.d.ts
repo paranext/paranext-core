132efd66a82e721a94c4d4c22bb76efe7ddd562e
Matt Lyons <matt_lyons@sil.org>
2024-01-09 13:13:38 -0600

    PR feedback

--- lib/papi-dts/papi.d.ts
+++ lib/papi-dts/papi.d.ts
@@ -1937,23 +1937,24 @@ declare module 'shared/models/network-object.model' {
     id: string,
     networkObjectPromise: Promise<NetworkObject<T>>,
   ) => Partial<NetworkableObject>;
-  /**
-   * Data about an object shared on the network
-   *
-   * @param id ID of the network object that processes use to reference it
-   * @param objectType Name of the type of this network object. Note this isn't about TypeScript
-   *   types, but instead focused on the platform data model. Names of types for the same logical
-   *   thing (e.g., Project Data Providers => `pdp`) should be the same across all process on the
-   *   network regardless of what programming language they use. For generic network objects,
-   *   `networkObject` is appropriate.
-   * @param functionNames Array of strings with the function names exposed on this network object
-   * @param attributes Optional object containing properties that describe this network object. The
-   *   properties associated with this network object depend on the `objectType`.
-   */
+  /** Data about an object shared on the network */
   export type NetworkObjectDetails = {
+    /** ID of the network object that processes use to reference it */
     id: string;
+    /**
+     * Name of the type of this network object. Note this isn't about TypeScript types, but instead
+     * focused on the platform data model. Names of types for the same logical thing (e.g., Project
+     * Data Providers => `pdp`) should be the same across all process on the network regardless of
+     * what programming language they use. For generic network objects, `networkObject` is
+     * appropriate.
+     */
     objectType: string;
+    /** Array of strings with the function names exposed on this network object */
     functionNames: string[];
+    /**
+     * Optional object containing properties that describe this network object. The properties
+     * associated with this network object depend on the `objectType`.
+     */
     attributes?: Record<string, unknown>;
   };
 }
