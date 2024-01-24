63e9798ff5f64e4bec0f3bbf8af574fd94879b9b
Ira Hopkinson <ira_hopkinson@sil.org>
2024-01-25 08:41:53 +1300

    fix notarization: use APPLE_APP_SPECIFIC_PASSWORD instead of APPLE_ID_PASS for authentication (#3491)

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -12,8 +12,10 @@ exports.default = async function notarizeMacos(context) {
     return;
   }
 
-  if (!('APPLE_ID' in process.env && 'APPLE_ID_PASS' in process.env)) {
-    console.warn('Skipping notarizing step. APPLE_ID and APPLE_ID_PASS env variables must be set');
+  if (!('APPLE_ID' in process.env && 'APPLE_APP_SPECIFIC_PASSWORD' in process.env)) {
+    console.warn(
+      'Skipping notarizing step. APPLE_ID and APPLE_APP_SPECIFIC_PASSWORD env variables must be set',
+    );
     return;
   }
 
@@ -23,6 +25,6 @@ exports.default = async function notarizeMacos(context) {
     appBundleId: build.appId,
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_ID_PASS,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
   });
 };
