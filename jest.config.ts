af6110727f651d695bfddd36c451b23068503ec8
Ira Hopkinson <ira_hopkinson@sil.org>
2024-01-25 21:25:21 +1300

    update packages
    - most major updates
    - minor & patch updates
    - ensure `volta` is extended to each `package.json`
    - remove `package-lock.json` files from each extension
    - "eslint-config-erb@4.10.0" caused an issue (might be fixed if we update "@typescript-eslint/*" to v6+)
    - update linting
    
    - delete `node_modules/electron` before `npm i`

--- jest.config.ts
+++ jest.config.ts
@@ -11,6 +11,7 @@ const {
 const config: Config = {
   moduleDirectories: ['node_modules', 'release/app/node_modules', 'src'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  modulePathIgnorePatterns: ['<rootDir>/extensions/dist', '<rootDir>/release'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/.erb/mocks/file.mock.js',
