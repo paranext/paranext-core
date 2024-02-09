2c4bc8120767d84648b784ea04459648ea92f8f1
tjcouch-sil <tj_couch@sil.org>
2024-02-09 16:59:08 -0600

    Updated Uri scheme documentation

--- src/shared/data/file-system.model.ts
+++ src/shared/data/file-system.model.ts
@@ -4,10 +4,21 @@
  * Represents a path in file system or other. Has a scheme followed by :// followed by a relative
  * path. If no scheme is provided, the app scheme is used. Available schemes are as follows:
  *
- * - `app://` - goes to the app's home directory and into `.platform.bible` (platform-dependent)
+ * - `app://` - goes to the `.platform.bible` directory inside the user's home directory.
+ *
+ *   - On Linux and Mac, this is `$HOME/.platform.bible`
+ *   - On Windows, this is `%USERPROFILE%/.platform.bible`
+ *   - Note: In development, `app://` always goes to `paranext-core/dev-appdata`
  * - `cache://` - goes to the app's temporary file cache at `app://cache`
  * - `data://` - goes to the app's data storage location at `app://data`
- * - `resources://` - goes to the resources directory installed in the app
- * - `file://` - an absolute file path from root
+ * - `resources://` - goes to the `resources` directory inside the install directory
+ *
+ *   - Note: In development, `resources://` always goes to the repo root, `paranext-core`. Not all files
+ *       are copied into the production `resources` folder, though. See `electron-builder.json5`'s
+ *       `extraResources` for some that are copied.
+ * - `file://` - an absolute file path from drive root
+ *
+ * Note: projects are stored in the production version of `app://projects` regardless of whether you
+ * are in production or development
  */
 export type Uri = string;
