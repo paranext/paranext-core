ffe368c4d4293d0f0a3cb14f298e562a2d77f013
tjcouch-sil <tj_couch@sil.org>
2024-02-15 11:33:27 -0600

    Added a link about booksPresent setting

--- extensions/src/usfm-data-provider/index.d.ts
+++ extensions/src/usfm-data-provider/index.d.ts
@@ -391,7 +391,9 @@ declare module 'papi-shared-types' {
     'platformScripture.versification': number;
     /**
      * Which books are present in this Scripture project. Represented as a string with 0 or 1 for
-     * each possible book by standardized book code (123 characters long)
+     * each possible book by [standardized book
+     * code](https://github.com/sillsdev/libpalaso/blob/master/SIL.Scripture/Canon.cs#L226) (123
+     * characters long)
      *
      * @example
      * '100111000000000000110000001000000000010111111111111111111111111111000000000000000000000000000000000000000000100000000000000'
