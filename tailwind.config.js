f41adbd7c4cb7a4be57412f6908468f93469d622
Rolf Heij <rolf_heij@sil.org>
2024-02-22 13:53:44 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+  ] /* These are the default directories, copied from the installation instructions.
+  These folders do not exist in our project */,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
