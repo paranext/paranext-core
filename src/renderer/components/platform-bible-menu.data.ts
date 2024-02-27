499ed15d7384aa8f830b96e6c1f118b03453e013
Matt Lyons <matt_lyons@sil.org>
2024-02-27 13:31:53 -0600

    Remove unused menu items

--- src/renderer/components/platform-bible-menu.data.ts
+++ src/renderer/components/platform-bible-menu.data.ts
@@ -6,21 +6,6 @@ const standardMenuLayout: GridMenuInfo = {
       name: 'Project',
       items: [
         {
-          name: 'Open',
-          command: 'platform.openProjectDialog',
-          hasDivider: true,
-        },
-        {
-          name: 'Download/Update Project',
-          command: 'platform.openDownloadUpdateProjectDialog',
-          hasDivider: true,
-        },
-        {
-          name: 'Download/Install Resources...',
-          command: 'platform.downloadAndInstallResources',
-          hasDivider: true,
-        },
-        {
           name: 'Open Resource Viewer...',
           command: 'resourceViewer.open',
           hasDivider: true,
@@ -36,41 +21,8 @@ const standardMenuLayout: GridMenuInfo = {
           hasDivider: true,
         },
         {
-          name: 'Open Hello World Project...',
-          command: 'helloWorld.openProject',
-          hasDivider: true,
-        },
-        {
-          name: 'Settings...',
-          command: 'platform.settings',
-          hasDivider: true,
-        },
-        {
           name: 'Exit',
           command: 'platform.quit',
-          hasDivider: true,
-        },
-      ],
-    },
-    {
-      name: 'Window',
-      items: [],
-    },
-    {
-      name: 'Layout',
-      items: [],
-    },
-    {
-      name: 'Help',
-      items: [
-        {
-          name: 'Visit Support.Bible',
-          command: 'platform.visitSupportPage',
-          hasDivider: true,
-        },
-        {
-          name: 'About Platform.Bible...',
-          command: 'platform.about',
           hasDivider: true,
         },
       ],
