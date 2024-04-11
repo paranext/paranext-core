aff9bfcab676d43aa048b1190d327b3b8b8427b7
Rolf Heij <rolf_heij@sil.org>
2024-04-11 21:40:43 +0200

    import shadcn dropdown menu, add icons, work on styles (redo)

--- lib/platform-bible-react/src/components/book-chapter-control/book-chapter-control.component.tsx
+++ lib/platform-bible-react/src/components/book-chapter-control/book-chapter-control.component.tsx
@@ -1,14 +1,59 @@
+import { History, Search } from 'lucide-react';
+import {
+  DropdownMenuTrigger as ShadDropdownMenuTrigger,
+  DropdownMenu as ShadDropdownMenu,
+  DropdownMenuContent as ShadDropdownMenuContent,
+} from '../shadcn-ui/dropdown-menu';
+import BookChapterInput from './book-chapter-input.component';
 import ChapterSelect from './chapter-select.component';
 
 function BookChapterControl() {
   return (
-    <ChapterSelect
-      endChapter={30}
-      handleSelectChapter={(chapterNumber: number) => {
-        throw new Error(`Function not implemented. ${chapterNumber}`);
-      }}
-      activeChapter={0}
-    />
+    <div>
+      <ShadDropdownMenu>
+        <Search
+          style={{
+            position: 'absolute',
+            left: '8px',
+            top: '32px',
+            color: 'gray',
+            width: '16px',
+          }}
+        />
+        {/* Adds a grey box behind our input */}
+        <ShadDropdownMenuTrigger style={{ backgroundColor: 'transparent', border: 'none' }}>
+          <BookChapterInput
+            // handleClick={() => {
+            //   throw new Error('Function not implemented.');
+            // }}
+            value="Hello"
+            placeholder="Hello"
+          />
+        </ShadDropdownMenuTrigger>
+        <History
+          style={{
+            position: 'absolute',
+            left: '276px',
+            top: '32px',
+            cursor: 'pointer',
+            width: '16px',
+          }}
+          onClick={() => {
+            // eslint-disable-next-line no-console
+            console.log('back in history');
+          }}
+        />
+        <ShadDropdownMenuContent>
+          <ChapterSelect
+            endChapter={30}
+            handleSelectChapter={(chapterNumber: number) => {
+              throw new Error(`Function not implemented. ${chapterNumber}`);
+            }}
+            activeChapter={0}
+          />
+        </ShadDropdownMenuContent>
+      </ShadDropdownMenu>
+    </div>
   );
 }
 
