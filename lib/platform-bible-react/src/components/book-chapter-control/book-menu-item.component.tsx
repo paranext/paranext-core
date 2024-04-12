74edbe9e5f8f6d54eb1e7c4892269dccdfef3b5a
Rolf Heij <rolf_heij@sil.org>
2024-04-12 12:03:04 +0200

    Implemented style upgrades (redo)

--- lib/platform-bible-react/src/components/book-chapter-control/book-menu-item.component.tsx
+++ lib/platform-bible-react/src/components/book-chapter-control/book-menu-item.component.tsx
@@ -3,7 +3,6 @@ import { PropsWithChildren } from 'react';
 import { Tally1 } from 'lucide-react';
 import { DropdownMenuItem as ShadDropdownMenuItem } from '@/components/shadcn-ui/dropdown-menu';
 import { cn } from '@/utils/shadcn-ui.util';
-import '@/components/book-chapter-control/book-menu-item.component.css';
 
 export type BookType = 'OT' | 'NT' | 'DC';
 
@@ -30,14 +29,23 @@ function BookMenuItem({
     <ShadDropdownMenuItem
       key={bookId}
       textValue={bookId}
-      className={cn('pr-font-normal pr-text-slate-700', isSelected ?? 'selected')}
+      className={cn('pr-font-normal pr-text-slate-700 ', {
+        // Overriding `data-[highlighted]` changes the default gray background that is normally shown on hover
+        'pr-bg-amber-50 pr-text-yellow-900 data-[highlighted]:pr-bg-amber-50': isSelected,
+      })}
       onSelect={(e) => {
         // preventDefault() here prevents the entire dropdown menu from closing when selecting this item
         e.preventDefault();
         handleSelectBook(bookId);
       }}
     >
-      <Tally1 className={cn('pr-mr-2.5 pr-h-4 pr-w-0.5', `${bookType.toLowerCase()}`)} />
+      <Tally1
+        className={cn('pr-mr-2.5 pr-h-4 pr-w-0.5', {
+          'pr-bg-red-300': bookType.toLowerCase() === 'ot',
+          'pr-bg-purple-200': bookType.toLowerCase() === 'nt',
+          'pr-bg-indigo-200': bookType.toLowerCase() === 'dc',
+        })}
+      />
       {Canon.bookIdToEnglishName(bookId)}
       {isSelected && <div>{children}</div>}
     </ShadDropdownMenuItem>
