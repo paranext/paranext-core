ff16708ff29c80abf4aadd4195cdc1978385872b
Rolf Heij <rolf_heij@sil.org>
2024-04-12 11:49:56 +0200

    Using utility classes (redo)

--- lib/platform-bible-react/src/components/book-chapter-control/chapter-select.component.tsx
+++ lib/platform-bible-react/src/components/book-chapter-control/chapter-select.component.tsx
@@ -1,4 +1,5 @@
-import './chapter-select.component.css';
+import { cn } from '@/utils/shadcn-ui.util';
+// import './chapter-select.component.css';
 
 export type ChapterSelectProps = {
   // The end chapter of the selected book
@@ -13,12 +14,16 @@ function ChapterSelect({ endChapter, activeChapter, handleSelectChapter }: Chapt
   const chapters = Array.from({ length: endChapter }, (_, i) => i + 1);
 
   return (
-    <div className="chapter-select">
+    <div className="pr-flex pr-flex-wrap pr-content-start pr-items-start pr-self-stretch pr-bg-amber-50">
       {chapters.map((chapter) => (
         // When adding onClick to <div> get error: Visible, non-interactive elements with click handlers must have at least one keyboard listener.
         // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
         <div
-          className={`chapter ${chapter === activeChapter ? 'active' : undefined}`}
+          className={cn(
+            'pr-h-5 pr-w-5 pr-cursor-pointer pr-items-center pr-justify-center pr-rounded pr-p-2 pr-text-amber-800',
+            'hover:pr-bg-amber-200',
+            `${chapter === activeChapter ?? 'pr-bg-amber-200 pr-font-semibold pr-text-amber-900'}`,
+          )}
           key={chapter}
           onClick={() => handleSelectChapter(chapter)}
         >
