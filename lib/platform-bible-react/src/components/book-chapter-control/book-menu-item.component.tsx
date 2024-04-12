0220d6a135dcd95f7a330917df65482e08d6335a
Rolf Heij <rolf_heij@sil.org>
2024-04-12 12:14:54 +0200

    remove todos, book color line height (redo)

--- lib/platform-bible-react/src/components/book-chapter-control/book-menu-item.component.tsx
+++ lib/platform-bible-react/src/components/book-chapter-control/book-menu-item.component.tsx
@@ -1,6 +1,5 @@
 import { Canon } from '@sillsdev/scripture';
 import { PropsWithChildren } from 'react';
-import { Tally1 } from 'lucide-react';
 import { DropdownMenuItem as ShadDropdownMenuItem } from '@/components/shadcn-ui/dropdown-menu';
 import { cn } from '@/utils/shadcn-ui.util';
 
@@ -29,7 +28,7 @@ function BookMenuItem({
     <ShadDropdownMenuItem
       key={bookId}
       textValue={bookId}
-      className={cn('pr-font-normal pr-text-slate-700 ', {
+      className={cn('pr-font-normal pr-text-slate-700', {
         // Overriding `data-[highlighted]` changes the default gray background that is normally shown on hover
         'pr-bg-amber-50 pr-text-yellow-900 data-[highlighted]:pr-bg-amber-50': isSelected,
       })}
@@ -39,17 +38,21 @@ function BookMenuItem({
         handleSelectBook(bookId);
       }}
     >
-      <Tally1
-        className={cn('pr-mr-2.5 pr-h-4 pr-w-0.5', {
-          'pr-bg-red-300': bookType.toLowerCase() === 'ot',
-          'pr-bg-purple-200': bookType.toLowerCase() === 'nt',
-          'pr-bg-indigo-200': bookType.toLowerCase() === 'dc',
-        })}
-      />
-      {Canon.bookIdToEnglishName(bookId)}
+      <span
+        className={cn(
+          'pr-border-b-0 pr-border-l-2 pr-border-r-0 pr-border-t-0 pr-border-solid pr-px-2',
+          { 'pr-border-l-red-200': bookType.toLowerCase() === 'ot' },
+          { 'pr-border-l-purple-200': bookType.toLowerCase() === 'nt' },
+          { 'pr-border-l-indigo-200': bookType.toLowerCase() === 'dc' },
+        )}
+      >
+        {Canon.bookIdToEnglishName(bookId)}
+      </span>
       {isSelected && <div>{children}</div>}
     </ShadDropdownMenuItem>
   );
 }
 
 export default BookMenuItem;
+
+// gap-2
