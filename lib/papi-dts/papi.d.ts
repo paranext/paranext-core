82a44aef3f0af30465ffaa05cdfeddc40b4ae8a9
tjcouch-sil <tj_couch@sil.org>
2024-04-15 12:15:17 -0500

    Added IProjectDataProviderFactory for public documentation, stopped throwing when there isn't a web view menu definition

--- lib/papi-dts/papi.d.ts
+++ lib/papi-dts/papi.d.ts
@@ -3965,6 +3965,34 @@ declare module 'shared/services/project-lookup.service' {
   const projectLookupService: ProjectLookupServiceType;
   export default projectLookupService;
 }
+declare module 'shared/models/project-data-provider-factory.interface' {
+  import { Dispose } from 'platform-bible-utils';
+  /**
+   * Network object that creates Project Data Providers of a specific `projectType` as requested on
+   * the `papi`. These are created internally within the platform to layer over
+   * TypeScript-extension-provided {@link IProjectDataProviderEngineFactory} or are created by
+   * independent processes on the `papi`.
+   */
+  interface IProjectDataProviderFactory extends Dispose {
+    /**
+     * Returns the registered network object name of a PDP for the given project ID and PSI. Called by
+     * the platform when someone uses the project data provider service to access a project's data.
+     *
+     * @param projectId Id of the project for which to return a project data provider.
+     * @param projectStorageInterpreterId Id of the project storage interpreter that corresponds to
+     *   the project to access
+     * @returns Id of the project data provider this `IProjectDataProviderFactory` created for this
+     *   project id. It should return the same project data provider for the same combination of
+     *   parameters throughout one session (in other words, in general, there should just be one
+     *   project data provider for one project id).
+     */
+    getProjectDataProviderId(
+      projectId: string,
+      projectStorageInterpreterId: string,
+    ): Promise<string>;
+  }
+  export default IProjectDataProviderFactory;
+}
 declare module 'shared/services/project-data-provider.service' {
   import { ProjectTypes, ProjectDataProviders } from 'papi-shared-types';
   import { IProjectDataProviderEngineFactory } from 'shared/models/project-data-provider-engine.model';
@@ -4842,6 +4870,7 @@ declare module '@papi/core' {
     IProjectDataProviderEngine,
     IProjectDataProviderEngineFactory,
   } from 'shared/models/project-data-provider-engine.model';
+  export type { default as IProjectDataProviderFactory } from 'shared/models/project-data-provider-factory.interface';
   export type { ProjectMetadata } from 'shared/models/project-metadata.model';
   export type { MandatoryProjectStorageDataTypes } from 'shared/models/project-storage-interpreter.model';
   export type {
