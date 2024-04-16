f106a34678a15430f2e399f34fc568b7d4a79755
tjcouch-sil <tj_couch@sil.org>
2024-04-16 12:15:47 -0500

    Added platformScripture settings

--- extensions/src/platform-scripture/src/types/platform-scripture.d.ts
+++ extensions/src/platform-scripture/src/types/platform-scripture.d.ts
@@ -2,3 +2,27 @@ declare module 'platform-scripture' {
   // Add extension types exposed on the papi for other extensions to use here
   // More instructions can be found in the README
 }
+
+declare module 'papi-shared-types' {
+  export interface ProjectSettingTypes {
+    /**
+     * Which versification scheme this Scripture project uses
+     *
+     * WARNING: This setting is an example and needs to be updated to support proper versification
+     * specification! For the moment, it simply corresponds to
+     * [`ScrVersType`](https://github.com/sillsdev/libpalaso/blob/master/SIL.Scripture/Versification.cs#L1340),
+     * but this is not correct and full design.
+     */
+    'platformScripture.versification': number;
+    /**
+     * Which books are present in this Scripture project. Represented as a string with 0 or 1 for
+     * each possible book by [standardized book
+     * code](https://github.com/sillsdev/libpalaso/blob/master/SIL.Scripture/Canon.cs#L226) (123
+     * characters long)
+     *
+     * @example
+     * '100111000000000000110000001000000000010111111111111111111111111111000000000000000000000000000000000000000000100000000000000'
+     */
+    'platformScripture.booksPresent': string;
+  }
+}
--- src/extension-host/data/core-project-settings-info.data.ts
+++ src/extension-host/data/core-project-settings-info.data.ts
@@ -17,41 +17,22 @@ export const platformProjectSettings: ProjectSettingsContribution = {
       label: '%project_settings_platform_language_label%',
       default: '%project_language_missing%',
     },
-    'platformScripture.booksPresent': {
-      label: '%project_settings_platform_booksPresent_label%',
-      description: '%project_settings_platform_booksPresent_description%',
-      default:
-        //                                                                                                  1
-        //        1         2         3         4         5         6         7         8         9         0         1         2         3
-        // 34567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
-        '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-    },
-    'platformScripture.versification': {
-      label: '%project_settings_platformScripture_versification_label%',
-      description: '%project_settings_platformScripture_versification_description%',
-      default: 4,
-    },
   },
 };
 
-// Based on https://github.com/paranext/paranext-core/blob/5c403e272b002ddd8970f735bc119f335c78c509/extensions/src/usfm-data-provider/index.d.ts#L401
-// Should be 123 characters long
-const booksPresentSettingsValidator: ProjectSettingValidator<
-  'platformScripture.booksPresent'
-> = async (newValue: string): Promise<boolean> => {
-  return newValue.length === 123 && newValue.replace(/[01]/g, '').length === 0;
+const fullNameValidator: ProjectSettingValidator<'platform.fullName'> = async (newValue) => {
+  return typeof newValue === 'string';
 };
 
-// Based on https://github.com/paranext/paranext-core/blob/5c403e272b002ddd8970f735bc119f335c78c509/extensions/src/usfm-data-provider/index.d.ts#L391
-// There are 7 options in the enum
-const versificationSettingsValidator: ProjectSettingValidator<
-  'platformScripture.versification'
-> = async (newValue: number): Promise<boolean> => {
-  return typeof newValue === 'number' && newValue >= 0 && newValue <= 6;
+// TODO: Validate that strings in the array to match BCP 47 values once the i18n code is ready
+const languageValidator: ProjectSettingValidator<'platform.language'> = async (
+  newValue: string,
+): Promise<boolean> => {
+  return typeof newValue === 'string';
 };
 
 /** Info about all settings built into core. Does not contain info for extensions' settings */
 export const coreProjectSettingsValidators: Partial<AllProjectSettingsValidators> = {
-  'platformScripture.booksPresent': booksPresentSettingsValidator,
-  'platformScripture.versification': versificationSettingsValidator,
+  'platform.fullName': fullNameValidator,
+  'platform.language': languageValidator,
 };
