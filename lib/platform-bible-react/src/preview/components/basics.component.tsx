045ec5920d93490745015713396059cccce4173e
Sebastian-ubs <141921979+Sebastian-ubs@users.noreply.github.com>
2024-06-04 18:23:16 +0200

    preview app: better aligned input examples (#921)

--- lib/platform-bible-react/src/preview/components/basics.component.tsx
+++ lib/platform-bible-react/src/preview/components/basics.component.tsx
@@ -56,7 +56,7 @@ function Basics() {
             </tr>
             <tr>
               <td>
-                Shadcn Default Input
+                Shadcn Input
                 <div className="pr-text-xs">(shadcn-ui/input)</div>
               </td>
               {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
@@ -66,8 +66,18 @@ function Basics() {
             </tr>
             <tr>
               <td>
+                Shdcn Input <div className="pr-text-xs">(from ui.shadcn.com &rarr; input)</div>
+              </td>
+              {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+              <td className="">
+                {/* copied from ui.shadcn.com but replaced ring-ring with pr-ring-[240 5% 64.9%], as they set --ring to this value in their style.css */}
+                <Input className="pr-flex pr-h-10 pr-w-full pr-rounded-md pr-border pr-border-input pr-bg-background pr-px-3 pr-py-2 pr-text-sm pr-ring-offset-background file:pr-border-0 file:pr-bg-transparent file:pr-text-sm file:pr-font-medium placeholder:pr-text-muted-foreground focus-visible:pr-outline-none focus-visible:pr-ring-2 focus-visible:pr-ring-[color:hsl(240,5%,64.9%)] focus-visible:pr-ring-offset-2 disabled:pr-cursor-not-allowed disabled:pr-opacity-50" />
+              </td>
+            </tr>
+            <tr>
+              <td>
                 Small Input
-                <div className="pr-text-xs">(from https://ui.jln.dev/ &rarr; popover)</div>
+                <div className="pr-text-xs">(from ui.jln.dev &rarr; popover)</div>
               </td>
               {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
               <td>
@@ -76,7 +86,7 @@ function Basics() {
             </tr>
             <tr>
               <td>
-                Small Input <div className="pr-text-xs">(from BCV control)</div>
+                BVC Input <div className="pr-text-xs">(from book-chapter-control)</div>
               </td>
               {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
               <td>
