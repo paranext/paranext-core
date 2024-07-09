ad6e5b83ed8035c971735db765b85d419cc68e9e
Sebastian-ubs <141921979+Sebastian-ubs@users.noreply.github.com>
2024-07-09 15:42:53 +0200

    add a draft Paratext10 theme to preview app (#985)
    * add a Paratext10 theme to preview app
    
    * adjustments, fixes, logo and comments

--- lib/platform-bible-react/src/preview/theme-toggle.component.tsx
+++ lib/platform-bible-react/src/preview/theme-toggle.component.tsx
@@ -11,7 +11,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '../components/shadcn-ui/dropdown-menu';
-import { useTheme } from './theme-provider.component';
+import { Theme, useTheme } from './theme-provider.component';
+import ParatextLogo from './icons/paratext-logo.component';
+import ShadCnLogo from './icons/shadcn-logo.component';
 
 export type ThemeToggleProps = ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
   className?: string;
@@ -51,14 +53,45 @@ export const ThemeButton = React.forwardRef<
 >(({ className }, ref) => {
   const { theme, setTheme } = useTheme();
   return (
-    <div ref={ref} className={className}>
-      <Button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
-        {theme === 'light' ? (
-          <Moon className="pr-h-[1.2rem] pr-w-[1.2rem]" />
-        ) : (
-          <Sun className="pr-h-[1.2rem] pr-w-[1.2rem]" />
-        )}
-      </Button>
+    <div className="pr-flex pr-gap-2">
+      <div ref={ref} className={className}>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() =>
+            setTheme(
+              // string replacement should always end up with a value of 'Theme', but ESLint cannot know or guarantee it
+              // eslint-disable-next-line no-type-assertion/no-type-assertion
+              (theme.includes('paratext-')
+                ? theme.replace('paratext-', '')
+                : `paratext-${theme}`) as Theme,
+            )
+          }
+        >
+          {theme.includes('paratext-') ? <ParatextLogo /> : <ShadCnLogo />}
+        </Button>
+      </div>
+      <div ref={ref} className={className}>
+        <Button
+          variant="outline"
+          size="icon"
+          onClick={() =>
+            setTheme(
+              // string replacement should always end up with a value of 'Theme', but ESLint cannot know or guarantee it
+              // eslint-disable-next-line no-type-assertion/no-type-assertion
+              (theme.includes('dark')
+                ? theme.replace('dark', 'light')
+                : theme.replace('light', 'dark')) as Theme,
+            )
+          }
+        >
+          {theme.includes('-light') ? (
+            <Sun className="pr-h-[1.2rem] pr-w-[1.2rem]" />
+          ) : (
+            <Moon className="pr-h-[1.2rem] pr-w-[1.2rem]" />
+          )}
+        </Button>
+      </div>
     </div>
   );
 });
