0f1b201b27010fccbd193523f00f7d3cbb39c161
Sebastian-ubs <slingk@biblesocieties.org>
2024-07-12 16:36:46 +0200

    fix twMerge to use our prefix

--- lib/platform-bible-react/src/utils/shadcn-ui.util.ts
+++ lib/platform-bible-react/src/utils/shadcn-ui.util.ts
@@ -1,8 +1,10 @@
 import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const twMergeCustom = extendTailwindMerge({prefix: 'pr-'});
 
 // shadcn/ui uses this export in its boilerplate code
 // eslint-disable-next-line import/prefer-default-export
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMergeCustom(clsx(inputs));
 }
