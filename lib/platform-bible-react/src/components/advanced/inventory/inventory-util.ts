9a86a9a8038c6c207c5d9df74156a9f3e76dfcce
Rolf Heij <rolf_heij@hotmail.com>
2024-08-26 17:08:12 -0400

    Add unit tests for inventory utils

--- lib/platform-bible-react/src/components/advanced/inventory/inventory-util.ts
+++ lib/platform-bible-react/src/components/advanced/inventory/inventory-util.ts
@@ -1,14 +1,16 @@
 /**
  * Splits USFM string into shorter line-like segments
+ *
  * @param text A single (likely very large) USFM string
  * @returns An array containing the input text, split into shorter segments
  */
 export const getLinesFromUSFM = (text: string) => {
-  return text.split(/(?=\n|\\(?:v|c|id))/g);
+  return text.split(/(?:\r?\n|\r)|(?=(?:\\(?:v|c|id)))/g);
 };
 
 /**
  * Extracts chapter or verse number from USFM strings that start with a chapter or verse marker
+ *
  * @param text USFM string
  * @returns Chapter or verse number if one is found. Else returns 0.
  */
