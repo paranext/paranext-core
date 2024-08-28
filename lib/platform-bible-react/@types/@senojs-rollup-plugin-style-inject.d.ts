9e6f6c79b1f349af97d0e069d992af9407525ec5
Ira Hopkinson <ira_hopkinson@sil.org>
2024-08-28 15:35:00 +1200

    - more code review
    - also make VS Code use the repo TS version

--- lib/platform-bible-react/@types/@senojs-rollup-plugin-style-inject.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@senojs/rollup-plugin-style-inject' {
-  type StyleInjectOptions = {
-    insertAt?: 'top' | 'bottom';
-  };
-
-  // This should return `PluginOption` type from vite but this is only build config.
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  export default function styleInject(options?: StyleInjectOptions): any;
-}
