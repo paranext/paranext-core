727fa2d9ac0928aaa60f5dc073e66a38aa6dae0d
Matt Lyons <matt_lyons@sil.org>
2024-10-15 14:22:23 -0500

    First draft of adding support for comments in PT9 projects

---
+++ extensions/src/platform-scripture-editor/src/comments.ts
@@ -0,0 +1,280 @@
+import { Comments } from '@biblionexus-foundation/platform-editor';
+import { MarkerContent, MarkerObject, Usj } from '@biblionexus-foundation/scripture-utilities';
+import {
+  CHAPTER_TYPE,
+  ScriptureReference,
+  UsjContentLocation,
+  UsjReaderWriter,
+  VERSE_TYPE,
+  VerseRefOffset,
+} from 'platform-bible-utils';
+import { LegacyComment } from 'legacy-comment-manager';
+import { Canon, VerseRef } from '@sillsdev/scripture';
+import { logger } from '@papi/frontend';
+
+export const MILESTONE_START = 'zmsc-s';
+export const MILESTONE_END = 'zmsc-e';
+
+const ID_TYPE_START = 'sid';
+const ID_TYPE_END = 'eid';
+
+/** Individual comment that can be read by the editor */
+export type EditorComment = {
+  author: string;
+  content: string;
+  deleted: boolean;
+  id: string;
+  timeStamp: number;
+  type: 'comment';
+  legacyComment: LegacyComment;
+};
+
+/** Comment thread that can be read by the editor */
+export type EditorThread = {
+  comments: EditorComment[];
+  id: string;
+  quote: string;
+  type: 'thread';
+  legacyComment: LegacyComment;
+};
+
+function valuesAreDeeplyEqual(a: unknown, b: unknown) {
+  return JSON.stringify(a) === JSON.stringify(b);
+}
+
+/** Convert comments from the data provider to comments that can be read by the editor */
+export function convertLegacyCommentsToEditorThreads(
+  legacyComments: LegacyComment[],
+): EditorThread[] {
+  const retVal: EditorThread[] = [];
+  const threads: Map<string, EditorThread> = new Map();
+  legacyComments.forEach((legacyComment) => {
+    let editorThread = threads.get(legacyComment.thread);
+    if (!editorThread) {
+      editorThread = {
+        comments: [],
+        id: legacyComment.thread,
+        quote: `${legacyComment.verseRef}${legacyComment.selectedText ? ' - ' : ''}${legacyComment.selectedText ?? ''}`,
+        type: 'thread',
+        legacyComment,
+      };
+      threads.set(editorThread.id, editorThread);
+      retVal.push(editorThread);
+    }
+
+    editorThread.comments.push({
+      author: legacyComment.user,
+      content: legacyComment.contents,
+      deleted: legacyComment.deleted,
+      id: legacyComment.id,
+      timeStamp: Date.parse(legacyComment.date),
+      type: 'comment',
+      legacyComment,
+    });
+  });
+  return retVal;
+}
+
+function getCommentDetails(
+  usjRW: UsjReaderWriter,
+  commentId: string,
+  bookId: string,
+): { start: VerseRefOffset; selectedText: string; contextAfter: string } | undefined {
+  const startQuery = `$..content[?(@.type=="ms" && @.marker=="${MILESTONE_START}" && @.sid=="${commentId}")]`;
+  const endQuery = `$..content[?(@.type=="ms" && @.marker=="${MILESTONE_END}" && @.eid=="${commentId}")]`;
+
+  const startNode = usjRW.findSingleValue<MarkerObject>(startQuery);
+  const endNode = usjRW.findSingleValue<MarkerObject>(endQuery);
+  if (!startNode || !endNode) {
+    logger.debug(`Start and end nodes are not both present. start:${startNode} end:${endNode}`);
+    return undefined;
+  }
+
+  const startParent = usjRW.findParent<MarkerObject>(startQuery);
+  const endParent = usjRW.findParent<MarkerObject>(endQuery);
+  if (!startParent || !endParent)
+    throw new Error(
+      `Start and end parents are not both present. start:${startParent} end:${endParent}`,
+    );
+
+  const start = usjRW.nodeToVerseRefAndOffset(bookId, startNode, startParent);
+  if (!start) throw new Error(`Could not find VerseRef for start of comment ${commentId}`);
+
+  const startPoint = { node: startNode, offset: 0, jsonPath: usjRW.nodeToJsonPath(startNode) };
+  const endPoint = { node: endNode, offset: 0, jsonPath: usjRW.nodeToJsonPath(endNode) };
+
+  const selectedText = usjRW.extractTextBetweenPoints(startPoint, endPoint, 20);
+  const contextAfter = usjRW.extractText(endPoint, 10);
+
+  return { start, selectedText, contextAfter };
+}
+
+/** Convert comments from the editor into comments that can be saved to the legacy data provider */
+export function convertEditorCommentsToLegacyComments(
+  comments: Comments,
+  usjRW: UsjReaderWriter,
+  scrRef: ScriptureReference,
+): LegacyComment[] {
+  const legacyComments: LegacyComment[] = [];
+  comments.forEach((editorComment) => {
+    const commentDetails = getCommentDetails(
+      usjRW,
+      editorComment.id,
+      Canon.bookNumberToId(scrRef.bookNum),
+    );
+    if (!commentDetails) return;
+    if (editorComment.type === 'comment') {
+      legacyComments.push({
+        contents: editorComment.content,
+        contextAfter: commentDetails.contextAfter,
+        date: new Date(editorComment.timeStamp).toISOString(),
+        deleted: editorComment.deleted,
+        hideInTextWindow: false,
+        id: editorComment.id,
+        language: 'en',
+        selectedText: commentDetails.selectedText,
+        startPosition: commentDetails.start.offset,
+        thread: editorComment.id,
+        user: editorComment.author,
+        verseRef: commentDetails.start.verseRef.toString(),
+      });
+    } else {
+      editorComment.comments.forEach((editorThreadComment) => {
+        legacyComments.push({
+          contents: editorThreadComment.content,
+          contextAfter: commentDetails.contextAfter,
+          date: new Date(editorThreadComment.timeStamp).toISOString(),
+          deleted: editorThreadComment.deleted,
+          hideInTextWindow: false,
+          id: editorThreadComment.id,
+          language: 'en',
+          selectedText: commentDetails.selectedText,
+          startPosition: commentDetails.start.offset,
+          thread: editorComment.id,
+          user: editorThreadComment.author,
+          verseRef: commentDetails.start.verseRef.toString(),
+        });
+      });
+    }
+  });
+  return legacyComments;
+}
+
+function getAnchorLocation(
+  usjRW: UsjReaderWriter,
+  anchorMarker: typeof MILESTONE_START | typeof MILESTONE_END,
+  anchorIdType: typeof ID_TYPE_START | typeof ID_TYPE_END,
+  anchorId: string,
+): UsjContentLocation | undefined {
+  const jsonPathExpressionForAnchor = `$..content[?(@.type=="ms" && @.marker=="${anchorMarker}" && @.${anchorIdType}=="${anchorId}")]`;
+  const node: MarkerObject | undefined = usjRW.findSingleValue(jsonPathExpressionForAnchor);
+  if (!node) return undefined;
+  return { node, offset: 0, jsonPath: usjRW.nodeToJsonPath(node) };
+}
+
+function insertAnchorIfNeeded(
+  usjRW: UsjReaderWriter,
+  startOrEnd: 'start' | 'end',
+  anchorId: string,
+  anchorLocation: UsjContentLocation,
+): boolean {
+  // Make sure it doesn't already exist
+  const anchorMarker = startOrEnd === 'start' ? MILESTONE_START : MILESTONE_END;
+  const anchorIdType = startOrEnd === 'start' ? ID_TYPE_START : ID_TYPE_END;
+  if (getAnchorLocation(usjRW, anchorMarker, anchorIdType, anchorId)) return false;
+
+  // Find the container for the anchor
+  const contentArray: MarkerContent[] | undefined = usjRW.findParent(anchorLocation.jsonPath);
+  if (!Array.isArray(contentArray))
+    throw new Error(`Unexpected anchor location parent: ${JSON.stringify(contentArray)}`);
+
+  // Figure out where this is in the content array
+  const matches = anchorLocation.jsonPath.match(/content\[(\d+)\]/g);
+  if (!matches || matches.length === 0)
+    throw new Error(`Unexpected jsonPath query: ${anchorLocation.jsonPath}`);
+  const lastNumber = matches[matches.length - 1].match(/(\d+)/);
+  const contentIndex = parseInt(lastNumber ? lastNumber[0] : '0', 10);
+  if (contentIndex > contentArray.length - 1)
+    throw new Error(`Index ${contentIndex} longer than content array: ${anchorLocation.jsonPath}`);
+  if (anchorLocation.node && !valuesAreDeeplyEqual(contentArray[contentIndex], anchorLocation.node))
+    throw new Error(
+      `Found unexpected node at anchor location, expected:${JSON.stringify(anchorLocation.node)}, actual:${JSON.stringify(contentArray[contentIndex])}`,
+    );
+
+  // Now insert the anchor
+  const anchorNode = { type: 'ms', marker: anchorMarker, [anchorIdType]: anchorId };
+  const currentContent = contentArray[contentIndex];
+  // If it's an end anchor and the comment didn't specify where it goes, pick a place
+  if (!anchorLocation.node && startOrEnd === 'end') {
+    let potentialIndex = contentIndex;
+    while (potentialIndex < contentArray.length) {
+      const potentialContent = contentArray[potentialIndex];
+      if (typeof potentialContent === 'string') break;
+      if (potentialContent.type === VERSE_TYPE || potentialContent.type === CHAPTER_TYPE) {
+        if (potentialIndex + 1 < contentArray.length) potentialIndex += 1;
+        break;
+      }
+      potentialIndex += 1;
+    }
+    contentArray.splice(potentialIndex, 0, anchorNode);
+  }
+  // Insert the anchor before anything else in that spot
+  else if (anchorLocation.offset === 0) {
+    contentArray.splice(contentIndex, 0, anchorNode);
+  } else {
+    if (typeof currentContent !== 'string')
+      throw new Error(`Inserting anchor at offset ${anchorLocation.offset} where no string exists`);
+    if (anchorLocation.offset > currentContent.length - 1)
+      throw new Error(`Node was too short for offset ${anchorLocation.offset}: ${currentContent}`);
+    // Break up the existing string and put the anchor between the two parts
+    const firstPart = currentContent.substring(0, anchorLocation.offset);
+    const secondPart = currentContent.substring(anchorLocation.offset);
+    contentArray.splice(contentIndex, 1, firstPart, anchorNode, secondPart);
+  }
+  usjRW.usjChanged();
+  return true;
+}
+
+/**
+ * Inserts comment anchors for the given comments into the provided USJ
+ *
+ * @returns True if some comment anchors were inserted, false if no comment anchors were inserted
+ */
+export function insertCommentAnchorsIntoUsj(usj: Usj, legacyComments: LegacyComment[]): boolean {
+  const usjRW = new UsjReaderWriter(usj);
+  let usjChanged = false;
+
+  const threads = convertLegacyCommentsToEditorThreads(legacyComments);
+  threads.forEach((thread) => {
+    const lc = thread.legacyComment;
+    const verseRef: VerseRef = new VerseRef(lc.verseRef);
+    // BUG: PT9 comments give offset in USFM space, not verse text space
+    let start = usjRW.verseRefToUsjContentLocation(verseRef, lc.startPosition);
+    const startAnchorInserted = insertAnchorIfNeeded(usjRW, 'start', thread.id, start);
+    if (!startAnchorInserted) {
+      // If the start anchor is there already, see if the end anchor is there, too
+      if (getAnchorLocation(usjRW, MILESTONE_END, ID_TYPE_END, thread.id)) return;
+    }
+    usjChanged = true;
+
+    // Relocate the start anchor
+    const newStart = getAnchorLocation(usjRW, MILESTONE_START, ID_TYPE_START, thread.id);
+    if (!newStart) throw new Error('Unable to find start anchor after it was inserted');
+    start = newStart;
+
+    // BUG: PT9 comments may include markup, so we might not find the end
+    const indicatedEnd: UsjContentLocation | undefined = lc.contextAfter
+      ? usjRW.findNextLocationOfMatchingText(start, lc.contextAfter)
+      : undefined;
+
+    // If no end was provided/found, lean on the insertion function to place the end anchor
+    const end = indicatedEnd ?? {
+      node: '',
+      offset: start.offset,
+      jsonPath: start.jsonPath,
+    };
+    insertAnchorIfNeeded(usjRW, 'end', thread.id, end);
+  });
+
+  return usjChanged;
+}
--- extensions/src/platform-scripture-editor/src/platform-scripture-editor.web-view.tsx
+++ extensions/src/platform-scripture-editor/src/platform-scripture-editor.web-view.tsx
@@ -1,18 +1,27 @@
 import {
+  Comments,
   EditorOptions,
   Editorial,
   EditorRef,
   Marginal,
   MarginalRef,
 } from '@biblionexus-foundation/platform-editor';
-import { Usj } from '@biblionexus-foundation/scripture-utilities';
+import { MarkerContent, Usj } from '@biblionexus-foundation/scripture-utilities';
 import { Canon, VerseRef } from '@sillsdev/scripture';
 import { JSX, useCallback, useEffect, useMemo, useRef } from 'react';
 import type { WebViewProps } from '@papi/core';
 import { logger } from '@papi/frontend';
 import { useProjectData, useProjectSetting } from '@papi/frontend/react';
-import { ScriptureReference, debounce } from 'platform-bible-utils';
+import { debounce, ScriptureReference, UsjReaderWriter } from 'platform-bible-utils';
 import { Button } from 'platform-bible-react';
+import { LegacyComment } from 'legacy-comment-manager';
+import {
+  convertEditorCommentsToLegacyComments,
+  convertLegacyCommentsToEditorThreads,
+  insertCommentAnchorsIntoUsj,
+  MILESTONE_END,
+  MILESTONE_START,
+} from './comments';
 
 /** The offset in pixels from the top of the window to scroll to show the verse number */
 const VERSE_NUMBER_SCROLL_OFFSET = 80;
@@ -65,7 +74,6 @@ globalThis.webViewComponent = function PlatformScriptureEditor({
   useWebViewScrollGroupScrRef,
 }: WebViewProps): JSX.Element {
   const [isReadOnly] = useWebViewState<boolean>('isReadOnly', true);
-  const Editor = isReadOnly ? Editorial : Marginal;
 
   // Using react's ref api which uses null, so we must use null
   // eslint-disable-next-line no-null/no-null
@@ -92,35 +100,135 @@ globalThis.webViewComponent = function PlatformScriptureEditor({
    */
   const hasFirstRetrievedScripture = useRef(false);
 
-  const [usj, setUsj] = useProjectData('platformScripture.USJ_Chapter', projectId).ChapterUSJ(
+  const [usj, setUsjRaw] = useProjectData('platformScripture.USJ_Chapter', projectId).ChapterUSJ(
     useMemo(() => new VerseRef(scrRef.bookNum, scrRef.chapterNum, scrRef.verseNum), [scrRef]),
     usjDocumentDefault,
   );
 
+  const setUsj = useCallback(
+    (newUsj: Usj) => {
+      if (!setUsjRaw) return;
+      const usjRW = new UsjReaderWriter(newUsj);
+      // Remove the milestones that we inserted before writing back to the PDP
+      usjRW.removeContentNodes((node: MarkerContent) => {
+        if (typeof node === 'string') return false;
+        if (node.type !== 'ms') return false;
+        if (node.marker === MILESTONE_START || node.marker === MILESTONE_END) return true;
+        return true;
+      });
+      setUsjRaw(newUsj);
+    },
+    [setUsjRaw],
+  );
+
+  const [legacyComments, setLegacyComments] = useProjectData(
+    'legacyCommentManager.comments',
+    projectId,
+  ).Comments(
+    useMemo(() => {
+      return { bookId: Canon.bookNumberToId(scrRef.bookNum), chapterNum: scrRef.chapterNum };
+    }, [scrRef]),
+    [],
+  );
+
   const debouncedSetUsj = useMemo(() => debounce((newUsj: Usj) => setUsj?.(newUsj), 300), [setUsj]);
 
-  // Editor's current usj state
+  // Editor's current usj and comment state
   const editorUsj = useRef(usj);
+  const editorLegacyComments = useRef(legacyComments);
+
+  const updateComments = useCallback(
+    (newComments: Comments | undefined, usjWithAnchors: Usj | undefined = editorUsj.current) => {
+      // Cannot convert between legacy and current comments without access to corresponding USJ
+      if (!usjWithAnchors) {
+        logger.warn('Updating comments without providing USJ');
+        return;
+      }
+      // We aren't currently overwriting comments, so if it's empty we have nothing to do
+      if (!newComments) {
+        logger.debug('Updating comments, but the comments are empty');
+        return;
+      }
+      // If we can't save a newly merged set of comments, we have nothing to do
+      if (!setLegacyComments) {
+        logger.warn('Updating comments without a way to save the comments to a PDP');
+        return;
+      }
+
+      // Record all the IDs of previously known comments
+      const legacyCommentIds = new Set<string>();
+      legacyComments.forEach((existingComment) => legacyCommentIds.add(existingComment.id));
+
+      // Determine which "new" comments are actually new
+      const usjRW = new UsjReaderWriter(usjWithAnchors);
+      const newLegacyComments = convertEditorCommentsToLegacyComments(newComments, usjRW, scrRef);
+      const legacyCommentsToAdd: LegacyComment[] = [];
+      newLegacyComments.forEach((newComment) => {
+        if (!legacyCommentIds.has(newComment.id)) legacyCommentsToAdd.push(newComment);
+      });
+
+      // Nothing to do
+      if (legacyCommentsToAdd.length === 0) return;
+
+      // Save the new comments to the data provider
+      const newCommentArray = [...legacyComments, ...legacyCommentsToAdd];
+      setLegacyComments(newCommentArray);
+    },
+    [legacyComments, scrRef, setLegacyComments],
+  );
+
+  const insertCommentAnchors = useCallback(
+    (newUsj: Usj) => {
+      try {
+        insertCommentAnchorsIntoUsj(newUsj, legacyComments);
+        // Getting more information about the error
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        logger.debug(`Error inserting anchors into USJ: ${error}, stack = ${error.stack}"`);
+      }
+    },
+    [legacyComments],
+  );
 
   // TODO: remove debounce when issue #826 is done.
   const onUsjChange = useCallback(
-    (newUsj: Usj) => {
+    (newUsj: Usj, newComments: Comments | undefined) => {
+      insertCommentAnchors(newUsj);
+      updateComments(newComments, newUsj);
       editorUsj.current = newUsj;
       debouncedSetUsj(newUsj);
     },
-    [debouncedSetUsj],
+    [debouncedSetUsj, insertCommentAnchors, updateComments],
   );
 
   // Update the editor if a change comes in
   useEffect(() => {
+    // We don't know if the USJ or comments will change first, but the USJ has to be correct before
+    // comments can be set
+
     // Deep compare the old and current state of the usj to make sure we don't change the editor's
     // state without a need. Note that it already does that internally using a different algorithm,
     // but we need to compare in such a way that the same object across iframes works fine
-    if (usj && !deepEqualAcrossIframes(usj, editorUsj.current)) {
+    if (
+      usj &&
+      legacyComments &&
+      editorRef.current &&
+      (!deepEqualAcrossIframes(usj, editorUsj.current) ||
+        !deepEqualAcrossIframes(legacyComments, editorLegacyComments.current))
+    ) {
+      insertCommentAnchors(usj);
       editorUsj.current = usj;
-      editorRef.current?.setUsj(usj);
+      editorRef.current.setUsj(usj);
+
+      if ('setComments' in editorRef.current) {
+        // Make sure that USJ has been loaded or setting comments will fail because the editor
+        // won't see any anchors for them
+        const threads = convertLegacyCommentsToEditorThreads(legacyComments);
+        editorLegacyComments.current = legacyComments;
+        editorRef.current.setComments?.(threads);
+      }
     }
-  }, [usj]);
+  }, [insertCommentAnchors, legacyComments, usj]);
 
   // On loading the first time, scroll the selected verse into view
   useEffect(() => {
@@ -178,15 +286,28 @@ globalThis.webViewComponent = function PlatformScriptureEditor({
     [isReadOnly, projectName, scrRef],
   );
 
-  return (
+  return isReadOnly ? (
     <>
       {/* Workaround to pull in platform-bible-react styles into the editor */}
       <Button className="tw-hidden" />
-      <Editor
+      <Editorial
         ref={editorRef}
         scrRef={scrRef}
         onScrRefChange={setScrRef}
-        onUsjChange={isReadOnly ? undefined : onUsjChange}
+        options={options}
+        logger={logger}
+      />
+    </>
+  ) : (
+    <>
+      {/* Workaround to pull in platform-bible-react styles into the editor */}
+      <Button className="tw-hidden" />
+      <Marginal
+        ref={editorRef}
+        scrRef={scrRef}
+        onScrRefChange={setScrRef}
+        onUsjChange={onUsjChange}
+        onCommentChange={updateComments}
         options={options}
         logger={logger}
       />
