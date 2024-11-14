6aaee4e5bcad85db6c5d8d541509d054940fb346
tjcouch-sil <tj_couch@sil.org>
2024-11-14 12:48:44 -0600

    Updated markdown renderer, added localhost:* to CSP connect-src

--- lib/platform-bible-react/src/components/advanced/extension-marketplace/markdown-renderer.component.tsx
+++ lib/platform-bible-react/src/components/advanced/extension-marketplace/markdown-renderer.component.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/utils/shadcn-ui.util';
-import Markdown from 'markdown-to-jsx';
+import Markdown, { MarkdownToJSX } from 'markdown-to-jsx';
+import { useMemo } from 'react';
 
 interface MarkdownRendererProps {
   /** Optional unique identifier */
@@ -7,6 +8,11 @@ interface MarkdownRendererProps {
   /** The markdown string to render */
   markdown: string;
   className?: string;
+  /**
+   * The []`target` attribute for `a` html
+   * tags](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#target). Defaults to `_self`
+   */
+  anchorTarget?: string;
 }
 
 /**
@@ -17,10 +23,27 @@ interface MarkdownRendererProps {
  * @param id Optional unique identifier
  * @returns A div containing the rendered markdown content.
  */
-export default function MarkdownRenderer({ id, markdown, className }: MarkdownRendererProps) {
+export default function MarkdownRenderer({
+  id,
+  markdown,
+  className,
+  anchorTarget,
+}: MarkdownRendererProps) {
+  const options: MarkdownToJSX.Options = useMemo(
+    () => ({
+      overrides: {
+        a: {
+          props: {
+            target: anchorTarget ?? '_self',
+          },
+        },
+      },
+    }),
+    [anchorTarget],
+  );
   return (
     <div id={id} className={cn('pr-twp tw-prose', className)}>
-      <Markdown>{markdown}</Markdown>
+      <Markdown options={options}>{markdown}</Markdown>
     </div>
   );
 }
