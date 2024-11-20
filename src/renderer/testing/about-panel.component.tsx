18f30c3568fb2f87e14350fe758abde175d04d83
tjcouch-sil <tj_couch@sil.org>
2024-11-20 10:15:47 -0600

    Added internet settings to Paratext registration info form, fixed disabling internet in sensitive locations

--- src/renderer/testing/about-panel.component.tsx
+++ src/renderer/testing/about-panel.component.tsx
@@ -2,14 +2,23 @@ import { useLocalizedStrings } from '@renderer/hooks/papi-hooks';
 import { SavedTabInfo, TabInfo } from '@shared/models/docking-framework.model';
 import { ReactComponent as InlineLogoAndName } from '@assets/Lockup Inline.svg';
 import { formatReplacementString, LocalizeKey } from 'platform-bible-utils';
+import { ReactElement } from 'react';
 import PackageInfo from '../../../release/app/package.json';
 
 export const TAB_TYPE_ABOUT = 'about';
+
+const DB_IP_WEBSITE_LINK = 'https://db-ip.com';
+const DB_IP_WEBSITE_NAME = 'DB-IP';
+const DB_IP_LICENSE = 'CC BY 4.0';
+const DB_IP_ATTRIBUTION_LINK = 'https://creativecommons.org/licenses/by/4.0/';
 
 const STRING_KEYS: LocalizeKey[] = [
   '%product_name%',
   '%about_versionLabel_format%',
   '%about_licenseLabel_format%',
+  '%about_db_ip_attribution_format%',
+  '%about_db_ip_attribution_intro%',
+  '%about_db_ip_attribution_terms%',
 ];
 
 export default function AboutPanel() {
@@ -18,8 +27,24 @@ export default function AboutPanel() {
       '%product_name%': productName,
       '%about_versionLabel_format%': versionLabelFormat,
       '%about_licenseLabel_format%': licenseLabelFormat,
+      '%about_db_ip_attribution_format%': dbIpAttributionFormat,
+      '%about_db_ip_attribution_intro%': dbIpAttributionIntro,
+      '%about_db_ip_attribution_terms%': dbIpAttributionTerms,
     },
   ] = useLocalizedStrings(STRING_KEYS);
+
+  const attributionLinkElements: { [linkName: string]: ReactElement } = {
+    websiteLink: (
+      <a target="_blank" rel="noreferrer" href={DB_IP_WEBSITE_LINK}>
+        {DB_IP_WEBSITE_NAME}
+      </a>
+    ),
+    terms: (
+      <a target="_blank" rel="noreferrer" href={DB_IP_ATTRIBUTION_LINK}>
+        {dbIpAttributionTerms}
+      </a>
+    ),
+  };
 
   return (
     <div className="about-panel dark">
@@ -27,9 +52,27 @@ export default function AboutPanel() {
         <InlineLogoAndName width="80%" />
         <h1>{productName}</h1>
         <p>Copyright © 2022-2024 SIL International</p>
-        <p>{formatReplacementString(versionLabelFormat, { version: PackageInfo.version })}</p>
-        <p>{formatReplacementString(licenseLabelFormat, { license: PackageInfo.license })}</p>
+        <p>{formatReplacementString(versionLabelFormat, PackageInfo)}</p>
+        <p>{formatReplacementString(licenseLabelFormat, PackageInfo)}</p>
         <p>{PackageInfo.description}</p>
+        <p>
+          {formatReplacementString(dbIpAttributionFormat, {
+            intro: dbIpAttributionIntro,
+            websiteLink: '{websiteLink}',
+            license: DB_IP_LICENSE,
+            terms: '{terms}',
+          })
+            .split('{')
+            .flatMap((attributionPart) => {
+              const closingBraceIndex = attributionPart.indexOf('}');
+              if (closingBraceIndex < 0) return attributionPart;
+              const linkName = attributionPart.substring(0, closingBraceIndex);
+              return [
+                attributionLinkElements[linkName],
+                attributionPart.substring(closingBraceIndex + 1),
+              ];
+            })}
+        </p>
       </div>
     </div>
   );
