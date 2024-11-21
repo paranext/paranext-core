f6c35d9958f3350cbda66e5b1ffa0cc1bb835e98
Matt Lyons <matt_lyons@sil.org>
2024-11-21 12:51:24 -0600

    Add subscriptions to the check aggregator to enable multiple webviews

--- c-sharp/Checks/CheckRunResult.cs
+++ c-sharp/Checks/CheckRunResult.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using SIL.Scripture;
 
 namespace Paranext.DataProvider.Checks;
 
@@ -6,54 +7,46 @@ namespace Paranext.DataProvider.Checks;
 /// Represents a single error/issue flagged by a check in a given project. This class must
 /// serialize/deserialize to the CheckRunResult type defined in TypeScript.
 /// </summary>
-public sealed class CheckRunResult(
-    string checkId,
-    string projectId,
-    string messageFormatString,
-    string text,
-    CheckLocation start,
-    CheckLocation end
+public sealed record CheckRunResult(
+    string CheckId,
+    string CheckResultType,
+    string ProjectId,
+    string MessageFormatString,
+    [property: JsonIgnore] string Text,
+    bool IsDenied,
+    VerseRef VerseRef,
+    CheckLocation Start,
+    CheckLocation End
 ) : IEquatable<CheckRunResult>
 {
-    public string CheckId { get; } = checkId;
-    public string ProjectId { get; } = projectId;
-    public string MessageFormatString { get; } = messageFormatString;
-
-    [JsonIgnore]
-    public string Text { get; } = text;
-    public CheckLocation Start { get; } = start;
-    public CheckLocation End { get; } = end;
-
-    public override bool Equals(object? obj)
-    {
-        return obj is CheckRunResult checkRunResult && Equals(checkRunResult);
-    }
-
     public bool Equals(CheckRunResult? other)
     {
-        if (ReferenceEquals(other, null))
+        if (other is null)
             return false;
 
         return CheckId == other.CheckId
+            && CheckResultType == other.CheckResultType
             && ProjectId == other.ProjectId
             && MessageFormatString == other.MessageFormatString
             && Text == other.Text
+            && IsDenied == other.IsDenied
+            && VerseRef.ToStringWithVersification() == other.VerseRef.ToStringWithVersification()
             && Start == other.Start
             && End == other.End;
     }
 
-    public static bool operator ==(CheckRunResult a, CheckRunResult b)
-    {
-        return a.Equals(b);
-    }
-
-    public static bool operator !=(CheckRunResult a, CheckRunResult b)
-    {
-        return !(a == b);
-    }
-
     public override int GetHashCode()
     {
-        return HashCode.Combine(CheckId, ProjectId, MessageFormatString, Text, Start, End);
+        int hash = HashCode.Combine(
+            CheckId,
+            CheckResultType,
+            ProjectId,
+            MessageFormatString,
+            Text,
+            IsDenied,
+            VerseRef,
+            Start
+        );
+        return HashCode.Combine(hash, End);
     }
 }
