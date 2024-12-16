66368abb63cdc871f6e2783683cdc33458e2772e
Rolf Heij <rolf_heij@sil.org>
2024-12-16 11:40:51 +0100

    Remove package

--- extensions/src/platform-scripture/jest.config.ts
+++ extensions/src/platform-scripture/jest.config.ts
@@ -6,7 +6,6 @@ module.exports = {
   transform: {
     '\\.(ts|tsx|js|jsx)$': 'ts-jest',
   },
-  transformIgnorePatterns: ['node_modules/(?!@thetypefounders/unicode-classifier/.*)'],
   moduleNameMapper: {
     '^@platform-scripture/(.*)$': '$1',
   },
