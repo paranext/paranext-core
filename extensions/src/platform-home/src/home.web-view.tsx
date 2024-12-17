b20668fa4ac631e558a226e9090f82d2cdb705ae
Rolf Heij <rolf_heij@sil.org>
2024-12-17 10:38:10 +0100

    Get some projs

--- extensions/src/platform-home/src/home.web-view.tsx
+++ extensions/src/platform-home/src/home.web-view.tsx
@@ -2,7 +2,12 @@ import { WebViewProps } from '@papi/core';
 import papi from '@papi/frontend';
 import { useData, useDataProvider, useLocalizedStrings } from '@papi/frontend/react';
 import type { ResourceType } from 'platform-bible-utils';
-import { FilterableResourceList, FILTERABLE_RESOURCE_LIST_STRING_KEYS } from 'platform-bible-react';
+import {
+  FilterableResourceList,
+  FILTERABLE_RESOURCE_LIST_STRING_KEYS,
+  usePromise,
+} from 'platform-bible-react';
+import { useCallback } from 'react';
 
 globalThis.webViewComponent = function GetResourcesDialog({ useWebViewState }: WebViewProps) {
   const [localizedStrings] = useLocalizedStrings(FILTERABLE_RESOURCE_LIST_STRING_KEYS);
@@ -21,6 +26,45 @@ globalThis.webViewComponent = function GetResourcesDialog({ useWebViewState }: W
 
   const openResource = (projectId: string) =>
     papi.commands.sendCommand('platformScriptureEditor.openResourceViewer', projectId);
+
+  const [projects] = usePromise(
+    useCallback(async () => {
+      const projectMetadata = await papi.projectLookup.getMetadataForAllProjects({
+        includeProjectInterfaces: ['platformScripture.USJ_Chapter'],
+      });
+      const projectInfo = await Promise.all(
+        projectMetadata.map(async (data) => {
+          const pdp = await papi.projectDataProviders.get('platform.base', data.id);
+          return {
+            projectId: data.id,
+            isEditable: await pdp.getSetting('platform.isEditable'),
+            fullName: await pdp.getSetting('platform.fullName'),
+          };
+        }),
+      );
+      return projectInfo;
+    }, []),
+    undefined,
+  );
+
+  console.log('editable projs:', projects);
+
+  // const [sharedProjectsInfo] = usePromise(
+  //   useCallback(async () => {
+  //     try {
+  //       const projectsInfo = await papi.commands.sendCommand(
+  //         'paratextBibleSendReceive.getSharedProjects',
+  //       );
+  //       return projectsInfo;
+  //     } catch (e) {
+  //       // setGetSharedProjectsError(getErrorMessage(e));
+  //       return undefined;
+  //     }
+  //   }, []),
+  //   undefined,
+  // );
+
+  // console.log('S/R projs:', sharedProjectsInfo);
 
   return (
     <FilterableResourceList
