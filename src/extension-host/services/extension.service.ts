0741e0fb8067c11f232e4f6024b391467325bcc6
tjcouch-sil <tj_couch@sil.org>
2025-01-15 15:26:31 -0600

    Fixed URL.parse problem

--- src/extension-host/services/extension.service.ts
+++ src/extension-host/services/extension.service.ts
@@ -848,9 +848,13 @@ function createRegisterUriHandlerFunction(manifest: ExtensionManifest): Register
 }
 
 function handleExtensionUri(uri: string) {
-  const url = URL.parse(uri);
-  if (!url) {
-    logger.warn(`Extension service received uri ${uri} but could not parse it`);
+  // need to use `new URL` instead of `URL.parse` because Node<22.1.0 doesn't have it. Can change
+  // when we get there
+  let url: URL;
+  try {
+    url = new URL(uri);
+  } catch (e) {
+    logger.warn(`Extension service received uri ${uri} but could not parse it. ${e}`);
     return;
   }
   if (url.protocol !== `${APP_URI_SCHEME}:`) {
@@ -867,7 +871,7 @@ function handleExtensionUri(uri: string) {
   const uriHandler = uriHandlersByExtensionKey.get(extensionKey);
 
   if (!uriHandler) {
-    logger.debug(
+    logger.warn(
       `Extension service received uri ${uri}, but there was not a registered URI handler for ${extensionKey}`,
     );
     return;
