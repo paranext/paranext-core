bc40fc2d53a607886925bce043f38cae2a7a6b1b
Ira Hopkinson <ira_hopkinson@sil.org>
2025-02-17 12:30:20 +1300

    - fix ESM-only in tests (from `vite-tsconfig-paths`)

--- lib/platform-bible-react/vite.config.ts
+++ lib/platform-bible-react/vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
 import styleInject from '@senojs/rollup-plugin-style-inject';
@@ -36,6 +36,10 @@ const config = defineConfig({
         },
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
 });
 export default config;
