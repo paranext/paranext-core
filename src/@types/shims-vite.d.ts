4700746100b53cd159d3bd52cc33c34326dd7fd9
Ira Hopkinson <ira_hopkinson@sil.org>
2025-02-21 15:01:45 +1300

    - code review
    - rebase and rebuild after PR #1453 was merged

--- src/@types/shims-vite.d.ts
+++ src/@types/shims-vite.d.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-// vite is shimmed since we only use it for vitest and this excludes it from type checking.
+// vite is shimmed since we only use it for vitest and this excludes it from type checking. Note to
+// use this a path for 'vite' is added in `tsconfig.json`.
+// Without this, when running `npm run typecheck:core` we get this error:
+//   node_modules/vite/dist/node/index.d.ts:5:41 - error TS2307: Cannot find module 'rollup/parseAst' or its corresponding type declarations.
 declare module 'vite' {
   export type TransformResult = any;
   export type ServerOptions = any;
