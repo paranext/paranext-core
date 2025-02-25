1c042b69c061f6a1d4bf9d6362f05f958d516ff3
Sebastian-ubs <slingk@biblesocieties.org>
2025-02-25 18:45:00 +0100

    build + use the popover color for Menubar

--- lib/platform-bible-react/src/components/shadcn-ui/menubar.tsx
+++ lib/platform-bible-react/src/components/shadcn-ui/menubar.tsx
@@ -136,7 +136,7 @@ const MenubarSubContent = React.forwardRef<
         'tw-z-50 tw-min-w-[8rem] tw-overflow-hidden tw-rounded-md tw-border tw-bg-popover tw-p-1 tw-text-popover-foreground data-[state=open]:tw-animate-in data-[state=closed]:tw-animate-out data-[state=closed]:tw-fade-out-0 data-[state=open]:tw-fade-in-0 data-[state=closed]:tw-zoom-out-95 data-[state=open]:tw-zoom-in-95 data-[side=bottom]:tw-slide-in-from-top-2 data-[side=left]:tw-slide-in-from-right-2 data-[side=right]:tw-slide-in-from-left-2 data-[side=top]:tw-slide-in-from-bottom-2',
         // CUSTOM
         {
-          'tw-bg-primary-foreground': context.variant === 'muted',
+          'tw-bg-popover': context.variant === 'muted',
         },
         className,
       )}
@@ -163,7 +163,7 @@ const MenubarContent = React.forwardRef<
           // CUSTOM
           'pr-twp',
           {
-            'tw-bg-primary-foreground': context.variant === 'muted',
+            'tw-bg-popover': context.variant === 'muted',
           },
           className,
         )}
@@ -200,46 +200,56 @@ MenubarItem.displayName = MenubarPrimitive.Item.displayName;
 const MenubarCheckboxItem = React.forwardRef<
   React.ElementRef<typeof MenubarPrimitive.CheckboxItem>,
   React.ComponentPropsWithoutRef<typeof MenubarPrimitive.CheckboxItem>
->(({ className, children, checked, ...props }, ref) => (
-  <MenubarPrimitive.CheckboxItem
-    ref={ref}
-    className={cn(
-      'tw-relative tw-flex tw-cursor-default tw-select-none tw-items-center tw-rounded-sm tw-py-1.5 tw-pl-8 tw-pr-2 tw-text-sm tw-outline-none focus:tw-bg-accent focus:tw-text-accent-foreground data-[disabled]:tw-pointer-events-none data-[disabled]:tw-opacity-50',
-      className,
-    )}
-    checked={checked}
-    {...props}
-  >
-    <span className="tw-absolute tw-left-2 tw-flex tw-h-3.5 tw-w-3.5 tw-items-center tw-justify-center">
-      <MenubarPrimitive.ItemIndicator>
-        <Check className="tw-h-4 tw-w-4" />
-      </MenubarPrimitive.ItemIndicator>
-    </span>
-    {children}
-  </MenubarPrimitive.CheckboxItem>
-));
+>(({ className, children, checked, ...props }, ref) => {
+  const context = useContext();
+  return (
+    <MenubarPrimitive.CheckboxItem
+      ref={ref}
+      className={cn(
+        'tw-relative tw-flex tw-cursor-default tw-select-none tw-items-center tw-rounded-sm tw-py-1.5 tw-pl-8 tw-pr-2 tw-text-sm tw-outline-none focus:tw-bg-accent focus:tw-text-accent-foreground data-[disabled]:tw-pointer-events-none data-[disabled]:tw-opacity-50',
+        // CUSTOM
+        mutedMenuElementClasses(context),
+        className,
+      )}
+      checked={checked}
+      {...props}
+    >
+      <span className="tw-absolute tw-left-2 tw-flex tw-h-3.5 tw-w-3.5 tw-items-center tw-justify-center">
+        <MenubarPrimitive.ItemIndicator>
+          <Check className="tw-h-4 tw-w-4" />
+        </MenubarPrimitive.ItemIndicator>
+      </span>
+      {children}
+    </MenubarPrimitive.CheckboxItem>
+  );
+});
 MenubarCheckboxItem.displayName = MenubarPrimitive.CheckboxItem.displayName;
 
 const MenubarRadioItem = React.forwardRef<
   React.ElementRef<typeof MenubarPrimitive.RadioItem>,
   React.ComponentPropsWithoutRef<typeof MenubarPrimitive.RadioItem>
->(({ className, children, ...props }, ref) => (
-  <MenubarPrimitive.RadioItem
-    ref={ref}
-    className={cn(
-      'tw-relative tw-flex tw-cursor-default tw-select-none tw-items-center tw-rounded-sm tw-py-1.5 tw-pl-8 tw-pr-2 tw-text-sm tw-outline-none focus:tw-bg-accent focus:tw-text-accent-foreground data-[disabled]:tw-pointer-events-none data-[disabled]:tw-opacity-50',
-      className,
-    )}
-    {...props}
-  >
-    <span className="tw-absolute tw-left-2 tw-flex tw-h-3.5 tw-w-3.5 tw-items-center tw-justify-center">
-      <MenubarPrimitive.ItemIndicator>
-        <Circle className="tw-h-2 tw-w-2 tw-fill-current" />
-      </MenubarPrimitive.ItemIndicator>
-    </span>
-    {children}
-  </MenubarPrimitive.RadioItem>
-));
+>(({ className, children, ...props }, ref) => {
+  const context = useContext();
+  return (
+    <MenubarPrimitive.RadioItem
+      ref={ref}
+      className={cn(
+        'tw-relative tw-flex tw-cursor-default tw-select-none tw-items-center tw-rounded-sm tw-py-1.5 tw-pl-8 tw-pr-2 tw-text-sm tw-outline-none focus:tw-bg-accent focus:tw-text-accent-foreground data-[disabled]:tw-pointer-events-none data-[disabled]:tw-opacity-50',
+        // CUSTOM
+        mutedMenuElementClasses(context),
+        className,
+      )}
+      {...props}
+    >
+      <span className="tw-absolute tw-left-2 tw-flex tw-h-3.5 tw-w-3.5 tw-items-center tw-justify-center">
+        <MenubarPrimitive.ItemIndicator>
+          <Circle className="tw-h-2 tw-w-2 tw-fill-current" />
+        </MenubarPrimitive.ItemIndicator>
+      </span>
+      {children}
+    </MenubarPrimitive.RadioItem>
+  );
+});
 MenubarRadioItem.displayName = MenubarPrimitive.RadioItem.displayName;
 
 const MenubarLabel = React.forwardRef<
