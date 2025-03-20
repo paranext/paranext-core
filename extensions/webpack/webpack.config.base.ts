8997a91ab0846019307699bfc22ce9ff27828e04
Rolf Heij <rolf_heij@sil.org>
2025-03-20 16:05:42 +0100

    Remove Emotion stuff

--- extensions/webpack/webpack.config.base.ts
+++ extensions/webpack/webpack.config.base.ts
@@ -143,16 +143,6 @@ const configBase: webpack.Configuration = {
       // use tsconfig.json paths https://www.npmjs.com/package/tsconfig-paths-webpack-plugin
       new TsconfigPathsPlugin(),
     ],
-    // Load `platform-bible-react`' `dependencies` from `paranext-core` so the extension will share
-    // these dependencies with the bundled copy of `platform-bible-react` and avoid duplicate
-    // packages. These paths are broken up like this so multi-extension folder can format the path
-    // properly
-    // https://webpack.js.org/configuration/resolve/#resolvealias
-    // TODO: Remove this when `platform-bible-react` is published to npm
-    alias: {
-      '@emotion/react': path.resolve(__dirname, '..', '../node_modules/@emotion/react'),
-      '@emotion/styled': path.resolve(__dirname, '..', '../node_modules/@emotion/styled'),
-    },
   },
 };
 
