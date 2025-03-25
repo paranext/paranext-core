60d144a0580006396c0dd0ba8c8e042646de1633
rolfheij-sil <108285668+rolfheij-sil@users.noreply.github.com>
2025-03-25 13:50:06 +0000

    Deploying to github-pages from @ paranext/paranext-core@112255e2c67ce85edff70872d236d95c42cb4a1e 🚀

--- platform-bible-react-storybook/assets/preview-C0p7j47a.js
+++
@@ -1,1 +0,0 @@
-var t="viewport",o={[t]:{value:void 0,isRotated:!1}},i={viewport:"reset",viewportRotated:!1},e,l=(e=globalThis.FEATURES)!=null&&e.viewportStoryGlobals?o:i;export{l as initialGlobals};
