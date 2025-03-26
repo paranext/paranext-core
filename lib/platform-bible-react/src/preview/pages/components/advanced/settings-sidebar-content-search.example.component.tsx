97f71872aca6b7c11435b88dbaa6f7b923fd989f
Sebastian-ubs <slingk@biblesocieties.org>
2025-03-26 19:58:58 +0100

    fix sidebar colors and general selection/hover contrast
    - correctly define sidebar variables in tailwind.config.ts
    - change color values in index.css for better contrast
    - fix colors of settings sidebar
    - add a muted variant for sidebar
    - improve examples for preview app

--- lib/platform-bible-react/src/preview/pages/components/advanced/settings-sidebar-content-search.example.component.tsx
+++ lib/platform-bible-react/src/preview/pages/components/advanced/settings-sidebar-content-search.example.component.tsx
@@ -1,25 +1,31 @@
 import { SettingsSidebarContentSearch } from '@/components/advanced/settings-components/settings-sidebar-content-search.component';
+import { SelectedSettingsSidebarItem } from '@/components/advanced/settings-components/settings-sidebar.component';
 import { useState } from 'react';
 
 export function SettingSidebarContentSearchExamples() {
   const [searchQuery, setSearchQuery] = useState<string>('');
+  const [selectedSidebarItem, setSelectedSidebarItem] = useState<SelectedSettingsSidebarItem>({
+    label: 'A',
+    projectId: undefined,
+  });
 
   return (
     <div>
       <SettingsSidebarContentSearch
         buttonPlaceholderText="?? Select a project"
-        extensionLabels={{ A: 'A', B: 'B', C: 'C' }}
+        extensionLabels={{ A: 'Aaaaaa', B: 'Bbbbb', C: 'Ccccccccccccccccccc cccccccccccc' }}
         projectInfo={[
           { projectName: 'a', projectId: '1' },
           { projectName: 'b', projectId: '2' },
           { projectName: 'c', projectId: '3' },
         ]}
-        selectedSidebarItem={{ label: '', projectId: '' }}
+        selectedSidebarItem={selectedSidebarItem}
         projectsSidebarGroupLabel="Projects Settings"
         extensionsSidebarGroupLabel="General Settings"
-        handleSelectSidebarItem={(key: string, projId?: string) =>
-          console.log('Selected:', key, projId)
-        }
+        handleSelectSidebarItem={(key: string, projId?: string) => {
+          setSelectedSidebarItem({ label: key, projectId: projId });
+          console.log('Selected:', key, projId);
+        }}
         searchValue={searchQuery}
         onSearch={(query) => {
           setSearchQuery(query);
