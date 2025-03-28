43cc0d7687b9bf26d173c82761e1e1402930d15c
Rolf Heij <rolf_heij@sil.org>
2025-03-28 14:36:31 +0100

    Fix typo in comment

--- src/shared/models/web-view.model.ts
+++ src/shared/models/web-view.model.ts
@@ -209,7 +209,7 @@ type WebViewDefinitionBase = {
    */
   allowPopups?: boolean;
   /**
-   * Whether a toolbar should be displayed at the top of the toolbar. Currently this toolbar cannot
+   * Whether a toolbar should be displayed at the top of the web view. Currently this toolbar cannot
    * be modified and is provided as is. It displays a BookChapterControl and a ScrollGroupSelector.
    */
   shouldShowToolbar?: boolean;
