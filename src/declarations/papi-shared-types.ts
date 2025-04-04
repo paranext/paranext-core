f604bd2da3ad071bb4370c9561655b3f1b427c7a
Sebastian-ubs <141921979+Sebastian-ubs@users.noreply.github.com>
2025-04-04 18:58:11 +0200

    add icons to external links in help menu (#1536)
    * add icons to external calls in help menu
    
    * about to be the last entry always

--- src/declarations/papi-shared-types.ts
+++ src/declarations/papi-shared-types.ts
@@ -69,7 +69,17 @@ declare module 'papi-shared-types' {
     'platform.openDeveloperDocumentationUrl': () => Promise<void>;
     /**
      * Open a link in a new browser window. Like `window.open` in the frontend with
-     * `target='_blank'`
+     * `target='_blank'` Consider using a visual indication along with this. E.g. for a menu
+     * download the https://lucide.dev/icons/external-link icon, add it to your extension's assets
+     * folder and use it like
+     *
+     * - `"iconPathAfter": "papi-extension://<yourExtension>/assets/icons/external-link.svg"`. We plan
+     *   to provide a common set of icons via an API in the future.
+     *
+     * For a button that opens external urls add
+     *
+     * - `aria-label="{localizedStrings['%ariaLabel_opensInBrowser%']}"`
+     * - Lucide icon `<ExternalLink />`
      */
     'platform.openWindow': (url: string) => Promise<void>;
 
