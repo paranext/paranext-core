237416137af066ab98dc8791eb24cdd4a7e8c206
tombogle <tom_bogle@sil.org>
2025-04-10 16:05:13 -0400

    PT-2973: Removed imports no longer needed

--- extensions/src/hello-world/src/main.ts
+++ extensions/src/hello-world/src/main.ts
@@ -7,7 +7,7 @@ import type {
   WebViewDefinition,
 } from '@papi/core';
 import type { HelloWorldEvent, HelloWorldProjectWebViewController } from 'hello-world';
-import { PlatformError, PlatformEventEmitter } from 'platform-bible-utils';
+import { PlatformEventEmitter } from 'platform-bible-utils';
 import { checkDetails, createHelloCheck } from './checks';
 import { HelloWorldProjectDataProviderEngineFactory } from './models/hello-world-project-data-provider-engine-factory.model';
 import { HELLO_WORLD_PROJECT_INTERFACES } from './models/hello-world-project-data-provider-engine.model';
