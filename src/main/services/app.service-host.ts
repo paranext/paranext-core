b8d04fee467ccd76cf9228e45545544ffe511108
jolierabideau <135999578+jolierabideau@users.noreply.github.com>
2025-04-11 15:03:18 -0400

    Release label in toolbar (#1582)
    add release label to toolbar, add ghost variant to badge, add getMarketingInfo to app service
    
    Co-authored-by: Jolie Rabideau <jolierabideau@MacBookPro.attlocal.net>

--- src/main/services/app.service-host.ts
+++ src/main/services/app.service-host.ts
@@ -2,6 +2,7 @@ import {
   AppInfo,
   appServiceNetworkObjectName,
   IAppService,
+  MarketingInfo,
 } from '@shared/services/app.service-model';
 import { networkObjectService } from '@shared/services/network-object.service';
 import packageInfo from '../../../release/app/package.json';
@@ -18,18 +19,32 @@ export const APP_VERSION: string = `${packageInfo.version}${build ? `+${build}` 
 /** Same as {@link AppInfo.uriScheme} */
 export const APP_URI_SCHEME = APP_NAME;
 
+export const MARKETING_VERSION = packageInfo.marketingVersion;
+
+export const MARKETING_VERSION_MONIKER = packageInfo.marketingVersionMoniker;
+
 const APP_INFO = Object.freeze({
   name: APP_NAME,
   version: APP_VERSION,
   uriScheme: APP_URI_SCHEME,
 });
 
+const MARKETING_INFO = Object.freeze({
+  marketingVersion: MARKETING_VERSION,
+  marketingVersionMoniker: MARKETING_VERSION_MONIKER,
+});
+
 async function getAppInfo(): Promise<AppInfo> {
   return APP_INFO;
 }
 
+async function getMarketingInfo(): Promise<MarketingInfo> {
+  return MARKETING_INFO;
+}
+
 const appService: IAppService = {
   getAppInfo,
+  getMarketingInfo,
 };
 
 /**
