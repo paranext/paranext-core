536ef154014a3d9c41bcd074b3f5419e3053dca8
tjcouch-sil <tj_couch@sil.org>
2025-04-14 18:12:09 -0500

    Changed isRightToLeft to textDirection to enable us to change default behavior later if we find appropriate

--- src/declarations/papi-shared-types.ts
+++ src/declarations/papi-shared-types.ts
@@ -231,10 +231,13 @@ declare module 'papi-shared-types' {
      */
     'platform.isEditable': boolean;
     /**
-     * Whether the project's text flows from right to left instead of from left to right. This is
-     * generally derived from the language definition for the project's language code.
+     * Which way the project's text flows. 'ltr' = left-to-right; 'rtl' = right-to-left. '' or
+     * undefined = left-to-right (may be changed in the future to detect). Defaults to ''. This is
+     * generally derived from the language definition for the project's language code. Note that
+     * additional string options may be added in the future, so handle accordingly. Adding
+     * additional options will not be considered a breaking change.
      */
-    'platform.isRightToLeft': boolean;
+    'platform.textDirection': 'ltr' | 'rtl' | '' | undefined;
   }
 
   /**
