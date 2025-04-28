7b43857138cbdbe12266a899351a2e7300ea941d
Sebastian-ubs <slingk@biblesocieties.org>
2025-04-28 13:00:40 +0200

    move home to pbr, add proj-res filter

---
+++ lib/platform-bible-react/src/preview/pages/components/basics/projectResourceFilter.examples.component.tsx
@@ -0,0 +1,10 @@
+import ProjectResourceFilter from '@/components/basics/projectResourceFilter.component';
+
+export function ProjectResourceFilterExamples() {
+  return (
+    <>
+      <ProjectResourceFilter onChange={() => {}} />
+      <ProjectResourceFilter onChange={() => {}} variant="ghost" />
+    </>
+  );
+}
