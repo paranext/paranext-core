222da73d6304dd8cb16b7f7fe136c41ffa83b8af
Alex Mercado <alex_mercado@sil.org>
2025-05-25 16:44:22 -0500

    Lint: Remove unused `import React`

--- lib/platform-bible-react/src/stories/advanced/tab-toolbar.stories.tsx
+++ lib/platform-bible-react/src/stories/advanced/tab-toolbar.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { defaultScrRef, getLocalizeKeyForScrollGroupId } from 'platform-bible-utils';
 import { ScrollGroupSelector } from '@/components/advanced/scroll-group-selector.component';
