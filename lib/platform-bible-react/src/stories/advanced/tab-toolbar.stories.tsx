ed042ad0ef825fb9d85e812506972e1e03afba9b
katherinejensen00 <katherinejensen00@gmail.com>
2025-05-28 03:39:52 -0700

    Pt 3169 tab toolbar basic (#1627)
    * First Attempt at Tab Toolbar
    
    * Visually hide slot children if they overflow
    Doesn't work for `:first-child`ren
    
    * Fixed a file affected by changes to PlatformTabTitle
    
    * Fix prettier style recommendations
    
    * Reduce and clean up
    
    * Remove background colors used for troubleshooting
    
    * Replace hard-coded BCV text input field width to one based on content
    
    * Add an animated Story to see tab-toolbar change widths
    
    * Improve performance of Storybook viewport animation
    
    * Animating Storybook container style tweaks
    
    * Add Tailwind container queries
    
    * Remove non-semantic divs
    
    * * Make TabToolbar center area disappear first
    * Make the View menu trigger disappear last
    
    * * Move ProjectMenuTrigger into its own flex item
    * Storybook: Add more example toolbar items
    * Add size styling to BCV Input to help it shrink
    
    * Fine-tune resize priorities and wrapping
    
    * Lint cleanup
    
    * Make AnimatedContainer a reusable Storybook decorator
    
    * AnimatedContainer gets more useful params
    
    * Linting
    
    * Lint: Remove unused `import React`
    
    * Lint macOS workflow: create a macports user
    
    * Use the `@/` path notation instead of `../../`
    https://reviewable.io/reviews/paranext/paranext-core/1627#-ORGAqhJEdNArCKmUQsR
    
    * Add gap spacing in the Tab Heading Title to compensate spacing that previously came from the icon button there
    
    * Fix: in select.tsx, point to select Shadcn docs instead of to button docs
    
    * Added ScrollGroupSelector to Storybook
    
    * Tab Header Title: replace tw-gap-1 with SCSS
    (Tailwind can't be reliably used in docking framework)
    
    ---------
    
    Co-authored-by: Alex Mercado <alex_mercado@sil.org>

---
+++ lib/platform-bible-react/src/stories/advanced/tab-toolbar.stories.tsx
@@ -0,0 +1,213 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { defaultScrRef, getLocalizeKeyForScrollGroupId } from 'platform-bible-utils';
+import { ScrollGroupSelector } from '@/components/advanced/scroll-group-selector.component';
+import { BookChapterControl } from '@/components/advanced/book-chapter-control/book-chapter-control.component';
+import { TabToolbar, TabToolbarProps } from '@/components/advanced/tab-toolbar.component';
+import { AnimatedContainer } from '@/storybook/decorators/animated-container';
+
+const setScrollGroupId = (newScrollGroupId: number | undefined) => {
+  console.log('New Scroll Group Id: ', newScrollGroupId);
+};
+
+const projectMenuData = {
+  columns: {
+    tools: { label: 'Tools', order: 1 },
+    info: { label: 'Info', order: 2 },
+    project: { label: 'Project', order: 3 },
+  },
+  groups: {
+    general: { column: 'info', order: 1 },
+    inventory: { column: 'tools', order: 1 },
+    checks: { column: 'tools', order: 2 },
+    projectTop: { column: 'project', order: 1, isExtensible: true },
+    manageBooks: { column: 'project', order: 2 },
+    deleteProject: { column: 'project', order: 3 },
+    projectDetails: { column: 'project', order: 4, isExtensible: true },
+  },
+  items: [
+    {
+      label: 'Inventory: Characters...',
+      group: 'inventory',
+      order: 1,
+      command: 'openCharactersInventory',
+      localizeNotes: '',
+    },
+    {
+      label: 'Inventory: Repeated Words...',
+      group: 'inventory',
+      order: 2,
+      command: 'openRepeatedWordsInventory',
+      localizeNotes: '',
+    },
+    {
+      label: 'Publisher Info',
+      group: 'general',
+      order: 1,
+      command: 'showPublisherInfo',
+      localizeNotes: '',
+    },
+    {
+      label: 'Assignments and Progress',
+      group: 'projectTop',
+      order: 2,
+      command: 'assignments',
+      localizeNotes: '',
+    },
+    {
+      label: 'Open Project Settings',
+      group: 'projectTop',
+      order: 3,
+      command: 'openSettings',
+      localizeNotes: '',
+    },
+  ],
+};
+
+const tabViewMenuData = {
+  columns: {
+    options: { label: 'Options', order: 1 },
+    viewOptions: { label: 'View Options', order: 2 },
+  },
+  groups: {
+    layout: { column: 'options', order: 1 },
+    colors: { column: 'viewOptions', order: 2 },
+  },
+  items: [
+    {
+      label: 'Background Color',
+      group: 'colors',
+      order: 1,
+      command: 'changeBackgroundColor',
+      localizeNotes: '',
+    },
+    {
+      label: 'Text Color',
+      group: 'colors',
+      order: 2,
+      command: 'changeTextColor',
+      localizeNotes: '',
+    },
+    {
+      label: 'Thick Borders',
+      group: 'layout',
+      order: 1,
+      command: 'showThickBorders',
+      localizeNotes: '',
+    },
+  ],
+};
+
+const myScrollGroupIdLocalizedStrings = {
+  [getLocalizeKeyForScrollGroupId('undefined')]: 'Ø',
+  [getLocalizeKeyForScrollGroupId(0)]: 'A',
+  [getLocalizeKeyForScrollGroupId(1)]: 'B',
+  [getLocalizeKeyForScrollGroupId(2)]: 'C',
+  [getLocalizeKeyForScrollGroupId(3)]: 'D',
+  [getLocalizeKeyForScrollGroupId(4)]: 'E',
+};
+
+const meta: Meta<TabToolbarProps> = {
+  title: 'Advanced/TabToolbar',
+  component: TabToolbar,
+  tags: ['autodocs'],
+  args: {
+    projectMenuCommandHandler: (command) => console.log('Project Menu Run command: ', command),
+    viewInfoMenuCommandHandler: (command) => console.log('View Info Run command: ', command),
+    projectMenuData,
+    tabViewMenuData,
+    startAreaChildren: (
+      <>
+        <BookChapterControl scrRef={defaultScrRef} handleSubmit={() => {}} className="tw-h-8" />
+        <ScrollGroupSelector
+          availableScrollGroupIds={[0, 1, 2, 3, 4]}
+          localizedStrings={myScrollGroupIdLocalizedStrings}
+          scrollGroupId={0}
+          onChangeScrollGroupId={setScrollGroupId}
+          size="sm"
+        />
+      </>
+    ),
+    centerAreaChildren: (
+      <>
+        <ScrollGroupSelector
+          availableScrollGroupIds={[0, 1, 2, 3, 4]}
+          localizedStrings={myScrollGroupIdLocalizedStrings}
+          scrollGroupId={0}
+          onChangeScrollGroupId={setScrollGroupId}
+          size="sm"
+        />
+        <ScrollGroupSelector
+          availableScrollGroupIds={[0, 1, 2, 3, 4]}
+          localizedStrings={myScrollGroupIdLocalizedStrings}
+          scrollGroupId={0}
+          onChangeScrollGroupId={setScrollGroupId}
+          size="sm"
+        />
+        <ScrollGroupSelector
+          availableScrollGroupIds={[0, 1, 2, 3, 4]}
+          localizedStrings={myScrollGroupIdLocalizedStrings}
+          scrollGroupId={0}
+          onChangeScrollGroupId={setScrollGroupId}
+          size="sm"
+        />
+      </>
+    ),
+    endAreaChildren: (
+      <>
+        <ScrollGroupSelector
+          availableScrollGroupIds={[0, 1, 2, 3, 4]}
+          localizedStrings={myScrollGroupIdLocalizedStrings}
+          scrollGroupId={0}
+          onChangeScrollGroupId={setScrollGroupId}
+          size="sm"
+        />
+        <ScrollGroupSelector
+          availableScrollGroupIds={[0, 1, 2, 3, 4]}
+          localizedStrings={myScrollGroupIdLocalizedStrings}
+          scrollGroupId={0}
+          onChangeScrollGroupId={setScrollGroupId}
+          size="sm"
+        />
+        <ScrollGroupSelector
+          availableScrollGroupIds={[0, 1, 2, 3, 4]}
+          localizedStrings={myScrollGroupIdLocalizedStrings}
+          scrollGroupId={0}
+          onChangeScrollGroupId={setScrollGroupId}
+          size="sm"
+        />
+        <BookChapterControl scrRef={defaultScrRef} handleSubmit={() => {}} className="tw-h-8" />
+      </>
+    ),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<TabToolbarProps>;
+
+export const Default: Story = {};
+
+export const AnimatedWidth: Story = {
+  render: (args) => (
+    <>
+      <AnimatedContainer minWidth="0%" maxWidth="30vw">
+        <TabToolbar {...args} />
+      </AnimatedContainer>
+      <AnimatedContainer minWidth="200px" maxWidth="60vw" skipAhead="2s">
+        <TabToolbar {...args} />
+      </AnimatedContainer>
+      <AnimatedContainer minWidth="300px" skipAhead="4s">
+        <TabToolbar {...args} />
+      </AnimatedContainer>
+    </>
+  ),
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+    docs: {
+      description: {
+        story:
+          'Uses CSS animations to smoothly animate the TabToolbar container width from 300px to the full viewport width and back, allowing you to observe its responsive behavior.',
+      },
+    },
+  },
+};
