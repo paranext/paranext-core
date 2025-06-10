66e47737d691034124f8a3e7b47fe2287623c64c
tjcouch-sil <tj_couch@sil.org>
2025-06-10 11:04:45 -0500

    Add TSDocs comments to elevated privileges createProcess

--- lib/papi-dts/papi.d.ts
+++ lib/papi-dts/papi.d.ts
@@ -5559,6 +5559,7 @@ declare module 'shared/models/create-process-privilege.model' {
   import { Readable, Writable } from 'stream';
   import { ExtensionBasicData } from 'shared/models/extension-basic-data.model';
   /**
+   *
    * Run {@link spawn} to create a child process. The platform will automatically kill all child
    * processes created this way in packaged builds. Child processes are not killed when running in
    * development.
@@ -5605,6 +5606,7 @@ declare module 'shared/models/create-process-privilege.model' {
     options: SpawnOptionsWithStdioTuple<StdioPipe, StdioPipe, StdioPipe>,
   ) => ChildProcessByStdio<Writable, Readable, Readable>;
   /**
+   *
    * Run {@link fork} to create a child process. The platform will automatically kill all child
    * processes created this way in packaged builds. Child processes are not killed when running in
    * development.
@@ -5648,8 +5650,79 @@ declare module 'shared/models/create-process-privilege.model' {
     release: string;
   };
   export type CreateProcess = {
+    /**
+     *
+     * Run {@link spawn} to create a child process. The platform will automatically kill all child
+     * processes created this way in packaged builds. Child processes are not killed when running in
+     * development.
+     *
+     * This method is essentially a layer over the [`spawn`
+     * method](https://nodejs.org/api/child_process.html#child_processspawncommand-args-options) from
+     * the Node `child_process` module. Please see its documentation for more information.
+     *
+     * @example The following example assumes there are subdirectories in the extension's files for
+     * win32, linux, and macOS that include appropriate executables.
+     *
+     * ```@typescript
+     * export async function activate(context: ExecutionActivationContext) {
+     *   const { executionToken } = context;
+     *   const { createProcess } = context.elevatedPrivileges;
+     *   if (!createProcess)
+     *     throw new Error('Forgot to add "createProcess" to "elevatedPrivileges" in manifest.json');
+     *   switch (createProcess.osData.platform) {
+     *     case 'win32':
+     *       createProcess.spawn(executionToken, 'win32/RunMe.exe', [], { stdio: [null, null, null] });
+     *       break;
+     *     case 'linux':
+     *       createProcess.spawn(executionToken, 'linux/runMe', [], { stdio: [null, null, null] });
+     *       break;
+     *     case 'darwin':
+     *       createProcess.spawn(executionToken, 'macOS/runMe', [], { stdio: [null, null, null] });
+     *       break;
+     *     default:
+     *       throw new Error(`Unsupported platform: ${createProcess.osData.platform}`);
+     *   }
+     * ```
+     *
+     * @param executionToken ExecutionToken object provided when an extension was activated
+     * @param command Command to run to start the process
+     * @param args Arguments to pass to the command
+     * @param options Options to pass to `spawn`. The `cwd` option will be overridden to the extension's
+     *   root directory.
+     * @returns A {@link ChildProcessByStdio} object representing the command
+     */
     spawn: PlatformSpawn;
+    /**
+     *
+     * Run {@link fork} to create a child process. The platform will automatically kill all child
+     * processes created this way in packaged builds. Child processes are not killed when running in
+     * development.
+     *
+     * This method is essentially a layer over the [`fork`
+     * method](https://nodejs.org/api/child_process.html#child_processforkmodulepath-args-options) from
+     * the Node `child_process` module. Please see its documentation for more information.
+     *
+     * @example The following example assumes there is a file named `childProcess.js` in the extension's
+     * `assets` subdirectory
+     *
+     * ```@typescript
+     * export async function activate(context: ExecutionActivationContext) {
+     *   const { executionToken } = context;
+     *   const { createProcess } = context.elevatedPrivileges;
+     *   if (!createProcess)
+     *     throw new Error('Forgot to add "createProcess" to "elevatedPrivileges" in manifest.json');
+     *   createProcess.fork(executionToken, 'assets/childProcess.js');
+     * ```
+     *
+     * @param executionToken ExecutionToken object provided when an extension was activated
+     * @param modulePath The module to run in the child
+     * @param args Arguments to pass when creating the node process
+     * @param options Options to pass to `fork`. The `cwd` option will be overridden to the extension's
+     *   root directory.
+     * @returns A {@link ChildProcess} object representing the process running the module
+     */
     fork: PlatformFork;
+    /** Data about the operating system on which this process is running */
     osData: OperatingSystemData;
   };
 }
