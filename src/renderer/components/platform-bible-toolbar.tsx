1dff6823a78a7ee0ed78f8402b068374970efbea
Rolf Heij <rolf_heij@sil.org>
2025-07-16 12:01:51 +0200

    Add Usersnap

--- src/renderer/components/platform-bible-toolbar.tsx
+++ src/renderer/components/platform-bible-toolbar.tsx
@@ -1,13 +1,19 @@
 import logo from '@assets/icon.png';
+import { provideMenuData } from '@renderer/components/platform-bible-menu.data';
 import {
   useData,
   useDataProvider,
   useLocalizedStrings,
   useScrollGroupScrRef,
 } from '@renderer/hooks/papi-hooks';
+import { app } from '@renderer/services/papi-frontend.service';
 import { availableScrollGroupIds } from '@renderer/services/scroll-group.service-host';
+import { localThemeService } from '@renderer/services/theme.service-host';
+import { handleMenuCommand } from '@shared/data/platform-bible-menu.commands';
 import { sendCommand } from '@shared/services/command.service';
+import { logger } from '@shared/services/logger.service';
 import { ScrollGroupScrRef } from '@shared/services/scroll-group.service-model';
+import { themeServiceDataProviderName } from '@shared/services/theme.service-model';
 import { HomeIcon, Moon, Sun, User } from 'lucide-react';
 import {
   Badge,
@@ -32,12 +38,6 @@ import {
   ThemeDefinitionExpanded,
 } from 'platform-bible-utils';
 import { useCallback, useEffect, useMemo, useState } from 'react';
-import { handleMenuCommand } from '@shared/data/platform-bible-menu.commands';
-import { app } from '@renderer/services/papi-frontend.service';
-import { themeServiceDataProviderName } from '@shared/services/theme.service-model';
-import { logger } from '@shared/services/logger.service';
-import { provideMenuData } from '@renderer/components/platform-bible-menu.data';
-import { localThemeService } from '@renderer/services/theme.service-host';
 
 const TOOLTIP_DELAY = 300;
 
