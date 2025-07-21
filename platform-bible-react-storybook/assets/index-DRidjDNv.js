d1a230561d94b30e0df8125418b163f694969d02
jolierabideau <135999578+jolierabideau@users.noreply.github.com>
2025-07-21 12:31:23 +0000

    Deploying to github-pages from @ paranext/paranext-core@fa26dac4f263e1d90cdfbb4e0f2e621c5797a3b8 🚀

--- platform-bible-react-storybook/assets/index-DRidjDNv.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./iframe-BArTF4QZ.js";import{r as t}from"./index-o101jNwU.js";var o=t();const c=e(o);export{c as R,o as r};
---
+++ platform-bible-react-storybook/assets/index-FfE_ZP1D.js
@@ -0,0 +1,1 @@
+import{g as e}from"./iframe-BDvoTL_W.js";import{r as t}from"./index-Doh_X05t.js";var o=t();const c=e(o);export{c as R,o as r};
