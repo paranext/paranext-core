b2a765607315f769786fae03e62baa5cfa334719
Sebastian-ubs <slingk@biblesocieties.org>
2025-07-22 15:00:30 +0200

    split home component out of webview

--- extensions/src/platform-get-resources/src/home.stories.tsx
+++ extensions/src/platform-get-resources/src/home.stories.tsx
@@ -1,14 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
-<<<<<<< HEAD
 import type { SharedProjectsInfo } from 'platform-scripture';
 import { ReactElement, useState } from 'react';
 import { Home, HomeProps, LocalProjectInfo } from './home.component';
 
-=======
-import { SharedProjectsInfo } from 'platform-scripture';
-import { ReactElement, useState } from 'react';
-import { HomeDialog, HomeDialogProps, LocalProjectInfo } from './home.component';
->>>>>>> 01214f876f (split home component out of webview)
 const staticLocalProjectsAndResources: LocalProjectInfo[] = [
   {
     projectId: '1',
@@ -16,10 +10,6 @@ const staticLocalProjectsAndResources: LocalProjectInfo[] = [
     fullName: 'Resource 1',
     name: 'Res1',
     language: 'myLanguage',
-<<<<<<< HEAD
-=======
-    //type: 'resource',
->>>>>>> 01214f876f (split home component out of webview)
   },
   {
     projectId: '2',
@@ -27,10 +17,6 @@ const staticLocalProjectsAndResources: LocalProjectInfo[] = [
     fullName: 'Resource 2',
     name: 'Res2',
     language: 'English',
-<<<<<<< HEAD
-=======
-    //type: 'resource',
->>>>>>> 01214f876f (split home component out of webview)
   },
   {
     projectId: '13',
@@ -38,10 +24,6 @@ const staticLocalProjectsAndResources: LocalProjectInfo[] = [
     fullName: 'Project 4 - editable',
     name: 'Pr4',
     language: '2ndLanguage',
-<<<<<<< HEAD
-=======
-    //type: 'project',
->>>>>>> 01214f876f (split home component out of webview)
   },
   {
     projectId: '14',
@@ -50,10 +32,6 @@ const staticLocalProjectsAndResources: LocalProjectInfo[] = [
       'Project 3 - read-only This_is_a_project_with_a_very_long_name_01234567890_!/"§$%&/()=?_öäüß',
     name: 'Pr3',
     language: '2ndLanguage',
-<<<<<<< HEAD
-=======
-    //type: 'project',
->>>>>>> 01214f876f (split home component out of webview)
   },
   {
     projectId: '25',
@@ -61,10 +39,6 @@ const staticLocalProjectsAndResources: LocalProjectInfo[] = [
     fullName: 'Project 5 - editable',
     name: 'Pr5',
     language: 'German',
-<<<<<<< HEAD
-=======
-    //type: 'project',
->>>>>>> 01214f876f (split home component out of webview)
   },
   {
     projectId: '26',
@@ -72,10 +46,6 @@ const staticLocalProjectsAndResources: LocalProjectInfo[] = [
     fullName: 'SDBH/SDBG',
     name: 'SdDict',
     language: 'Hebrew/Greek',
-<<<<<<< HEAD
-=======
-    //type: 'dictionary',
->>>>>>> 01214f876f (split home component out of webview)
   },
 ];
 
@@ -98,7 +68,6 @@ const staticProjectsAndResources: SharedProjectsInfo = {
   },
 };
 
-<<<<<<< HEAD
 const meta: Meta<typeof Home> = {
   title: 'Bundled Extensions/platform-get-resources/Home',
   component: Home,
@@ -111,9 +80,6 @@ export default meta;
 type Story = StoryObj<typeof Home>;
 
 function DefaultHomeDecorator(Story: (update?: { args: Partial<HomeProps> }) => ReactElement) {
-=======
-function HomeDecorator(Story: (update?: { args: Partial<HomeDialogProps> }) => ReactElement) {
->>>>>>> 01214f876f (split home component out of webview)
   const [localProjectsAndResources, setLocalProjectsAndResources] = useState<LocalProjectInfo[]>(
     [],
   );
@@ -150,7 +116,6 @@ function HomeDecorator(Story: (update?: { args: Partial<HomeDialogProps> }) => R
   );
 }
 
-<<<<<<< HEAD
 export const Default: Story = {
   decorators: [DefaultHomeDecorator],
 };
@@ -181,19 +146,4 @@ function OnlyWebProjectDecorator(Story: (update?: { args: Partial<HomeProps> }) 
 
 export const OnlyWebProject: Story = {
   decorators: [OnlyWebProjectDecorator],
-=======
-const meta: Meta<typeof HomeDialog> = {
-  title: 'Bundled Extensions/platform-get-resources/Home',
-  component: HomeDialog,
-  tags: ['autodocs'],
-  argTypes: {},
-  decorators: [HomeDecorator],
-};
-export default meta;
-
-type Story = StoryObj<typeof HomeDialog>;
-
-export const Default: Story = {
-  args: {},
->>>>>>> 01214f876f (split home component out of webview)
 };
