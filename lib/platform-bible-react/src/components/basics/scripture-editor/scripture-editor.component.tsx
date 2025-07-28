90159dcd58f0216ffb2ebab27289d51771942f62
Alex Mercado <alex_mercado@sil.org>
2025-07-28 15:27:32 -0500

    Add Story controls to editor component

--- lib/platform-bible-react/src/components/basics/scripture-editor/scripture-editor.component.tsx
+++ lib/platform-bible-react/src/components/basics/scripture-editor/scripture-editor.component.tsx
@@ -1,20 +1,23 @@
-import { Editorial } from '@eten-tech-foundation/platform-editor';
+import { Editorial, EditorRef } from '@eten-tech-foundation/platform-editor';
 import { Usj, USJ_TYPE, USJ_VERSION } from '@eten-tech-foundation/scripture-utilities';
 import '@/components/basics/scripture-editor/scripture-editor.component.css';
 
-const defaultUsj: Usj = {
+const DEFAULT_USJ: Usj = {
   type: USJ_TYPE,
   version: USJ_VERSION,
   content: ['hello world'],
 };
 
 /** Component to render a scripture editor (or viewer) */
-export function ScriptureEditor() {
+export function ScriptureEditor({editorRef, defaultUsj = DEFAULT_USJ}: {editorRef: React.MutableRefObject<EditorRef | null>, defaultUsj?: Usj }) {
   return (
     <div>
       <h1>Scripture Editor Component</h1>
       <p>This component is under construction.</p>
-      <Editorial defaultUsj={defaultUsj} />
+      <Editorial ref={editorRef} defaultUsj={defaultUsj} />
     </div>
   );
 }
+
+// TODO: Mock data
+// Expose all parameters for the component
--- lib/platform-bible-react/src/stories/basics/scripture-editor.stories.tsx
+++ lib/platform-bible-react/src/stories/basics/scripture-editor.stories.tsx
@@ -1,5 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { ScriptureEditor } from '../../components/basics/scripture-editor/scripture-editor.component';
+import { USJ_TYPE, USJ_VERSION } from '@eten-tech-foundation/scripture-utilities';
+
 
 const meta: Meta<typeof ScriptureEditor> = {
   title: 'Basics/ScriptureEditor',
@@ -7,6 +9,20 @@ const meta: Meta<typeof ScriptureEditor> = {
   tags: ['autodocs'],
   argTypes: {
     // Add argTypes here as needed for props
+    editorRef: {
+      control: 'object',
+      description: 'Reference to the editor instance',
+      defaultValue: null,
+    },
+    defaultUsj: {
+      control: 'object',
+      description: 'Default USJ object to initialize the editor with',
+      defaultValue: {
+        type: USJ_TYPE,
+        version: USJ_VERSION,
+        content: [''],
+      },
+    },
   },
 };
 export default meta;
@@ -15,6 +31,12 @@ type Story = StoryObj<typeof ScriptureEditor>;
 
 export const Default: Story = {
   args: {
+    defaultUsj: {
+      type: USJ_TYPE,
+      version: USJ_VERSION,
+      content: ['Won\'t you be my neighbor?'],
+    },
+
     // Provide default props if needed
   },
 };
