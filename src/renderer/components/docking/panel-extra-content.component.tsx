26864c70e5f306522223419d15e268a9c430806d
Alex Mercado <alex_mercado@sil.org>
2025-07-30 15:07:40 -0500

    Remove dock's expand button Add Lucide moreIcon

--- src/renderer/components/docking/panel-extra-content.component.tsx
+++ src/renderer/components/docking/panel-extra-content.component.tsx
@@ -1,28 +1,19 @@
 import { sendCommand } from '@shared/services/command.service';
 import { logger } from '@shared/services/logger.service';
-import { Maximize2, Minimize2, Plus } from 'lucide-react';
 import { Button } from 'platform-bible-react';
-import { DockContext, PanelData } from 'rc-dock';
+import { PanelData } from 'rc-dock';
 import React from 'react';
 import './panel-extra-content.component.scss';
 
 interface PanelExtraContentProps {
   panelData: PanelData;
-  context: DockContext;
 }
 
 /**
  * Component that renders the extra content in the panel header, including the maximize/minimize
  * button and the new tab button
  */
-export function PanelExtraContent({ panelData, context }: PanelExtraContentProps) {
-  const handleMaximize = (e: React.MouseEvent) => {
-    // RC Dock needs null here
-    // eslint-disable-next-line no-null/no-null
-    context.dockMove(panelData, null, 'maximize');
-    e.stopPropagation();
-  };
-
+export function PanelExtraContent({ panelData }: PanelExtraContentProps) {
   const handleOpenNewTab = async (tabGroupId?: string) => {
     try {
       await sendCommand('platformGetResources.openNewTab', tabGroupId);
@@ -32,21 +23,13 @@ export function PanelExtraContent({ panelData, context }: PanelExtraContentProps
   };
 
   return (
-    <>
-      <Button
-        variant="ghost"
-        className="new-tab-button"
-        onClick={() => handleOpenNewTab(panelData.id)}
-      >
-        <Plus />
-      </Button>
-
-      {panelData.parent && panelData.parent.mode !== 'window' && (
-        <Button variant="ghost" className="maximize-button" onClick={handleMaximize}>
-          {panelData.parent.mode === 'maximize' ? <Minimize2 /> : <Maximize2 />}
-        </Button>
-      )}
-    </>
+    <Button
+      variant="ghost"
+      className="new-tab-button"
+      onClick={() => handleOpenNewTab(panelData.id)}
+    >
+      <Plus />
+    </Button>
   );
 }
 
