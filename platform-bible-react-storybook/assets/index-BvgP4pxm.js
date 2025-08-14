d624d55cd43e1e78ac7f6b9277d6c0122350f06f
rolfheij-sil <108285668+rolfheij-sil@users.noreply.github.com>
2025-08-14 18:16:02 +0000

    Deploying to github-pages from @ paranext/paranext-core@4486af5268fc33a8d3a9dd461af2d368e1889602 🚀

---
+++ platform-bible-react-storybook/assets/index-BvgP4pxm.js
@@ -0,0 +1,1 @@
+import{g as e}from"./iframe-PQ3SVnim.js";import{r as t}from"./index-DvlJK5g4.js";var o=t();const c=e(o);export{c as R,o as r};
--- platform-bible-react-storybook/assets/index-C50j32mq.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./iframe-CqZw80g5.js";import{r as t}from"./index-DFgd3jyS.js";var o=t();const c=e(o);export{c as R,o as r};
