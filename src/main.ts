709608cb6c421600b87506b974923d5712669f0a
tjcouch-sil <tj_couch@sil.org>
2025-08-25 12:19:25 -0500

    Squashed 'extensions/src/hello-someone/' changes from 0aa7a6864a..d8fd69d133
    d8fd69d133 PT-3107: Improve release workflow (#116)
    ae66f871aa PT-2390: README improvements (#114)
    a79c364d5b Added CODEOWNERS matching our code stewardship list (#113)
    d8e215a198 PT-2390: Clarify template use in README.md (#112)
    003cc24fcd Use standard 'WebView' in comments (#111)
    8745d6ac9b Use standard 'WebView' in comments
    c5267f459a Swapped built-in logs to debug since we already have logs that cover this in core; they are just sample actions (#110)
    3bf3cbab4a In package.json sripts, update lint:scripts, lint:styles (#109)
    97b43e3531 Update Tailwind prose colors (#108)
    5c3c7130ca Update Tailwind prose colors
    c4f69f2696 Fix lint.yml GitHub workflow and run npm install to update package-lock.json (#107)
    8d302c8151 Add github action for CI with lint and format check (#106)
    08624a5980 Add codeql.yml to run CodeQL manually (#105)
    354a546d55 Add "remote-allow-origins" to launch.json to allow external debugging (#104)
    adec29c2a4 Add "remote-allow-origins" to launch.json to allow external debugging
    
    git-subtree-dir: extensions/src/hello-someone
    git-subtree-split: d8fd69d133a567dc09a125311318fc28861b1360

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { logger } from '@papi/backend';
 
 export async function activate() {
-  logger.info('Extension template is activating!');
+  logger.debug('Extension template is activating!');
 }
 
 export async function deactivate() {
-  logger.info('Extension template is deactivating!');
+  logger.debug('Extension template is deactivating!');
   return true;
 }
