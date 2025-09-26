40803e9511fb8279654809a0be4dca173a14d380
Rolf Heij <rolf_heij@sil.org>
2025-09-26 19:55:14 +0200

    PT-3436: Improve Checks sidepanel: Cards and Setup requirements

--- extensions/src/platform-scripture/src/checks-side-panel.web-view.tsx
+++ extensions/src/platform-scripture/src/checks-side-panel.web-view.tsx
@@ -47,6 +47,56 @@ import {
 import { CHECK_RESULTS_INVALIDATED_EVENT } from './checks/check.model';
 import { CheckCard, CheckStates } from './checks/checks-side-panel/check-card.component';
 
+/**
+ * Enum representing the different scopes that can be selected for checks.
+ *
+ * - `Chapter`: Scope of a single chapter.
+ * - `Book`: Scope of an entire book.
+ * - `All`: Scope of the entire project.
+ */
+export enum CheckScopes {
+  Chapter = 'Chapter',
+  Book = 'Book',
+  All = 'All',
+}
+
+/** Object containing strings for the project full and short names */
+type ProjectOption = {
+  fullName: string;
+  shortName: string;
+};
+
+/**
+ * Gets the short and full names of a project from its ID.
+ *
+ * @param projectId The ID of the project to get the names of.
+ * @returns An object with the short and full names of the project, or undefined if the project is
+ *   not editable.
+ */
+async function getProjectNames(projectId: string): Promise<ProjectOption | undefined> {
+  const pdp = await projectDataProviders.get('platform.base', projectId);
+
+  if (!(await pdp.getSetting('platform.isEditable'))) return undefined;
+
+  const projectShortName = await pdp.getSetting('platform.name');
+  const projectFullName = await pdp.getSetting('platform.fullName');
+
+  return { shortName: projectShortName, fullName: projectFullName };
+}
+
+export type CheckInfo = {
+  /** Unique identifier of the check */
+  checkId: string;
+  /** Localized display name of the check */
+  checkName: string;
+  /** Whether the check has been properly set up */
+  isSetup: boolean;
+  // TODO: Might need to change this to a command that we call when it is clicked on, but not sure
+  // until we implement this more as part of https://paratextstudio.atlassian.net/browse/PT-2279
+  /** Optional link to the relevant settings/inventories to set up */
+  setUpLink?: string;
+};
+
 const defaultCheckRunnerCheckDetails: CheckRunnerCheckDetails = {
   checkDescription: '',
   checkId: '',
@@ -621,6 +671,10 @@ global.webViewComponent = function ChecksSidePanelWebView({
     },
     [updateWebViewDefinition],
   );
+
+  const isValidCheckScope = (value: string): value is CheckScopes => {
+    return Object.values(CheckScopes).some((scopeValue) => scopeValue === value);
+  };
 
   const handleSelectScope = useCallback(
     (newScope: string) => {
