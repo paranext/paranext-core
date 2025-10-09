da0daead552d04a0b60bef808c7f046b4db8c578
merchako <alex_mercado@sil.org>
2025-10-09 03:18:24 +0000

    Deploying to github-pages from @ paranext/paranext-core@16a1342a683e6658bab41a40d7ef2bd1ac144000 🚀

--- platform-bible-react-storybook/assets/index-CfeOJYUq.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./iframe-3yEZ8Llg.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
---
+++ platform-bible-react-storybook/assets/index-DEEpPjCA.js
@@ -0,0 +1,1 @@
+import{g as e}from"./iframe-Bq8lxKKc.js";import{r as t}from"./index-69R2V6G7.js";var o=t();const c=e(o);export{o as r,c as v};
