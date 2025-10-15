41b67792485579e174de91c6d2452cd5c15d2df7
Rolf Heij <rolf_heij@sil.org>
2025-10-15 16:55:11 +0200

    PT-3546: Update legacy comment data provider

--- c-sharp/Projects/CommentSelector.cs
+++ c-sharp/Projects/CommentSelector.cs
@@ -1,3 +1,5 @@
+using SIL.Scripture;
+
 namespace Paranext.DataProvider.Projects;
 
 public class CommentSelector
@@ -9,4 +11,31 @@ public class CommentSelector
     public int VerseNum { get; set; } = 0;
 
     public string? CommentId { get; set; } = null;
+}
+
+public class CommentThreadSelector
+{
+    public string? Status { get; set; }
+    public string? Type { get; set; }
+    public string? ThreadId { get; set; }
+    public DateFilter? DateFilter { get; set; }
+    public string? User { get; set; }
+    public string? AssignedTo { get; set; }
+    public List<ScriptureRange>? ScriptureRanges { get; set; }
+}
+
+public class DateFilter
+{
+    public string? Exact { get; set; }
+    public string? Before { get; set; }
+    public string? After { get; set; }
+    public string? Start { get; set; }
+    public string? End { get; set; }
+}
+
+public class ScriptureRange
+{
+    public required VerseRef Start { get; set; }
+    public required VerseRef End { get; set; }
+    public string? Granularity { get; set; }
 }
