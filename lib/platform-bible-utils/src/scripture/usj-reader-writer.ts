712a787b683767bdee94c0a0db4ffb683c16a232
tjcouch-sil <tj_couch@sil.org>
2025-11-12 11:49:27 -0600

    Fixed search result selection wrong index, typo

--- lib/platform-bible-utils/src/scripture/usj-reader-writer.ts
+++ lib/platform-bible-utils/src/scripture/usj-reader-writer.ts
@@ -2145,7 +2145,7 @@ export class UsjReaderWriter implements IUsjReaderWriter {
       !this.markersMap.shouldOptionalClosingMarkersBePresent &&
       markerWithAnyAttributes.closed !== 'true'
     ) {
-      // If the closing marker is optional, optional closing markers should be left out, and is it
+      // If the closing marker is optional, optional closing markers should be left out, and it is
       // not specified as closed, it should not have a closing marker
       hasExplicitClosingMarker = false;
     }
