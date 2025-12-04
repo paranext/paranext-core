af310fbe6ddd1904c968b00865c630d4c6ed0857
rolfheij-sil <108285668+rolfheij-sil@users.noreply.github.com>
2025-12-04 15:31:20 +0000

    Deploying to github-pages from @ paranext/paranext-core@ef3b7100324b4ede65c080e8d5a50f1f3e455435 🚀

---
+++ platform-bible-react-storybook/assets/index-j2SrRYGk.js
@@ -0,0 +1,1 @@
+import{r as e}from"./iframe-CbiqY5rW.js";import"./jsx-runtime-DYH7p4H-.js";var n=e.createContext(void 0);function c(t){const o=e.useContext(n);return t||o||"ltr"}export{c as u};
---
+++ platform-bible-react-storybook/assets/tab-toolbar.stories-BvUSI5D9.js
@@ -0,0 +1,43 @@
+import{j as o}from"./jsx-runtime-DYH7p4H-.js";import{T as p,J as i}from"./index-DY9IrCTG.js";import{S as r}from"./scroll-group-selector.component-B-v32meP.js";import{B as u}from"./book-chapter-control.component-HjDa_jvB.js";import{r as N}from"./iframe-CbiqY5rW.js";import{T as d}from"./tab-toolbar.component-q2xeU0e6.js";import"./index-D2w9gCAG.js";import"./select-BdhiEML5.js";import"./index-CPleRUKj.js";import"./index-BK-JrMtU.js";import"./index-BaQP4hhM.js";import"./index-DTAXz6r9.js";import"./index-BzuZXtl-.js";import"./index-Dj6FJPdz.js";import"./index-DNmw49TY.js";import"./index-6wg4jWbT.js";import"./index-j2SrRYGk.js";import"./index-DY0kwC2E.js";import"./index-DtsCDfR_.js";import"./index-BXXCYkUS.js";import"./floating-ui.react-dom-DnheErdj.js";import"./index-ZQgpkrue.js";import"./index-CqbdS-2m.js";import"./index-MibOODGh.js";import"./index-DSyjLlnY.js";import"./index-DCRGPiUG.js";import"./index-DizF63-q.js";import"./index-Caplk3YO.js";import"./index-CQbuKnQA.js";import"./index-B5JnW5zy.js";import"./shadcn-ui.util-DMJ19wEV.js";import"./index-BPbCuWFR.js";import"./chevron-down-DK4PpAlj.js";import"./createLucideIcon-B-ADoEmV.js";import"./check-Dm2RQABL.js";import"./chevron-up-Cu_6ki70.js";import"./book-item.utils-Dt1zdtl4.js";import"./command-B9dKhGJv.js";import"./index-Bi55vPeI.js";import"./index-DgkjGXyq.js";import"./index-5PMepb-4.js";import"./dialog-BaIhpAHt.js";import"./index-Czfav6j7.js";import"./x-CiiqOtnc.js";import"./search-DvHDT9tA.js";import"./index.es-D4jfZzAn.js";import"./button-C2rkuAJh.js";import"./popover-DZI5ZAFw.js";import"./chevron-left-DLgQW7qQ.js";import"./chevron-right-DqgQ5dL0.js";import"./arrow-right-CVA88f5w.js";import"./tab-dropdown-menu.component-DvgBGQ_X.js";import"./dropdown-menu-DHyI65Ok.js";import"./index-SYbHJGR0.js";import"./index-BhlYrLLh.js";import"./circle-C4w27o9h.js";import"./tooltip-BGQp5l3w.js";import"./menu.util-Dxh7JGT4.js";import"./menu-icon.component-DFjeQZT0.js";import"./tab-toolbar-container.component-BPK5dFSB.js";function c({children:e,minWidth:T="0%",maxWidth:y="calc(100vw - 4rem)",skipAhead:A="0%"}){const m=N.useMemo(()=>`widthAnimation-${Math.random().toString(36).substr(2,9)}`,[]);return o.jsxs(o.Fragment,{children:[o.jsxs("div",{style:{animation:`${m} 6s infinite alternate ease-in-out`,animationDelay:`-${A}`,border:"2px solid #ccc",borderRadius:"16px",marginBlock:"1rem",background:"#fdfdfd",boxShadow:"0 4px 8px rgba(0,0,0,0.08)",overflow:"hidden",padding:".5rem",maxWidth:"100%"},children:[e,o.jsx("style",{children:`
+            @keyframes ${m} {
+              0% {
+                width: ${T};
+              }
+              100% {
+                width: ${y};
+              }
+            }
+          `})]}),o.jsx("p",{style:{paddingInlineStart:"1rem",fontSize:12,color:"#888"},children:"Container width is animated"})]})}c.__docgenInfo={description:"",methods:[],displayName:"AnimatedContainer",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""},minWidth:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'0%'",computed:!1}},maxWidth:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'calc(100vw - 4rem)'",computed:!1}},skipAhead:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'0%'",computed:!1}}}};const t=e=>{console.log("New Scroll Group Id: ",e)},h=[{book:"GEN",chapterNum:1,verseNum:1},{book:"PSA",chapterNum:23,verseNum:1},{book:"MAT",chapterNum:5,verseNum:3}],g=e=>{console.log("Adding recent search: ",e)},G={columns:{tools:{label:"Tools",order:1},info:{label:"Info",order:2},project:{label:"Project",order:3}},groups:{general:{column:"info",order:1},inventory:{column:"tools",order:1},checks:{column:"tools",order:2},projectTop:{column:"project",order:1,isExtensible:!0},manageBooks:{column:"project",order:2},deleteProject:{column:"project",order:3},projectDetails:{column:"project",order:4,isExtensible:!0}},items:[{label:"Inventory: Characters...",group:"inventory",order:1,command:"openCharactersInventory",localizeNotes:""},{label:"Inventory: Repeated Words...",group:"inventory",order:2,command:"openRepeatedWordsInventory",localizeNotes:""},{label:"Publisher Info",group:"general",order:1,command:"showPublisherInfo",localizeNotes:""},{label:"Assignments and Progress",group:"projectTop",order:2,command:"assignments",localizeNotes:""},{label:"Open Project Settings",group:"projectTop",order:3,command:"openSettings",localizeNotes:""}]},k={columns:{options:{label:"Options",order:1},viewOptions:{label:"View Options",order:2}},groups:{layout:{column:"options",order:1},colors:{column:"viewOptions",order:2}},items:[{label:"Background Color",group:"colors",order:1,command:"changeBackgroundColor",localizeNotes:""},{label:"Text Color",group:"colors",order:2,command:"changeTextColor",localizeNotes:""},{label:"Thick Borders",group:"layout",order:1,command:"showThickBorders",localizeNotes:""}]},a={[i("undefined")]:"Ø",[i(0)]:"A",[i(1)]:"B",[i(2)]:"C",[i(3)]:"D",[i(4)]:"E"},Do={title:"Advanced/TabToolbar",component:d,tags:["autodocs"],args:{onSelectProjectMenuItem:e=>console.log("Project Menu Run command: ",e),onSelectViewInfoMenuItem:e=>console.log("View Info Run command: ",e),projectMenuData:G,tabViewMenuData:k,startAreaChildren:o.jsxs(o.Fragment,{children:[o.jsx(u,{scrRef:p,handleSubmit:()=>{},recentSearches:h,onAddRecentSearch:g}),o.jsx(r,{availableScrollGroupIds:[0,1,2,3,4],localizedStrings:a,scrollGroupId:0,onChangeScrollGroupId:t,size:"sm"})]}),centerAreaChildren:o.jsxs(o.Fragment,{children:[o.jsx(r,{availableScrollGroupIds:[0,1,2,3,4],localizedStrings:a,scrollGroupId:0,onChangeScrollGroupId:t,size:"sm"}),o.jsx(r,{availableScrollGroupIds:[0,1,2,3,4],localizedStrings:a,scrollGroupId:0,onChangeScrollGroupId:t,size:"sm"}),o.jsx(r,{availableScrollGroupIds:[0,1,2,3,4],localizedStrings:a,scrollGroupId:0,onChangeScrollGroupId:t,size:"sm"})]}),endAreaChildren:o.jsxs(o.Fragment,{children:[o.jsx(r,{availableScrollGroupIds:[0,1,2,3,4],localizedStrings:a,scrollGroupId:0,onChangeScrollGroupId:t,size:"sm"}),o.jsx(r,{availableScrollGroupIds:[0,1,2,3,4],localizedStrings:a,scrollGroupId:0,onChangeScrollGroupId:t,size:"sm"}),o.jsx(r,{availableScrollGroupIds:[0,1,2,3,4],localizedStrings:a,scrollGroupId:0,onChangeScrollGroupId:t,size:"sm"}),o.jsx(u,{scrRef:p,handleSubmit:()=>{},recentSearches:h,onAddRecentSearch:g})]})}},n={},s={render:e=>o.jsxs(o.Fragment,{children:[o.jsx(c,{minWidth:"0%",maxWidth:"30vw",children:o.jsx(d,{...e})}),o.jsx(c,{minWidth:"200px",maxWidth:"60vw",skipAhead:"2s",children:o.jsx(d,{...e})}),o.jsx(c,{minWidth:"300px",skipAhead:"4s",children:o.jsx(d,{...e})})]}),parameters:{controls:{hideNoControlsWarning:!0},docs:{description:{story:"Uses CSS animations to smoothly animate the TabToolbar container width from 300px to the full viewport width and back, allowing you to observe its responsive behavior."}}}},l={args:{className:"tw-bg-red-100 tw-border-red-400"},parameters:{docs:{description:{story:"TabToolbar with custom background and border color using the className prop."}}}};var b,S,f;n.parameters={...n.parameters,docs:{...(b=n.parameters)==null?void 0:b.docs,source:{originalSource:"{}",...(f=(S=n.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var x,j,v;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: args => <>
+      <AnimatedContainer minWidth="0%" maxWidth="30vw">
+        <TabToolbar {...args} />
+      </AnimatedContainer>
+      <AnimatedContainer minWidth="200px" maxWidth="60vw" skipAhead="2s">
+        <TabToolbar {...args} />
+      </AnimatedContainer>
+      <AnimatedContainer minWidth="300px" skipAhead="4s">
+        <TabToolbar {...args} />
+      </AnimatedContainer>
+    </>,
+  parameters: {
+    controls: {
+      hideNoControlsWarning: true
+    },
+    docs: {
+      description: {
+        story: 'Uses CSS animations to smoothly animate the TabToolbar container width from 300px to the full viewport width and back, allowing you to observe its responsive behavior.'
+      }
+    }
+  }
+}`,...(v=(j=s.parameters)==null?void 0:j.docs)==null?void 0:v.source}}};var C,I,w;l.parameters={...l.parameters,docs:{...(C=l.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    className: 'tw-bg-red-100 tw-border-red-400'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'TabToolbar with custom background and border color using the className prop.'
+      }
+    }
+  }
+}`,...(w=(I=l.parameters)==null?void 0:I.docs)==null?void 0:w.source}}};const Vo=["Default","AnimatedWidth","WithCustomClassName"];export{s as AnimatedWidth,n as Default,l as WithCustomClassName,Vo as __namedExportsOrder,Do as default};
