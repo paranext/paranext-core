7f46ab3c7be1d8309c396a20ab1f1ef7d3aed976
Sebastian-ubs <141921979+Sebastian-ubs@users.noreply.github.com>
2025-12-18 16:40:24 +0100

    home+find: better loading, responsive, status (#1825)
    * home+find: better loading and responsiveness
    
    * menu on small width, s/r-pickle, muted when not locally available
    
    * fix
    
    * fix linting errors
    
    * fix linting errors
    
    * sort by short name (ux feedback from Ian)
    
    * move dot left, fix storybook small width
    
    * make headers buttons; remove table x padding
    
    * review comments (1) - l10nLoading in home
    
    https://reviewable.io/reviews/paranext/paranext-core/1825#-OZolsOB2h9SqXfWUjmG
    
    * review comments: (2) - use cn
    
    * small fix for color and overflow
    
    * review comments: simplify condition
    
    * review comments: use l10n loading state
    
    * fix linting
    
    * allow alert in stories

---
+++ extensions/src/platform-get-resources/src/home-item-menu.tsx
@@ -0,0 +1,36 @@
+import { Ellipsis } from 'lucide-react';
+import {
+  Button,
+  cn,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from 'platform-bible-react';
+import { ReactNode } from 'react';
+
+export interface HomeItemMenuProps {
+  /* children of type <DropdownMenuItem> */
+  children: ReactNode;
+  /* Optional class name to apply to the ellipsis button */
+  ellipsisButtonClassName?: string;
+}
+
+/**
+ * A dropdown menu for a home item, triggered by an ellipsis button.
+ *
+ * @param children Children of type <DropdownMenuItem>
+ * @param ellipsisButtonClassName Optional class name to apply to the ellipsis button
+ * @returns
+ */
+export function HomeItemDropdownMenu({ children, ellipsisButtonClassName }: HomeItemMenuProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className={cn('tw-w-4', ellipsisButtonClassName)}>
+          <Ellipsis />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="start">{children}</DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
