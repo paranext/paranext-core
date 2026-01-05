3b51167efcc33a1a1354bb7d1f831f5b138bbc22
Katherine Jensen <katherine_jensen@sil.org>
2026-01-05 14:25:59 -0800

    Fix ScrTextCollection not being reinitialized before tests
    Part of PapiTestBase's teardown is a step to remove all projects from the ScrTextCollection, but if other test classes don't call Paratext.Globals.Initialize or a base setup that calls that, then the ScrTextCollection stays empty even if projects exist in the folder because it uses the exisitng cleared out ScrTextCollection

--- c-sharp-tests/Projects/FixtureSetup.cs
+++ c-sharp-tests/Projects/FixtureSetup.cs
@@ -1,47 +1,47 @@
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
 using System.Reflection;
 using Paranext.DataProvider.ParatextUtils;
+using SIL.WritingSystems;
 
-namespace TestParanextDataProvider.Projects
+[SetUpFixture]
+[ExcludeFromCodeCoverage]
+public class FixtureSetup
 {
-    [SetUpFixture]
-    [ExcludeFromCodeCoverage]
-    public class FixtureSetup
+    private static readonly string s_testFolder = Path.Combine(
+        Path.GetTempPath(),
+        "Platform.Bible.Tests"
+    );
+
+    public static string TestFolderPath => s_testFolder;
+
+    [OneTimeSetUp]
+    public void RunBeforeAnyTests()
     {
-        private static readonly string s_testFolder = Path.Combine(
-            Path.GetTempPath(),
-            "Platform.Bible.Tests"
-        );
+        if (!Sldr.IsInitialized)
+            Sldr.Initialize(true); // always use offline mode in tests.
 
-        public static string TestFolderPath => s_testFolder;
+        // Create folder for test running. Shouldn't actually be used, but ParatextData
+        // throws an error if the directory doesn't exist.
+        if (!Directory.Exists(s_testFolder))
+            Directory.CreateDirectory(s_testFolder);
 
-        [OneTimeSetUp]
-        public void RunBeforeAnyTests()
-        {
-            // Create folder for test running. Shouldn't actually be used, but ParatextData
-            // throws an error if the directory doesn't exist.
-            if (!Directory.Exists(s_testFolder))
-                Directory.CreateDirectory(s_testFolder);
+        var dllPath = Assembly.GetExecutingAssembly().Location;
+        var srcDir = Path.Combine(dllPath, "..", "..", "..", "..", "..", "c-sharp", "assets");
+        var sourcePath = Path.Combine(srcDir, "usfm.sty");
+        var destPath = Path.Combine(s_testFolder, "usfm.sty");
 
-            var dllPath = Assembly.GetExecutingAssembly().Location;
-            var srcDir = Path.Combine(dllPath, "..", "..", "..", "..", "..", "c-sharp", "assets");
-            var sourcePath = Path.Combine(srcDir, "usfm.sty");
-            var destPath = Path.Combine(s_testFolder, "usfm.sty");
+        // Only copy the usfm.sty file if it doesn't already exist.
+        if (!File.Exists(destPath))
+            File.Copy(sourcePath, destPath);
 
-            // Only copy the usfm.sty file if it doesn't already exist.
-            if (!File.Exists(destPath))
-                File.Copy(sourcePath, destPath);
+        ParatextGlobals.Initialize(s_testFolder);
+    }
 
-            ParatextGlobals.Initialize(s_testFolder);
-        }
-
-        [OneTimeTearDown]
-        public void RunAfterAnyTests()
-        {
-            // Clean up after the tests are run.
-            if (Directory.Exists(s_testFolder))
-                Directory.Delete(s_testFolder, true);
-        }
+    [OneTimeTearDown]
+    public void RunAfterAnyTests()
+    {
+        // Clean up after the tests are run.
+        if (Directory.Exists(s_testFolder))
+            Directory.Delete(s_testFolder, true);
     }
 }
