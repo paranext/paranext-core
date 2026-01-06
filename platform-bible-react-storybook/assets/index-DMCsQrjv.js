b3390fbb7b12981fb74a699cd9c7c0674e2a6ffe
tjcouch-sil <104016682+tjcouch-sil@users.noreply.github.com>
2026-01-06 22:23:01 +0000

    Deploying to github-pages from @ paranext/paranext-core@05b3c2eb437d8bd6744ecf7b512209c54e23a507 🚀

---
+++ platform-bible-react-storybook/assets/index-DMCsQrjv.js
@@ -0,0 +1,1 @@
+import{r as e}from"./iframe-D8E0DHM7.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
---
+++ platform-bible-react-storybook/assets/index-DTwt9jhc.js
@@ -0,0 +1,1 @@
+import{g as e}from"./iframe-D8E0DHM7.js";import{r as t}from"./index-DZZBo1Qm.js";var o=t();const c=e(o);export{o as r,c as v};
