3629b2ff0629e8174fefccf67c11daf7edde3016
Sebastian-ubs <141921979+Sebastian-ubs@users.noreply.github.com>
2026-01-27 18:24:20 +0100

    Remove unnecessary blank line

--- extensions/src/platform-scripture/src/main.ts
+++ extensions/src/platform-scripture/src/main.ts
@@ -547,7 +547,6 @@ export async function activate(context: ExecutionActivationContext) {
   logger.debug('platformScripture is finished activating!');
 }
 
-
 export async function deactivate() {
   logger.debug('platformScripture is deactivating!');
   return true;
