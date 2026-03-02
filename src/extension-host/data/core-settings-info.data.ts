1446a5d15264b24edde1d5a94de749505a58948c
tjcouch-sil <tj_couch@sil.org>
2026-03-02 17:35:38 -0600

    Added simple/power setting platform.interfaceMode

--- src/extension-host/data/core-settings-info.data.ts
+++ src/extension-host/data/core-settings-info.data.ts
@@ -42,6 +42,11 @@ export const platformSettings: SettingsContribution = [
         description: '%settings_platform_zoomFactor_description%',
         default: DEFAULT_ZOOM_FACTOR,
       },
+      'platform.interfaceMode': {
+        label: '%settings_platform_interfaceMode_label%',
+        description: '%settings_platform_interfaceMode_description%',
+        default: 'simple',
+      },
     },
   },
 ];
@@ -119,6 +124,12 @@ const zoomFactorValidator: SettingValidator<'platform.zoomFactor'> = async (
   return true;
 };
 
+const interfaceModeValidator: SettingValidator<'platform.interfaceMode'> = async (
+  newValue: string,
+): Promise<boolean> => {
+  return newValue === 'simple' || newValue === 'power';
+};
+
 export const coreSettingsValidators: Partial<AllSettingsValidators> = {
   'platform.verseRef': verseRefSettingsValidator,
   'platform.interfaceLanguage': interfaceLanguageValidator,
@@ -127,4 +138,5 @@ export const coreSettingsValidators: Partial<AllSettingsValidators> = {
   'platform.commentsEnabled': booleanValidator,
   'platform.requestTimeout': requestTimeoutValidator,
   'platform.zoomFactor': zoomFactorValidator,
+  'platform.interfaceMode': interfaceModeValidator,
 };
