ca339222c36c497cc7d5248c444ea001e53d1318
TJ Couch <tj_couch@sil.org>
2026-03-27 15:48:39 -0500

    Added ProgressUtils implementation that runs UI and run later stuff immediately as a simple way to get ParatextData's code running

---
+++ c-sharp/ParatextUtils/ProgressUtilsRunImmediately.cs
@@ -0,0 +1,99 @@
+using System.ComponentModel;
+using PtxUtils.Progress;
+
+namespace Paranext.DataProvider.ParatextUtils;
+
+/// <summary>
+/// A <see cref="ProgressUtils"/> implementation that executes all actions immediately on the
+/// calling thread. This is useful for our backend .NET process so we can run the code that
+/// ParatextData says to run. Without this, the default implementation is `DoNothingProgressUtils`
+/// which does not run anything that is supposed to be run on the UI thread or run later.
+///
+/// Note that even the "run later" methods run immediately in this implementation. If you encounter
+/// code that requires it to run later, you can change the implementation.
+/// </summary>
+public class ProgressUtilsRunImmediately : ProgressUtils
+{
+    protected override bool OnMainUiThreadInternal => true;
+
+    protected override void InvokeOnUIThreadInternal(ThreadStart action, bool exclusively) =>
+        action();
+
+    protected override void InvokeLaterOnUIThreadInternal(ThreadStart action) => action();
+
+    protected override void InvokeLaterOnUIThreadAllowingReEntryInternal(ThreadStart action) =>
+        action();
+
+    protected override EventHandler InvokeLaterOnIdleInternal(
+        ThreadStart action,
+        EventHandler cancelIdleEvent
+    )
+    {
+        action();
+
+        return (_s, _e) => {
+            // No op since the action is invoked immediately in this implementation.
+        };
+    }
+
+    protected override EventHandler InvokeLaterOnIdleInternal(
+        ThreadStart action,
+        EventHandler cancelIdleEvent,
+        int msDelay
+    )
+    {
+        action();
+
+        return (_s, _e) => {
+            // No op since the action is invoked immediately in this implementation.
+        };
+    }
+
+    protected override void CancelInvokeLaterOnIdleInternal(EventHandler idleEvent)
+    {
+        // No op since the action is invoked immediately in this implementation.
+    }
+
+    protected override void InvokeLaterOnIdleInternal(ThreadStart action) => action();
+
+    protected override void ExecuteInternal(
+        string title,
+        CancelModes cancelMode,
+        ThreadStart action,
+        bool topMost
+    ) => action();
+
+    protected override void ExecuteOnSameThreadInternal(
+        string title,
+        CancelModes cancelMode,
+        ThreadStart action
+    ) => action();
+
+    protected override BackgroundExecution CurrentExecutingBackgroundSingleAsyncInternal(
+        BackgroundExecutionAction action
+    ) => throw new NotImplementedException();
+
+    protected override void ExecuteInBackgroundSingleAsyncInternal(
+        IComponent parentControl,
+        string title,
+        CancelModes cancelMode,
+        BackgroundExecutionAction action,
+        ThreadStart cancelAction,
+        ExceptionHandlerDelegate exceptionHandler
+    ) => throw new NotImplementedException();
+
+    protected override BackgroundExecution ExecuteInBackgroundInternal(
+        IComponent parentControl,
+        string title,
+        CancelModes cancelMode,
+        Action<IProgressControl> action,
+        ThreadStart cancelAction,
+        ExceptionHandlerDelegate exceptionHandler,
+        // This is copied directly from `DoNothingProgressUtils` and is how the abstract method
+        // `ProgressUtils.ExecuteInBackgroundInternal` is declared. If it becomes a problem, we can
+        // deal with it then
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        ProgressBarOptions options = null
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+    ) => throw new NotImplementedException();
+}
