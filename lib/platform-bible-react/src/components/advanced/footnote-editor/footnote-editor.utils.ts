1d624b1be65bc56b96423768932feb699b0cfef6
Katherine Jensen <katherine_jensen@sil.org>
2026-03-30 07:56:53 -0700

    Fix prettier formatting

--- lib/platform-bible-react/src/components/advanced/footnote-editor/footnote-editor.utils.ts
+++ lib/platform-bible-react/src/components/advanced/footnote-editor/footnote-editor.utils.ts
@@ -35,9 +35,9 @@ export function generateInlineMarkerMenuListItems(
           action: () => {
             // `insertMarker` is a planned but not yet released method in the platform-editor API
             // eslint-disable-next-line no-type-assertion/no-type-assertion
-            (editorRef.current as unknown as { insertMarker?: (m: string) => void })?.insertMarker?.(
-              marker,
-            );
+            (
+              editorRef.current as unknown as { insertMarker?: (m: string) => void }
+            )?.insertMarker?.(marker);
             closeMarkersMenu();
           },
         };
