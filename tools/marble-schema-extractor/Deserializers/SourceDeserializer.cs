4d0679b45f21ae7452bdde77f0822dc4da5b44ec
Matt Lyons <matt_lyons@sil.org>
2026-04-29 13:46:47 -0500

    [P3][audit] enhanced-resources: add ArchiveListingDeserializer + image/bible/source shims with tests

---
+++ tools/marble-schema-extractor/Deserializers/SourceDeserializer.cs
@@ -0,0 +1,10 @@
+namespace MarbleSchemaExtractor;
+
+internal sealed class SourceDeserializer
+{
+    private readonly ArchiveListingDeserializer _inner = new();
+
+    public string CollectStats(ResourceArchive a) => _inner.CollectStats(a);
+
+    public string CollectSamples(ResourceArchive a, int count) => _inner.CollectSamples(a, count);
+}
