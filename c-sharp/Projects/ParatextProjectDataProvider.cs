967b9115841463c0ec7e3961c52937f17b7719d4
Danny Rorabaugh <imnasnainaec@gmail.com>
2026-05-01 13:39:18 -0400

    Add handling for extensions with boolean project settings

--- c-sharp/Projects/ParatextProjectDataProvider.cs
+++ c-sharp/Projects/ParatextProjectDataProvider.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
 using System.Xml;
 using System.Xml.Linq;
 using System.Xml.XPath;
@@ -1166,6 +1167,32 @@ internal class ParatextProjectDataProvider : ProjectDataProvider
                     ),
                 };
             }
+
+            // Check if extension setting is a non-string type
+            bool? defaultBool = null;
+            try
+            {
+                if (ProjectSettingsService.GetDefault(PapiClient, settingName) is JsonElement elem)
+                    defaultBool = elem.Deserialize<bool?>();
+            }
+            catch
+            {
+                // No registered default; keep original string behavior
+            }
+
+            // Boolean setting found, so convert from string
+            if (defaultBool != null)
+            {
+                return settingValue.ToUpperInvariant() switch
+                {
+                    "F" => false,
+                    "FALSE" => false,
+                    "T" => true,
+                    "TRUE" => true,
+                    _ => defaultBool,
+                };
+            }
+
             return settingValue;
         }
 
@@ -1305,6 +1332,8 @@ internal class ParatextProjectDataProvider : ProjectDataProvider
                                 ),
                             };
                         }
+                        else if (value is bool boolValue)
+                            value = boolValue ? "T" : "F";
                         scrText.Settings.SetSetting(paratextSettingName, value!.ToString());
                         // We are notifying when we release our lock, so don't automatically
                         // notify in `Save`
