0b1f623958be66da358620962d41ef4d7cd654e3
Levi Wenger <myeyesareonmizar@gmail.com>
2026-05-12 17:19:28 -0400

    feat: add ResourcePickerDialog dialog wrapper

---
+++ src/renderer/components/dialogs/resource-picker.dialog.tsx
@@ -0,0 +1,38 @@
+import { useLocalizedStrings } from '@papi/frontend/react';
+import { ResourcePickerDialog, RESOURCE_PICKER_DIALOG_STRING_KEYS } from 'platform-bible-react';
+import { DIALOG_BASE } from '@renderer/components/dialogs/dialog-base.data';
+import {
+  DialogDefinition,
+  DialogTypes,
+  RESOURCE_PICKER_DIALOG_TYPE,
+} from '@renderer/components/dialogs/dialog-definition.model';
+
+function ResourcePickerDialogWrapper({
+  allResources,
+  resourceType,
+  selectedResourceIds,
+  submitDialog,
+}: DialogTypes[typeof RESOURCE_PICKER_DIALOG_TYPE]['props']) {
+  const [localizedStrings] = useLocalizedStrings(RESOURCE_PICKER_DIALOG_STRING_KEYS);
+
+  return (
+    <ResourcePickerDialog
+      allResources={allResources}
+      resourceType={resourceType}
+      selectedResourceIds={selectedResourceIds}
+      localizedStrings={localizedStrings}
+      onSelect={submitDialog}
+    />
+  );
+}
+
+export const RESOURCE_PICKER_DIALOG: DialogDefinition<typeof RESOURCE_PICKER_DIALOG_TYPE> =
+  Object.freeze({
+    ...DIALOG_BASE,
+    tabType: RESOURCE_PICKER_DIALOG_TYPE,
+    defaultTitle: '%resourcePicker_title%',
+    initialSize: { width: 560, height: 520 },
+    Component: ResourcePickerDialogWrapper,
+  });
+
+export default RESOURCE_PICKER_DIALOG;
