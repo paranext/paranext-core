cbc2c9a93909a99bbb5d5149cbfae7b8f7c70503
Katherine Jensen <katherine_jensen@sil.org>
2026-05-22 12:12:00 -0700

    Add transition screen when project changes

--- src/renderer/app.component.tsx
+++ src/renderer/app.component.tsx
@@ -1,18 +1,24 @@
+import { useEffect } from 'react';
 import { PlatformDockLayout } from '@renderer/components/docking/platform-dock-layout.component';
 import { TestContext } from '@renderer/context/papi-context/test.context';
 import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
 import './app.component.scss';
 import { NotificationDisplay } from './components/notification-display';
 import { OverlayHost } from './components/overlay-host.component';
+import { WorkspaceUpdatingOverlay } from './components/overlays/workspace-updating-overlay.component';
 import { PlatformBibleToolbar } from './components/platform-bible-toolbar';
+import { initWorkspaceUpdatingService } from './services/workspace-updating-service';
 
 function Main() {
+  useEffect(() => initWorkspaceUpdatingService(), []);
+
   return (
     <TestContext.Provider value="test">
       <PlatformBibleToolbar />
       <PlatformDockLayout />
       <NotificationDisplay />
       <OverlayHost />
+      <WorkspaceUpdatingOverlay />
     </TestContext.Provider>
   );
 }
