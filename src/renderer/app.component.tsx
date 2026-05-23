c710ef1981b304de2b98b971a03346c89b750d24
Katherine Jensen <katherine_jensen@sil.org>
2026-05-22 19:50:21 -0700

    Remove some unneeded tests and fix localization keys

--- src/renderer/app.component.tsx
+++ src/renderer/app.component.tsx
@@ -5,7 +5,7 @@ import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
 import './app.component.scss';
 import { NotificationDisplay } from './components/notification-display';
 import { OverlayHost } from './components/overlay-host.component';
-import { WorkspaceUpdatingOverlay } from './components/overlays/workspace-updating-overlay.component';
+import { WorkspaceUpdatingOverlay } from './components/overlays/overlay-workspace-updating.component';
 import { PlatformBibleToolbar } from './components/platform-bible-toolbar';
 import { initWorkspaceUpdatingService } from './services/workspace-updating-service';
 
