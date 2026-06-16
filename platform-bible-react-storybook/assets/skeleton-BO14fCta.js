7c72e5db06786f8fdb9fc06a2041d844a677317a
tjcouch-sil <104016682+tjcouch-sil@users.noreply.github.com>
2026-06-16 21:24:42 +0000

    Deploying to github-pages from @ paranext/paranext-core@2ebfafd612f65b71cd0d575dad830777125db47f 🚀

--- platform-bible-react-storybook/assets/skeleton-BO14fCta.js
+++
@@ -1,3 +0,0 @@
-import{j as n}from"./iframe-D3e8NHgC.js";import{c as o}from"./utils-BPbySc-g.js";function d({className:e,...t}){return n.jsx("div",{"data-slot":"skeleton",className:o("pr-twp tw:animate-pulse tw:rounded-md tw:bg-muted",e),...t})}d.__docgenInfo={description:`Use to show a placeholder while content is loading. This component is from Shadcn UI.
-
-@see Shadcn UI Documentation: {@link https://ui.shadcn.com/docs/components/skeleton}`,methods:[],displayName:"Skeleton"};export{d as S};
