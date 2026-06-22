e22178a724499891a8cf17f1d0430ba60b2b7739
rolfheij-sil <108285668+rolfheij-sil@users.noreply.github.com>
2026-06-22 10:33:37 +0000

    Deploying to github-pages from @ paranext/paranext-core@865a2a194466f062b33d4c1742e6aa7ea9627c88 🚀

--- platform-bible-react-storybook/assets/arrow-up-DGv1BV0w.js
+++
@@ -1,6 +0,0 @@
-import{c as e}from"./createLucideIcon-BXLVz5Yd.js";/**
- * @license lucide-react v1.8.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const t=[["path",{d:"m5 12 7-7 7 7",key:"hav0vg"}],["path",{d:"M12 19V5",key:"x0mq9r"}]],n=e("arrow-up",t);export{n as A};
