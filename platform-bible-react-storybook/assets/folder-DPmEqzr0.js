d7c7002e174dadcc99fe8822c993a58251cc7b8c
merchako <alex_mercado@sil.org>
2026-07-02 01:02:24 +0000

    Deploying to github-pages from @ paranext/paranext-core@f6a60fca20ba8576f581827c8436de00f0f986ba 🚀

---
+++ platform-bible-react-storybook/assets/folder-DPmEqzr0.js
@@ -0,0 +1,6 @@
+import{c as e}from"./createLucideIcon-CuO2Cm-m.js";/**
+ * @license lucide-react v1.8.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z",key:"1kt360"}]],d=e("folder",t);export{d as F};
---
+++ platform-bible-react-storybook/assets/scroll-group-selector.stories-wf9POnnj.js
@@ -0,0 +1,31 @@
+import{r as d,j as a}from"./iframe-Dw0rVdo7.js";import"./index-D7R6UePp.js";import{S as n}from"./scroll-group-selector.component-T3-tKdne.js";import{l as r}from"./scripture-util-CIEEBU92-DFh2FVNP.js";import"./preload-helper-CTOgD26E.js";import"./index.es-LuWhpyxP.js";import"./index-D2t4nnj1.js";import"./select-Cs-EZS9q.js";import"./utils-BPbySc-g.js";import"./createReactComponent-1MGnNPfE.js";import"./IconCheck-DYO8bJDn.js";import"./index-BaQP4hhM.js";import"./index-BStPAbu_.js";import"./index-D9mJ00bA.js";import"./index-CtGA9XXZ.js";import"./index-BjWwjg1F.js";import"./index-BYchALu_.js";import"./index-Cfl-M2qe.js";import"./index-B66_QFvp.js";import"./index-wxsUL-3S.js";import"./index-BT3cHHEV.js";import"./index-Uwo-3Th-.js";import"./index-Do87lSy7.js";import"./index-Dnj6JFjT.js";import"./index-Bz0x5Def.js";import"./index-B_8u8Kfy.js";import"./z-index-CoNkaVR8.js";const p=[void 0,0,1,2,3,4],u={[r("undefined")]:"None",[r(0)]:"A",[r(1)]:"B",[r(2)]:"C",[r(3)]:"D",[r(4)]:"E"},U={title:"Advanced/ScrollGroupSelector",component:n,tags:["autodocs"]},s={render:e=>{const[o,t]=d.useState(e.scrollGroupId);return a.jsx(n,{...e,scrollGroupId:o,onChangeScrollGroupId:t})},args:{availableScrollGroupIds:p,scrollGroupId:void 0,localizedStrings:u}},l={render:e=>{const[o,t]=d.useState(e.scrollGroupId);return a.jsx(n,{...e,scrollGroupId:o,onChangeScrollGroupId:t,size:"default"})},args:{availableScrollGroupIds:p,scrollGroupId:2,localizedStrings:u}},c={render:e=>{const[o,t]=d.useState(e.scrollGroupId);return a.jsx(n,{...e,scrollGroupId:o,onChangeScrollGroupId:t,className:"tw:bg-yellow-100",size:"sm"})},args:{availableScrollGroupIds:p,scrollGroupId:1,localizedStrings:u}};var i,m,S;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: args => {
+    const [selected, setSelected] = useState<number | undefined>(args.scrollGroupId);
+    return <ScrollGroupSelector {...args} scrollGroupId={selected} onChangeScrollGroupId={setSelected} />;
+  },
+  args: {
+    availableScrollGroupIds,
+    scrollGroupId: undefined,
+    localizedStrings
+  }
+}`,...(S=(m=s.parameters)==null?void 0:m.docs)==null?void 0:S.source}}};var G,g,I;l.parameters={...l.parameters,docs:{...(G=l.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: args => {
+    const [selected, setSelected] = useState<number | undefined>(args.scrollGroupId);
+    return <ScrollGroupSelector {...args} scrollGroupId={selected} onChangeScrollGroupId={setSelected} size="default" />;
+  },
+  args: {
+    availableScrollGroupIds,
+    scrollGroupId: 2,
+    localizedStrings
+  }
+}`,...(I=(g=l.parameters)==null?void 0:g.docs)==null?void 0:I.source}}};var f,h,C;c.parameters={...c.parameters,docs:{...(f=c.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => {
+    const [selected, setSelected] = useState<number | undefined>(args.scrollGroupId);
+    return <ScrollGroupSelector {...args} scrollGroupId={selected} onChangeScrollGroupId={setSelected} className="tw:bg-yellow-100" size="sm" />;
+  },
+  args: {
+    availableScrollGroupIds,
+    scrollGroupId: 1,
+    localizedStrings
+  }
+}`,...(C=(h=c.parameters)==null?void 0:h.docs)==null?void 0:C.source}}};const V=["Default","WithCustomSize","WithCustomClass"];export{s as Default,c as WithCustomClass,l as WithCustomSize,V as __namedExportsOrder,U as default};
