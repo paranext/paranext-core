8d8200644d418a2205d9a8f6b40850960cbe01aa
imnasnainaec <imnasnainaec@gmail.com>
2026-08-13 20:12:08 +0000

    Deploying to github-pages from @ paranext/paranext-core@ff22ea69aa94d26e0477043ac7f21ef477896d57 🚀

---
+++ platform-bible-react-storybook/assets/index-DhjsFjAt.js
@@ -0,0 +1,1 @@
+import{r as e}from"./iframe-CI0j7VzG.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
---
+++ platform-bible-react-storybook/assets/scroll-group-selector.stories-D9LH_RlW.js
@@ -0,0 +1,41 @@
+import{r as a,j as u}from"./iframe-CI0j7VzG.js";import"./index-BKT-qVtu.js";import{S as s}from"./scroll-group-selector.component-DNMbOEFl.js";import{u as t}from"./scripture-util-Df5kpddK-BLSo4ED6.js";import"./preload-helper-CTOgD26E.js";import"./index.es-LuWhpyxP.js";import"./index-D2t4nnj1.js";import"./index-wk9rVj3k.js";import"./select-CjCmSLYF.js";import"./utils-BPbySc-g.js";import"./z-index-CoNkaVR8.js";import"./IconSelector-CYJ9-wsl.js";import"./createReactComponent-Dd2Kg3EF.js";import"./IconCheck-BBMUiH8O.js";import"./index-BaQP4hhM.js";import"./index-CY1OXvWa.js";import"./index-DhjsFjAt.js";import"./index-DokoenQl.js";import"./index-DBvvxmDx.js";import"./index-DjW7NFjX.js";import"./index-B9clKLwf.js";import"./index-BtfoUy8b.js";import"./index-COzqfNiL.js";import"./index-D2MxmmWw.js";import"./index-CEHeO39W.js";import"./index-WjLW6JCT.js";import"./index-DaARP9YL.js";import"./index-BT99s3tj.js";import"./index-BZs9EtUs.js";const p=[void 0,0,1,2,3,4],i={[t("undefined")]:"None",[t(0)]:"A",[t(1)]:"B",[t(2)]:"C",[t(3)]:"D",[t(4)]:"E"},ee={title:"Advanced/ScrollGroupSelector",component:s,tags:["autodocs"]},l={render:e=>{const[r,o]=a.useState(e.scrollGroupId);return u.jsx(s,{...e,scrollGroupId:r,onChangeScrollGroupId:o})},args:{availableScrollGroupIds:p,scrollGroupId:void 0,localizedStrings:i}},c={render:e=>{const[r,o]=a.useState(e.scrollGroupId);return u.jsx(s,{...e,scrollGroupId:r,onChangeScrollGroupId:o,disabled:!0})},args:{availableScrollGroupIds:p,scrollGroupId:1,localizedStrings:i}},n={render:e=>{const[r,o]=a.useState(e.scrollGroupId);return u.jsx(s,{...e,scrollGroupId:r,onChangeScrollGroupId:o,size:"default"})},args:{availableScrollGroupIds:p,scrollGroupId:2,localizedStrings:i}},d={render:e=>{const[r,o]=a.useState(e.scrollGroupId);return u.jsx(s,{...e,scrollGroupId:r,onChangeScrollGroupId:o,className:"tw:bg-yellow-100",size:"sm"})},args:{availableScrollGroupIds:p,scrollGroupId:1,localizedStrings:i}};var m,S,G;l.parameters={...l.parameters,docs:{...(m=l.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: args => {
+    const [selected, setSelected] = useState<number | undefined>(args.scrollGroupId);
+    return <ScrollGroupSelector {...args} scrollGroupId={selected} onChangeScrollGroupId={setSelected} />;
+  },
+  args: {
+    availableScrollGroupIds,
+    scrollGroupId: undefined,
+    localizedStrings
+  }
+}`,...(G=(S=l.parameters)==null?void 0:S.docs)==null?void 0:G.source}}};var I,g,f;c.parameters={...c.parameters,docs:{...(I=c.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  render: args => {
+    const [selected, setSelected] = useState<number | undefined>(args.scrollGroupId);
+    return <ScrollGroupSelector {...args} scrollGroupId={selected} onChangeScrollGroupId={setSelected} disabled />;
+  },
+  args: {
+    availableScrollGroupIds,
+    scrollGroupId: 1,
+    localizedStrings
+  }
+}`,...(f=(g=c.parameters)==null?void 0:g.docs)==null?void 0:f.source}}};var h,C,b;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  render: args => {
+    const [selected, setSelected] = useState<number | undefined>(args.scrollGroupId);
+    return <ScrollGroupSelector {...args} scrollGroupId={selected} onChangeScrollGroupId={setSelected} size="default" />;
+  },
+  args: {
+    availableScrollGroupIds,
+    scrollGroupId: 2,
+    localizedStrings
+  }
+}`,...(b=(C=n.parameters)==null?void 0:C.docs)==null?void 0:b.source}}};var z,x,v;d.parameters={...d.parameters,docs:{...(z=d.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  render: args => {
+    const [selected, setSelected] = useState<number | undefined>(args.scrollGroupId);
+    return <ScrollGroupSelector {...args} scrollGroupId={selected} onChangeScrollGroupId={setSelected} className="tw:bg-yellow-100" size="sm" />;
+  },
+  args: {
+    availableScrollGroupIds,
+    scrollGroupId: 1,
+    localizedStrings
+  }
+}`,...(v=(x=d.parameters)==null?void 0:x.docs)==null?void 0:v.source}}};const re=["Default","Disabled","WithCustomSize","WithCustomClass"];export{l as Default,c as Disabled,d as WithCustomClass,n as WithCustomSize,re as __namedExportsOrder,ee as default};
