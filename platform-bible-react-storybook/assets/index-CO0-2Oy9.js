0604ee3e1f932c0f5ffa0419f7cd7174f9426e42
mattgetgen <90851054+mattgetgen@users.noreply.github.com>
2026-08-28 18:07:05 +0000

    Deploying to github-pages from @ paranext/paranext-core@c54d76de9efbee354f260aa21fc45aa2695a31c2 🚀

--- platform-bible-react-storybook/assets/index-CO0-2Oy9.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./iframe-BCMCgd5N.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
---
+++ platform-bible-react-storybook/assets/shrink-step.context-xsHot5Pt.js
@@ -0,0 +1,1 @@
+import{r as e}from"./iframe-Cze7um0c.js";const t=Object.freeze({WIDE:0,TIGHT:1,TIGHTER:2,MINIMUM:3}),n=e.createContext(t.WIDE);function r(){return e.useContext(n)}export{n as S,t as a,r as u};
