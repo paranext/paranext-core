dc7589aed14dfef00f7dccb39b3f9755f75a8c1e
mattgetgen <90851054+mattgetgen@users.noreply.github.com>
2026-09-01 17:18:48 +0000

    Deploying to github-pages from @ paranext/paranext-core@7f6d2b63a052973dacb8fcb1d35209bda8ba4cb1 🚀

--- platform-bible-react-storybook/assets/shrink-step.context-BYGTOxKI.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./iframe-Bl__27RN.js";const t=Object.freeze({WIDE:0,TIGHT:1,TIGHTER:2,MINIMUM:3}),n=e.createContext(t.WIDE);function r(){return e.useContext(n)}export{n as S,t as a,r as u};
