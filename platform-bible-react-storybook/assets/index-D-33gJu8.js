5ae431acdff87378194f2837997a0d7ca82d0dcf
Sebastian-ubs <141921979+Sebastian-ubs@users.noreply.github.com>
2026-09-08 14:49:28 +0000

    Deploying to github-pages from @ paranext/paranext-core@495d44a439d383f59004817ad7ef99412bc04b75 🚀

--- platform-bible-react-storybook/assets/index-D-33gJu8.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./iframe-Cp-z13My.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
---
+++ platform-bible-react-storybook/assets/index-D7H6AtM5.js
@@ -0,0 +1,1 @@
+import{r as e}from"./iframe-CrSKGsLF.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
