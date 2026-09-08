b9910025b6de32fe54860307e8095a5e4c680b4b
jolierabideau <135999578+jolierabideau@users.noreply.github.com>
2026-09-08 19:39:39 +0000

    Deploying to github-pages from @ paranext/paranext-core@2b3f415409c8ae192d33bfdc5c1ef4ee13447ed4 🚀

---
+++ platform-bible-react-storybook/assets/index-Blgp0cz8.js
@@ -0,0 +1,1 @@
+import{r as n,R as d}from"./iframe-vYBtg29e.js";import{u as c}from"./index-Bp0If46l.js";var i=d[" useId ".trim().toString()]||(()=>{}),u=0;function f(t){const[e,o]=n.useState(i());return c(()=>{o(r=>r??String(u++))},[t]),e?`radix-${e}`:""}export{f as u};
---
+++ platform-bible-react-storybook/assets/index-Bp0If46l.js
@@ -0,0 +1,1 @@
+import{r as e}from"./iframe-vYBtg29e.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
