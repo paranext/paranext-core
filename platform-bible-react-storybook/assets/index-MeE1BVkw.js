a8028ecaafe30b36b9f9e83a1774f619b2cec2fd
lyonsil <matt_lyons@sil.org>
2026-09-14 18:47:34 +0000

    Deploying to github-pages from @ paranext/paranext-core@5b80a9d70b2676bf23e7f150e6b67978fe6322a4 🚀

--- platform-bible-react-storybook/assets/index-MeE1BVkw.js
+++
@@ -1,1 +0,0 @@
-import{r as n,R as d}from"./iframe-C7BuJ7OU.js";import{u as c}from"./index-SqVI2le6.js";var i=d[" useId ".trim().toString()]||(()=>{}),u=0;function f(t){const[e,o]=n.useState(i());return c(()=>{o(r=>r??String(u++))},[t]),e?`radix-${e}`:""}export{f as u};
--- platform-bible-react-storybook/assets/index-SqVI2le6.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./iframe-C7BuJ7OU.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
