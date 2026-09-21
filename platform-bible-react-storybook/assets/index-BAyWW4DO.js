908bda1f32864f8bad9eb31dfbf17132d1a22508
katherinejensen00 <katherine_jensen@sil.org>
2026-09-21 17:29:00 +0000

    Deploying to github-pages from @ paranext/paranext-core@890d4f8e3e4038f8598f70645afebbb39947ae39 🚀

--- platform-bible-react-storybook/assets/index-BAyWW4DO.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./iframe-DN4TZNjI.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
---
+++ platform-bible-react-storybook/assets/index-Bjuu3hij.js
@@ -0,0 +1,1 @@
+import{r as e}from"./iframe-DY9ET-4F.js";var o=globalThis!=null&&globalThis.document?e.useLayoutEffect:()=>{};export{o as u};
