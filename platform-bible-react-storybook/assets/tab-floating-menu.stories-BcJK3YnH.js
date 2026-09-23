f3eb7e2dd9edea0cc6f8dfea18e0fc41df676547
katherinejensen00 <katherine_jensen@sil.org>
2026-09-23 22:50:20 +0000

    Deploying to github-pages from @ paranext/paranext-core@017aea94c5cd260e5accadee4238353316163faf 🚀

---
+++ platform-bible-react-storybook/assets/tab-floating-menu.stories-BcJK3YnH.js
@@ -0,0 +1,51 @@
+import{j as r}from"./iframe-B2RFrLgy.js";import{T as M}from"./tab-dropdown-menu.component-RzoEI0rT.js";import{T as w}from"./tab-toolbar-container.component-BiVtGWlw.js";import{c as j}from"./createLucideIcon-CrcL7h88.js";import"./preload-helper-CTOgD26E.js";import"./dropdown-menu--iUNoaUE.js";import"./menu.context-C597e1lk.js";import"./index-BnuTq2W6.js";import"./utils-BPbySc-g.js";import"./z-index-DiGYIwoM.js";import"./IconChevronRight-DA1zpnyM.js";import"./index-MLgWA4d8.js";import"./index-DRY2P-bc.js";import"./index-Dqe1W6a2.js";import"./index-il1NJKeu.js";import"./index-CsufbBdq.js";import"./index-BC8kA-4A.js";import"./index-D4k2_iFy.js";import"./index-CoPxZDiv.js";import"./index-D1dMN21N.js";import"./index-Cl7vIWUF.js";import"./index-u97pfg6Y.js";import"./floating-ui.dom-CQVRXqPN.js";import"./index-CmJTvE4z.js";import"./index-DPyuZgxP.js";import"./index-DdTRfUxa.js";import"./createReactComponent-Db5xavSd.js";import"./IconCheck-B5EIhw5S.js";import"./tooltip-UU6KwYTh.js";import"./button-DMuXj9wg.js";import"./index-CQulcN6m.js";import"./focus.util-DRSEP984.js";import"./use-interaction-modality.hook-BzXRsyES.js";import"./menu.util-C3wJQgOq.js";import"./menu-icon.component-DcHoOxW0.js";import"./shrink-step.context-CfSQ-HEi.js";import"./use-shrink-step.hook-N4Aez33n.js";/**
+ * @license lucide-react v1.8.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const T=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],I=j("plus",T);function b({onSelectProjectMenuItem:n,projectMenuData:a,id:g,className:f,menuButtonIcon:y}){return r.jsx(w,{className:"tw:pointer-events-none",id:g,children:a&&r.jsx(M,{onSelectMenuItem:n,menuData:a,tabLabel:"Project",icon:y,className:`tw:pointer-events-auto tw:shadow-lg ${f}`,buttonVariant:"outline",showSectionHeadings:!0})})}b.__docgenInfo={description:`Renders a TabDropdownMenu with a trigger button that looks like the menuButtonIcon or like the
+default of three stacked horizontal lines (aka the hamburger). The menu "floats" over the content
+so it is always visible. When clicked, it displays a dropdown menu with the projectMenuData.`,methods:[],displayName:"TabFloatingMenu",props:{onSelectProjectMenuItem:{required:!0,tsType:{name:"SelectMenuItemHandler"},description:`The handler to use for toolbar item commands related to the project menu. Here is a basic
+example of how to create this:
+
+@example
+
+\`\`\`tsx
+const projectMenuCommandHandler: SelectMenuItemHandler = async (selectedMenuItem) => {
+  const commandName = selectedMenuItem.command;
+  try {
+    // Assert the more specific type. Assert the more specific type. The menu data should
+    // specify a valid command name here. If not, the error will be caught.
+    // eslint-disable-next-line no-type-assertion/no-type-assertion
+    await papi.commands.sendCommand(commandName as CommandNames);
+  } catch (e) {
+    throw new Error(
+      \`handleMenuCommand error: command: \${commandName}. \${JSON.stringify(e)}\`,
+    );
+  }
+};
+\`\`\``},projectMenuData:{required:!1,tsType:{name:"Localized",elements:[{name:"MultiColumnMenu"}],raw:"Localized<MultiColumnMenu>"},description:`Menu data that is used to populate the Menubar component for the project menu. In an extension,
+the menu data comes from menus.json in the contributions folder. To access that info, use
+useMemo to get the WebViewMenu.`},id:{required:!1,tsType:{name:"string"},description:"Optional unique identifier"},className:{required:!1,tsType:{name:"string"},description:"Additional css classes to help with unique styling of the extensible toolbar"},menuButtonIcon:{required:!1,tsType:{name:"ReactNode"},description:"Icon that will be displayed on the Menu Button. Defaults to the hamburger menu icon."}}};const N={columns:{tools:{label:"Tools",order:1},info:{label:"Info",order:2},project:{label:"Project",order:3}},groups:{general:{column:"info",order:1},inventory:{column:"tools",order:1},checks:{column:"tools",order:2},projectTop:{column:"project",order:1,isExtensible:!0},manageBooks:{column:"project",order:2},deleteProject:{column:"project",order:3},projectDetails:{column:"project",order:4,isExtensible:!0}},items:[{label:"Inventory: Characters...",group:"inventory",order:1,command:"openCharactersInventory",localizeNotes:""},{label:"Inventory: Repeated Words...",group:"inventory",order:2,command:"openRepeatedWordsInventory",localizeNotes:""},{label:"Publisher Info",group:"general",order:1,command:"showPublisherInfo",localizeNotes:""},{label:"Assignments and Progress",group:"projectTop",order:2,command:"assignments",localizeNotes:""},{label:"Open Project Settings",group:"projectTop",order:3,command:"openSettings",localizeNotes:""}]},me={title:"Advanced/TabFloatingMenu",component:b,tags:["autodocs"],args:{onSelectProjectMenuItem:n=>console.log("Project Menu Run command: ",n),projectMenuData:N}},e={},o={args:{menuButtonIcon:r.jsx(I,{})},parameters:{docs:{description:{story:"TabFloatingMenu with a custom icon as the menu button."}}}},t={args:{className:"tw:bg-red-100 tw:border-red-400"},parameters:{docs:{description:{story:"TabFloatingMenu with custom background and border color."}}}};var s,i,m;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var c,l,d;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    menuButtonIcon: <Plus />
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'TabFloatingMenu with a custom icon as the menu button.'
+      }
+    }
+  }
+}`,...(d=(l=o.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};var u,p,h;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    className: 'tw:bg-red-100 tw:border-red-400'
+  },
+  parameters: {
+    docs: {
+      description: {
+        story: 'TabFloatingMenu with custom background and border color.'
+      }
+    }
+  }
+}`,...(h=(p=t.parameters)==null?void 0:p.docs)==null?void 0:h.source}}};const ce=["Default","WithCustomIcon","WithCustomClassName"];export{e as Default,t as WithCustomClassName,o as WithCustomIcon,ce as __namedExportsOrder,me as default};
